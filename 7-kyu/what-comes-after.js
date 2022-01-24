cbd079fe1d2941e0c8b41f8684d4328193318db9
Alex Curran <alexcurran.va@gmail.com>
2022-01-24 10:52:52 -0600

    Working solution for what-comes-after.js

---
+++ 7-kyu/what-comes-after.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * You will be given two inputs: a string of words and a letter.
+ * For each string, return the alphabetic character after every instance of letter (case insensitive).
+ * If there is a number, punctuation, or underscore following the letter it should not be returned.
+ * 
+ * Solution 1
+ * Time: O(n)
+ * Space: O(n)
+ * 
+ * @param {string} str 
+ * @param {string} letter
+ * @returns {string}
+ */
+function comesAfter(str, letter) {
+    const myRegex = new RegExp(`(?<=${letter})[a-z]`, "gi");
+    const finalStrArr = str.match(myRegex);
+
+    return finalStrArr ? finalStrArr.join("") : "";
+}
+
+console.log( comesAfter("are you really learning Ruby?", "r") ); // "eenu"
+console.log( comesAfter("Pirates say arrrrrrrrr.", "r") ); // "arrrrrrrr"
