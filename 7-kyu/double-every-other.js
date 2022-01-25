fcf1e2269a0b70cbf43b52522c445f8bb748ce58
Alex Curran <alexcurran.va@gmail.com>
2022-01-25 13:55:34 -0600

    Working O(n) solution for double-every-other.js

---
+++ 7-kyu/double-every-other.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+function doubleEveryOther(arr) {
+    return arr.map((num, index) => index % 2 === 0 ? num : num * 2);
+}
+
+console.log( doubleEveryOther([1, 2, 3, 4]) ); // [1, 4, 3, 8]
