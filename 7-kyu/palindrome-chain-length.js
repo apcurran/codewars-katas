ad6a7b129af13155d2219d6bb41092fc67e1b720
Alex Curran <alexcurran.va@gmail.com>
2022-01-27 16:55:22 -0600

    Working solution for palindrome-chain-length.js

---
+++ 7-kyu/palindrome-chain-length.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n^2)
+ * space: O(n^2)
+ * 
+ * @param {number} n 
+ * @returns {number}
+ */
+function palindromeChainLength(n) {
+    const reversedNumDigits = reverseDigits(n);
+
+    if (n !== reversedNumDigits) {
+        return 1 + palindromeChainLength(n + reversedNumDigits);
+    }
+
+    return 0;
+}
+
+function reverseDigits(num) {
+    return +num
+            .toString()
+            .split("")
+            .reverse()
+            .join("");
+}
+
+console.log( palindromeChainLength(87) ); // 4
+console.log( palindromeChainLength(89) ); // 24
