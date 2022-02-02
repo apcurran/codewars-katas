a6e198c51eea8d32feeca4c160aea012a22e870a
Alex Curran <alexcurran.va@gmail.com>
2022-02-02 09:30:35 -0600

    Working solution for convert-a-linked-list-to-a-str.js

---
+++ 7-kyu/convert-a-linked-list-to-a-str.js
@@ -0,0 +1,36 @@
+"use strict";
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Node} list 
+ * @returns {string}
+ */
+function stringify(list) {
+    let current = list;
+    let resStr = "";
+    
+    while (current !== null) {
+        resStr += `${current.data} -> `;
+        current = current.next;
+    }
+
+    // Add last null pointer
+    resStr += "null";
+
+    return resStr;
+}
+
+const myList = new Node(1, new Node(2, new Node(3)));
+console.log(myList);
+console.log( stringify(myList) ); // "1 -> 2 -> 3 -> null"
+
