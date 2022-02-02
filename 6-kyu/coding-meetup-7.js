c9e5c8ce3bdaf6da2ed29ea6c144dcff1e0e6a1e
Alex Curran <alexcurran.va@gmail.com>
2022-02-02 09:52:23 -0600

    Working solution for coding-meetup-7.js

---
+++ 6-kyu/coding-meetup-7.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object[]]} list 
+ * @returns {object[]}
+ */
+function findSenior(list) {
+    list.sort((objA, objB) => objA.age - objB.age);
+
+    return list.filter((obj) => obj.age === (list[list.length - 1]).age);
+}
+
+var list1 = [
+    { firstName: "Gabriel", lastName: "X.", country: "Monaco", continent: "Europe", age: 49, language: "PHP" },
+    { firstName: "Odval", lastName: "F.", country: "Mongolia", continent: "Asia", age: 38, language: "Python" },
+    { firstName: "Emilija", lastName: "S.", country: "Lithuania", continent: "Europe", age: 19, language: "Python" },
+    { firstName: "Sou", lastName: "B.", country: "Japan", continent: "Asia", age: 49, language: "PHP" },
+];
+console.log( findSenior(list1) );
+// [
+//     { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//     { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]
