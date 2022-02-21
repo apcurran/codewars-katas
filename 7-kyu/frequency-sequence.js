d576a130b2d3a5c93b45127c6c538d7fa3b11821
Alex Curran <alexcurran.va@gmail.com>
2022-02-21 09:49:47 -0600

    Working solution for frequency-sequence.js

---
+++ 7-kyu/frequency-sequence.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @param {string} sep 
+ * @returns {string}
+ */
+function freqSeq(str, sep) {
+    let charMap = new Map();
+    // Create a res string arr
+    let resStrArr = [];
+    // Iterate through str
+    for (let char of str) {
+        // Record each char and charFreq in a map obj
+        if (!charMap.has(char)) {
+            charMap.set(char, 1);
+        } else {
+            const prevCharFreq = charMap.get(char);
+            charMap.set(char, prevCharFreq + 1);
+        }
+    }
+    // Loop through map obj
+    for (let char of str) {
+        const charFreq = charMap.get(char);
+        // for each char add to the resStrArr the freq val
+        resStrArr.push(charFreq);
+    }
+
+    // Join arr with sep between chars
+    return resStrArr.join(sep);
+}
+
+console.log( freqSeq("hello world", "-") ); // => "1-1-3-3-2-1-1-2-1-3-1"
+
