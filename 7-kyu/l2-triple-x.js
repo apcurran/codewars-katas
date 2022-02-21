128d9f1bc1c8056c96aed7ff4df9f9d8a28f4af6
Alex Curran <alexcurran.va@gmail.com>
2022-02-21 11:21:41 -0600

    Second solution

--- 7-kyu/l2-triple-x.js
+++ 7-kyu/l2-triple-x.js
@@ -1,7 +1,38 @@
 "use strict";
 
+// /**
+//  * Solution 1
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string} str 
+//  * @returns {boolean}
+//  */
+// function tripleX(str){
+//     let firstX = false;
+
+//     for (let i = 0; i < str.length; i++) {
+//         const currLetter = str[i];
+
+//         if (firstX) break;
+
+//         if (currLetter === "x") {
+//             firstX = true;
+//             // check for next two chars as x chars
+//             const secondLetter = str[i + 1];
+//             const thirdLetter = str[i + 2];
+
+//             if (secondLetter === "x" && thirdLetter === "x") {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * Solution 1
+ * Solution 2
  * time: O(n)
  * space: O(1)
  * 
@@ -9,26 +40,11 @@
  * @returns {boolean}
  */
 function tripleX(str){
-    let firstX = false;
+    const firstXIndex = str.indexOf("x");
+    const isSecondX = str[firstXIndex + 1] === "x";
+    const isThirdX = str[firstXIndex + 2] === "x";
 
-    for (let i = 0; i < str.length; i++) {
-        const currLetter = str[i];
-
-        if (firstX) break;
-
-        if (currLetter === "x") {
-            firstX = true;
-            // check for next two chars as x chars
-            const secondLetter = str[i + 1];
-            const thirdLetter = str[i + 2];
-
-            if (secondLetter === "x" && thirdLetter === "x") {
-                return true;
-            }
-        }
-    }
-
-    return false;
+    return isSecondX && isThirdX;
 }
 
 console.log( tripleX("abraxxxas") ); // true
