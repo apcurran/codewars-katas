1c15d16d1782e53d5d44f37ac0b0b68d021426d5
Alex Curran <alexcurran.va@gmail.com>
2022-02-24 16:03:39 -0600

    Working solution for printer-errors.js

---
+++ 7-kyu/printer-errors.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function printerError(str) {
+    let errCounter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const currCharCode = str.charCodeAt(i);
+
+        if (currCharCode > 109) {
+            errCounter++;
+        }
+    }
+
+    return `${errCounter}/${str.length}`;
+}
+
+console.log( printerError("aaabbbbhaijjjm") ); // "0/14"
+console.log( printerError("aaaxbbbbyyhwawiwjjjwwm") ); // "8/22"
