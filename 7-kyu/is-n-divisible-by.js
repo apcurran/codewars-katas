108b388a47cf74dc417392c96a6664dfb45145f8
Alex Curran <alexcurran.va@gmail.com>
2022-02-25 15:55:55 -0600

    Working solution for is-n-divisible-by.js

---
+++ 7-kyu/is-n-divisible-by.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n)
+ * space: O(n) -- rest param in arr format
+ * 
+ * @param {number} n 
+ * @param  {...number} otherArgs 
+ * @returns {boolean}
+ */
+function isDivisible(n, ...otherArgs) {
+    for (let arg of otherArgs) {
+        if (n % arg !== 0) return false;
+    }
+
+    return true;
+}
+
+console.log( isDivisible(100,5,4,10,25,20) ); // true
+console.log( isDivisible(12,7) ); // false
+console.log( isDivisible(7) ); // true
