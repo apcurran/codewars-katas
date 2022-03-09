ba23d6816feeea59eea4bd2858878aa4b6e8a1c8
Alex Curran <alexcurran.va@gmail.com>
2022-03-09 09:53:21 -0600

    Working solution for least-larger.js

---
+++ 7-kyu/least-larger.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @param {number} i 
+ * @returns {number}
+ */
+function leastLarger(arr, i) {
+    const iNum = arr[i];
+    const largerNums = arr.filter((num) => num > iNum);
+    const smallestLargerNum = Math.min(...largerNums);
+
+    return arr.indexOf(smallestLargerNum);
+}
+
+console.log( leastLarger([4, 1, 3, 5, 6], 0) ); // 3
+console.log( leastLarger( [4, 1, 3, 5, 6], 4 ) ); // -1
+console.log( leastLarger( [1, 3, 5, 2, 4], 0 ) ); // 3
