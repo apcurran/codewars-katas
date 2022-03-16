9a5f13f8b18a41b3ca57e9a5fca4d4413484c11e
Alex Curran <alexcurran.va@gmail.com>
2022-03-16 09:38:47 -0500

    Working solution for filter-unused-digits.js

---
+++ 7-kyu/filter-unused-digits.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {...number} nums
+ * @returns {string}
+ */
+function unusedDigits(...nums) {
+    const numsDigitsArr = nums
+                            .join("")
+                            .split("");    
+    const allDigitsStr = "0123456789";
+    let missingDigitsStr = "";
+
+    for (let strDigit of allDigitsStr) {
+        const isDigitPresent = numsDigitsArr.includes(strDigit);
+
+        if (!isDigitPresent) {
+            missingDigitsStr += strDigit;
+        }
+    }
+
+    return missingDigitsStr;
+}
+
+console.log( unusedDigits(12, 34, 56, 78) ); // "09"
+console.log( unusedDigits(2015, 8, 26) ); // "3479"
