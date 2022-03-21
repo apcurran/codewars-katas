7a0b67acf96b34d05e9a313dcf51d743456ae191
Alex Curran <alexcurran.va@gmail.com>
2022-03-21 09:41:57 -0500

    Working solution for parts-of-a-list.js

---
+++ 7-kyu/parts-of-a-list.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n^2)
+ * 
+ * @param {string[]} arr 
+ * @returns {string[][]}
+ */
+function partlist(arr) {
+    let resArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        const firstPartArr = arr
+                                .slice(0, i)
+                                .join(" ");
+        
+        const secondPartArr = arr
+                                .slice(i)
+                                .join(" ");
+        const newArr = [firstPartArr, secondPartArr];
+        resArr.push(newArr);
+    }
+
+    return resArr;
+}
+
+console.log( partlist(["az", "toto", "picaro", "zone", "kiwi"]) );
+// [
+//     ["az", "toto picaro zone kiwi"],
+//     ["az toto", "picaro zone kiwi"],
+//     ["az toto picaro", "zone kiwi"],
+//     ["az toto picaro zone", "kiwi"]
+// ]
