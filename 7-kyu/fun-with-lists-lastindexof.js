c4ee6ff446743d7c3e1a0b3f289f2faf47ef89d5
Alex Curran <alexcurran.va@gmail.com>
2022-04-11 11:52:14 -0500

    Working solution fun-with-lists-lastindexof.js

---
+++ 7-kyu/fun-with-lists-lastindexof.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {object} head 
+ * @param {number} value 
+ * @returns {number}
+ */
+function lastIndexOf(head, value) {
+    let index = 0;
+    let lastValIndex = -1;
+    let current = head;
+
+    while (current !== null) {
+        if (current.data === value) {
+            lastValIndex = index;
+        }
+        
+        index++;
+        current = current.next;
+    }
+
+    return lastValIndex;
+}
