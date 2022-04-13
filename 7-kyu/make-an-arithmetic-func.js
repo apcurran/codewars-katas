43a51ac6681222b3f0d05ad3e8187905965a1782
Alex Curran <alexcurran.va@gmail.com>
2022-04-13 09:16:18 -0500

    Working solution for make-an-arithmetic-func.js

---
+++ 7-kyu/make-an-arithmetic-func.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @param {string} operator 
+ * @returns {number}
+ */
+function arithmetic(a, b, operator) {
+    switch (operator) {
+        case "add":
+            return a + b;
+        case "subtract":
+            return a - b;
+        case "multiply":
+            return a * b;
+        case "divide":
+            return a / b;
+    }
+}
+
+console.log( arithmetic(5, 2, "add") ); // 7
