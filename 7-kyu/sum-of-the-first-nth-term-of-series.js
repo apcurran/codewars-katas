708aa81713329b8a7b59226909d64a4c6f28d906
Alex Curran <alexcurran.va@gmail.com>
2022-04-15 10:04:10 -0500

    Working solution for sum-of-the-first-nth-term-of-series.js

---
+++ 7-kyu/sum-of-the-first-nth-term-of-series.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {string}
+ */
+function SeriesSum(n) {
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        const currNum = 1 / (1 + i * 3);
+        sum += currNum;
+    }
+
+    return sum.toFixed(2);
+}
+
+console.log( SeriesSum(2) ); // "1.25"
