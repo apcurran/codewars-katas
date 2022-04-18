c45c68e213f778be9f32573bdd50b6f7c4e08a5d
Alex Curran <alexcurran.va@gmail.com>
2022-04-18 09:12:54 -0500

    Working solution for chain-me.js

---
+++ 7-kyu/chain-me.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} input 
+ * @param {function[]} fsArr 
+ * @returns {number}
+ */
+function chain(input, fsArr) {
+    return fsArr.reduce((result, currFunc) => currFunc(result), input);
+}
+
+function add(num) {
+    return num + 1;
+}
+
+function mult(num) {
+    return num * 30;
+}
+
+console.log( chain(2, [add, mult]) ); // 90
