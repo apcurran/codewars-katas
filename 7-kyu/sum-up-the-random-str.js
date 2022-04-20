278cc069ab0a6b993bf8508b231176520ff154e4
Alex Curran <alexcurran.va@gmail.com>
2022-04-20 09:27:22 -0500

    Working solution for sum-up-the-random-str.js

---
+++ 7-kyu/sum-up-the-random-str.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n) 
+ * space: O(n) -- split copy arr
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function sumFromString(str) {
+    return str
+            .split(/[^0-9]/)
+            .reduce((sum, currStr) => sum + Number(currStr), 0);
+}
+
+console.log(sumFromString("In 2015, I want to know how much does iPhone 6+ cost?")); // 2021
