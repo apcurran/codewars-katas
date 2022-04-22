d531ae321c55e8393e9848e8c579bc85477b9ec0
Alex Curran <alexcurran.va@gmail.com>
2022-04-22 09:26:54 -0500

    Working regex solution for simple-str-chars.js

---
+++ 7-kyu/simple-str-chars.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @returns {number[]}
+ */
+function solve(str) {
+    const uppercaseChars = str.match(/[A-Z]/g) || [];
+    const lowercaseChars = str.match(/[a-z]/g) || [];
+    const numberChars = str.match(/[0-9]/g) || [];
+    const specialChars = str.match(/\W|_/g) || [];
+
+    return [uppercaseChars.length, lowercaseChars.length, numberChars.length, specialChars.length];
+}
+
+// console.log( solve("*'&ABCDabcde12345") ); // [4,5,5,3]
+console.log( solve("*'&ABCD12345") ); // [4,5,5,3]
