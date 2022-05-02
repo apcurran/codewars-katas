9b0bdd0276e0d177b084c2bbfe11c87c45afd45d
Alex Curran <alexcurran.va@gmail.com>
2022-05-02 09:26:50 -0500

    Working solution for spongebob-meme.js

---
+++ 7-kyu/spongebob-meme.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} sentence 
+ * @returns {string}
+ */
+function spongeMeme(sentence) {
+    return sentence
+        .split("")
+        .map((char, index) => {
+            // check if index is even -> uppercase
+            // check if index is odd -> lowercase
+            const isEven = index % 2 === 0;
+
+            return isEven ? char.toUpperCase() : char.toLowerCase();
+        }).join("");
+}
+
+console.log(spongeMeme("stop Making spongebob Memes!")); // "StOp mAkInG SpOnGeBoB MeMeS!"
