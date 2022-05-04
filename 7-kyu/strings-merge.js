7ad3c36404ae356e92355e7112d2f16fb7709e89
Alex Curran <alexcurran.va@gmail.com>
2022-05-04 09:24:02 -0500

    Working solution for strings-merge.js

---
+++ 7-kyu/strings-merge.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} string1 
+ * @param {string} string2 
+ * @param {string} letter 
+ * @returns {string}
+ */
+function stringMerge(string1, string2, letter) {
+    const firstWordBeginning = string1.split(letter, 1);
+    const secondWordEnding = string2.slice(string2.indexOf(letter));
+
+    return firstWordBeginning + secondWordEnding;
+}
+
+// console.log( stringMerge("hello", "world", "l") ); // "held"
+// console.log( stringMerge("coding", "anywhere", "n") ); // "codinywhere"
+console.log( stringMerge("jason", "samson", "s") ); // "jasamson"
