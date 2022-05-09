41bf3d5b826ce2e6695465aa9268df348429bc1e
Alex Curran <alexcurran.va@gmail.com>
2022-05-09 09:46:36 -0500

    Working solution for where-is-my-parent.js

---
+++ 6-kyu/where-is-my-parent.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2 * log n) -- O(n) operation within n * log n sort func
+ * space: O(n)
+ * 
+ * @param {string} dancingBrigade 
+ * @returns {string}
+ */
+function findChildren(dancingBrigade) {
+    const alphaStr = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+    return dancingBrigade
+            .split("")
+            .sort((a, b) => alphaStr.indexOf(a) - alphaStr.indexOf(b))
+            .join("");
+}
+
+console.log( findChildren("beeeEBb") ); // "BbbEeee"
