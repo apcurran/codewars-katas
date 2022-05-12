cfad767de7988d646993a3e2afe32d7d6b215e2a
Alex Curran <alexcurran.va@gmail.com>
2022-05-12 10:10:54 -0500

    Working solution for make-the-deadfish-swim.js

---
+++ 6-kyu/make-the-deadfish-swim.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} data 
+ * @returns {number[]}
+ */
+function parse(data) {
+    let resArr = [];
+    let currNum = 0;
+
+    for (let char of data) {
+        if (char === "i") {
+            currNum++;
+        } else if (char === "d") {
+            currNum--;
+        } else if (char === "s") {
+            currNum = currNum * currNum;
+        } else if (char === "o") {
+            resArr.push(currNum);
+        }
+    }
+
+    return resArr;
+}
+
+console.log( parse("iiisdoso") ); // [8, 64]
