2291aecc6287d0bfd19e6cc228c0327bd2d8c6b7
Alex Curran <alexcurran.va@gmail.com>
2022-05-23 09:25:18 -0500

    Working solution for min-sum-of-arr.js

---
+++ 7-kyu/min-sum-of-arr.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function minSum(arr) {
+    // sort list small -> large
+    const smallToLargeArr = [...arr].sort((a, b) => a - b);
+    let minimalSum = 0;
+    
+    // iterate through first half of list
+    for (let i = 0; i < smallToLargeArr.length / 2; i++) {
+        const smallerNum = smallToLargeArr[i];
+        const largerNum = smallToLargeArr[smallToLargeArr.length - i - 1];
+        // for each smallest num (at beginning) mult. by largest num (at end)
+        const minProd = smallerNum * largerNum;
+        // add prod to overall sum
+        minimalSum += minProd;
+    }
+
+    return minimalSum;
+}
+
+console.log( minSum([5, 4, 2, 3]) ); // 22
+console.log( minSum([12, 6, 10, 26, 3, 24]) ); // 342
