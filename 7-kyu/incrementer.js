1738611e7a1dffca772c77b6144a268645610d55
Alex Curran <alexcurran.va@gmail.com>
2022-05-24 09:40:37 -0500

    Working solutions for incrementer.js

---
+++ 7-kyu/incrementer.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// /**
+//  * solution 1 -- for loop
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums 
+//  * @returns {number[]}
+//  */
+// function incrementer(nums) {
+//     let resArr = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const currNum = nums[i];
+//         const incrementAmt = i + 1;
+//         const incrementedNum = (currNum + incrementAmt) % 10;
+//         resArr.push(incrementedNum);
+//     }
+
+//     return resArr;
+// }
+
+/**
+ * solution 2 -- array.map() method
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums 
+ * @returns {number[]}
+ */
+function incrementer(nums) {
+    return nums.map((num, index) => {
+        const incrementAmt = index + 1;
+
+        return (num + incrementAmt) % 10;
+    });
+}
+
+console.log( incrementer([1, 2, 3]) ); // [2, 4, 6]
+console.log( incrementer([4, 6, 9, 1, 3]) ); // [5, 8, 2, 5, 8]
