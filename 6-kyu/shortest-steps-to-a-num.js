6e91caf5775d0a0c64a34ec1c35e9997e3dc0ffe
Alex Curran <alexcurran.va@gmail.com>
2022-05-26 09:22:00 -0500

    Working solution for shortest-steps-to-a-num.js

---
+++ 6-kyu/shortest-steps-to-a-num.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n) -- worst-case / O(log n) best-case
+ * space: O(1)
+ * 
+ * @param {number} num 
+ * @returns {number}
+ */
+function shortestStepsToNum(num) {
+    let numCopy = num;
+    let stepsCounter = 0;
+
+    while (numCopy > 1) {
+        if (numCopy % 2 === 0) {
+            numCopy /= 2;
+        } else {
+            numCopy--;
+        }
+
+        stepsCounter++;
+    }
+
+    return stepsCounter;
+}
+
+console.log( shortestStepsToNum(1) ); // 0
+console.log( shortestStepsToNum(12) ); // 4
+console.log( shortestStepsToNum(16) ); // 4
+console.log( shortestStepsToNum(71) ); // 9
