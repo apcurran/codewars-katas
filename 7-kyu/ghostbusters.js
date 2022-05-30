b070f173d7441d9e1660cce1381b2178fdd58a5b
Alex Curran <alexcurran.va@gmail.com>
2022-05-30 09:29:15 -0500

    Working solution for ghostbusters.js

---
+++ 7-kyu/ghostbusters.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} building 
+ * @returns {string}
+ */
+function ghostBusters(building) {
+    if (!building.includes(" ")) return "You just wanted my autograph didn't you?";
+
+    return building
+            .split(" ")
+            .join("");
+}
+
+console.log( ghostBusters("Sky scra per") ); // "Skyscraper"
+console.log( ghostBusters("") ); // "You just wanted my autograph didn't you?"
+console.log( ghostBusters("O  f fi ce") ); // "Office"
