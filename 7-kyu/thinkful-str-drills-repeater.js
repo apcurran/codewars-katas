4d678cb62170b8ceb9b07e9e87de0fe6f480ee10
Alex Curran <alexcurran.va@gmail.com>
2022-06-06 08:38:19 -0500

    Working solution for thinkful-str-drills-repeater.js

---
+++ 7-kyu/thinkful-str-drills-repeater.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @param {number} n 
+ * @returns {string}
+ */
+function repeater(str, n) {
+    return str.repeat(n);
+}
