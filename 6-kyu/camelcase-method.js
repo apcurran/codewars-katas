eb5fafe765e4e62111b4c2aeee53b0bbe4576836
Alex Curran <alexcurran.va@gmail.com>
2022-06-14 11:51:59 -0500

    Working solution for camelcase-method.js

---
+++ 6-kyu/camelcase-method.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(n + m)
+ * 
+ * @returns {string}
+ */
+String.prototype.camelCase = function () {
+    if (this == "") {
+        return "";
+    } else {
+        return this
+                .trim()
+                .split(" ")
+                .map((word) => word[0].toUpperCase() + word.slice(1))
+                .join("");
+    }
+};
+
+console.log( "hello case".camelCase() ); // "HelloCase"
+console.log( " camel case word".camelCase() ); // "CamelCaseWord"
+console.log( "".camelCase() ); // ""
