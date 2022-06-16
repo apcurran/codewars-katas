a63c831b43650ee0f39b982621a048d1da379d97
Alex Curran <alexcurran.va@gmail.com>
2022-06-16 09:26:47 -0500

    Working solution for remove-all-marked-elems-of-list.js

---
+++ 7-kyu/remove-all-marked-elems-of-list.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = integerList len
+ * m = valuesList len
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {number[]} integerList 
+ * @param {number[]} valuesList 
+ * @returns {number[]}
+ */
+Array.prototype.remove_ = function(integerList, valuesList) {
+    return integerList.filter((num) => !valuesList.includes(num));
+};
