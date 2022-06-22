05e468dc8e77215782a429209eaea51fc7601ad6
Alex Curran <alexcurran.va@gmail.com>
2022-06-22 11:46:58 -0500

    Working solution for pair-zeros.js

---
+++ 7-kyu/pair-zeros.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function pairZeros(arr) {
+    let prevZero = true;
+
+    return arr.filter((num) => {
+        if (num === 0) {
+            prevZero = !prevZero;
+
+            return !prevZero;
+        }
+
+        return true;
+    });
+}
+
+console.log( pairZeros([1, 0, 1, 0, 2, 0, 0, 3, 0]) ); // [1,0,1,2,0,3,0]
