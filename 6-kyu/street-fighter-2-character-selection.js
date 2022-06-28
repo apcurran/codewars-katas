d80103dc2e08aeacc6f410a2c847b0a8cc87c069
Alex Curran <alexcurran.va@gmail.com>
2022-06-28 09:45:26 -0500

    Working solution for street-fighter-2-character-selection.js

---
+++ 6-kyu/street-fighter-2-character-selection.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = moves len
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[][]} fighters 
+ * @param {number[]} startingPos 
+ * @param {string[]} moves 
+ * @returns {string[]}
+ */
+function streetFighterSelection(fighters, startingPos, moves) {
+    let [currPosY, currPosX] = startingPos;
+    let selectedCharacters = [];
+
+    for (let move of moves) {
+        if (move === "up") {
+            currPosY = 0;
+        } else if (move === "down") {
+            currPosY = 1;
+        } else if (move === "right") {
+            const newPos = currPosX === 5 ? 0 : currPosX + 1;
+            currPosX = newPos;
+        } else if (move === "left") {
+            const newPos = currPosX === 0 ? 5 : currPosX - 1;
+            currPosX = newPos;
+        }
+
+        const characterHovered = fighters[currPosY][currPosX];
+        selectedCharacters.push(characterHovered);
+    }
+
+    return selectedCharacters;
+}
+
+const fighters = [
+	["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+	["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+];
+const startingPosition = [0, 0];
+const moves = ["up", "left", "right", "left", "left"];
+
+console.log( streetFighterSelection(fighters, startingPosition, moves) ); // ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
