6b155c03d9dfff12266a93c6a0e7d47ab055f191
Alex Curran <alexcurran.va@gmail.com>
2022-07-05 09:53:27 -0500

    Alternate functional-style solution

--- 7-kyu/odd-even-str-sort.js
+++ 7-kyu/odd-even-str-sort.js
@@ -1,7 +1,37 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} str 
+//  * @returns {string}
+//  */
+// function sortMyString(str) {
+//     let strMap = new Map();
+
+//     for (let i = 0; i < str.length; i++) {
+//         const currLetter = str[i];
+//         strMap.set(i, currLetter);
+//     }
+
+//     let evenStrGroup = "";
+//     let oddStrGroup = "";
+
+//     for (let [index, letter] of strMap) {
+//         if (index % 2 === 0) {
+//             evenStrGroup += letter;
+//         } else {
+//             oddStrGroup += letter;
+//         }
+//     }
+
+//     return `${evenStrGroup} ${oddStrGroup}`;
+// }
+
 /**
- * solution 1
+ * solution 2
  * time: O(n)
  * space: O(n)
  * 
@@ -9,25 +39,15 @@
  * @returns {string}
  */
 function sortMyString(str) {
-    let strMap = new Map();
+    const letterArr = str.split("");
+    const evenGroup = letterArr
+                        .filter((letter, index) => index % 2 === 0)
+                        .join("");
+    const oddGroup = letterArr
+                        .filter((letter, index) => index % 2 !== 0)
+                        .join("");
 
-    for (let i = 0; i < str.length; i++) {
-        const currLetter = str[i];
-        strMap.set(i, currLetter);
-    }
-
-    let evenStrGroup = "";
-    let oddStrGroup = "";
-
-    for (let [index, letter] of strMap) {
-        if (index % 2 === 0) {
-            evenStrGroup += letter;
-        } else {
-            oddStrGroup += letter;
-        }
-    }
-
-    return `${evenStrGroup} ${oddStrGroup}`;
+    return `${evenGroup} ${oddGroup}`;
 }
 
 console.log( sortMyString("CodeWars") ); // "CdWr oeas"
