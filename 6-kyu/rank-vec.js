dadca4f1da5829518f154afe2e2ad751af01fc4a
Alex Curran <alexcurran.va@gmail.com>
2022-07-14 10:26:02 -0500

    Working solution for rank-vec.js

---
+++ 6-kyu/rank-vec.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+function ranks(arr) {
+   const sortedArr = [...arr].sort((a, b) => b - a);
+
+   return arr.map((val) => sortedArr.indexOf(val) + 1);
+}
+
+console.log( ranks([9,3,6,10]) ); // [2,4,3,1]
