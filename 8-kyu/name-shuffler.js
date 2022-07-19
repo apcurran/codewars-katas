0c1a522b62aeb8962de1a29de79f1506887ee138
Alex Curran <alexcurran.va@gmail.com>
2022-07-19 12:16:33 -0500

    Working solution for name-shuffler.js

---
+++ 8-kyu/name-shuffler.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1) -- technically will always be a split arr of len 2 for name
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function nameShuffler(str) {
+    const [firstName, lastName] = str.split(" ");
+
+    return `${lastName} ${firstName}`;
+}
+
+console.log( nameShuffler("john McClane") ); // "McClane john"
