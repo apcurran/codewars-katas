0bad20d2528cc6caa53513eddd4a93e70b2f3c98
Alex Curran <alexcurran.va@gmail.com>
2022-07-21 16:25:28 -0500

    Working solution for rev-list.js

---
+++ 7-kyu/rev-list.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- in-place reversal of arr
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function reverseList(arr) {
+    return arr.reverse();
+}
