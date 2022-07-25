8ccbfc003f5cac0b0fc2f4bc8541cc12ec9914a4
Alex Curran <alexcurran.va@gmail.com>
2022-07-25 10:26:52 -0500

    Working solution for coding-meetup-4-find-the-first-py-dev.js

---
+++ 7-kyu/coding-meetup-4-find-the-first-py-dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {object[]} list 
+ * @returns {string}
+ */
+function getFirstPython(list) {
+    const pyDevObj = list.find((dev) => dev.language === "Python");
+
+    if (!pyDevObj) return "There will be no Python developers";
+
+    return `${pyDevObj.firstName}, ${pyDevObj.country}`;
+}
+
+const list1 = [
+    { firstName: "Mark", lastName: "G.", country: "Scotland", continent: "Europe", age: 22, language: "JavaScript" },
+    { firstName: "Victoria", lastName: "T.", country: "Puerto Rico", continent: "Americas", age: 30, language: "Python" },
+    { firstName: "Emma", lastName: "B.", country: "Norway", continent: "Europe", age: 19, language: "Clojure" }
+];
+console.log( getFirstPython(list1) ); // "Victoria, Puerto Rico"
