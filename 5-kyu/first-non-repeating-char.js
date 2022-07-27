9b678d740bf8bb0636c81b8c0d8df24fc6413315
Alex Curran <alexcurran.va@gmail.com>
2022-07-27 10:57:42 -0500

    Working solution for first-non-repeating-char.js

---
+++ 5-kyu/first-non-repeating-char.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @returns {string}
+ */
+function firstNonRepeatingLetter(str) {
+    const nonRepeatChar = str
+                            .split("")
+                            .find((char) => {
+                                const myRegex = new RegExp(char, "gi");
+
+                                return str.match(myRegex).length === 1;
+                            });
+    
+    return nonRepeatChar ? nonRepeatChar : "";
+}
+
+console.log( firstNonRepeatingLetter("stress") ); // "t"
+console.log( firstNonRepeatingLetter("moonmen") ); // "e"
+console.log( firstNonRepeatingLetter("sTreSS") ); // "T"
