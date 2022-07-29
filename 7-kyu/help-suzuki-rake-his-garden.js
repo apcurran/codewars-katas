8bc9dd3c495181e9835d0fd28e36ed42be825475
Alex Curran <alexcurran.va@gmail.com>
2022-07-29 11:43:53 -0500

    Working solution for help-suzuki-rake-his-garden.js

---
+++ 7-kyu/help-suzuki-rake-his-garden.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// /**
+//  * solution 1 -- map method
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} garden
+//  * @returns {string}
+//  */
+// function rakeGarden(garden) {
+//     return garden
+//         .split(" ")
+//         .map((word) => {
+//             if (word === "rock") return word;
+
+//             return "gravel";
+//         })
+//         .join(" ");
+// }
+
+/**
+ * solution 2 -- string.replace() method
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} garden
+ * @returns {string}
+ */
+function rakeGarden(garden) {
+    return garden.replace(/\w+/g, (word) => {
+        if (word === "rock") return word;
+
+        return "gravel";
+    });
+}
+
+console.log(rakeGarden("slug spider rock gravel gravel gravel gravel gravel gravel gravel")); // gravel gravel rock gravel gravel gravel gravel gravel gravel gravel
