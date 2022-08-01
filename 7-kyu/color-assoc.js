6c9056dd14bb5cf07ea2bcebf3cab759f3e71b3e
Alex Curran <alexcurran.va@gmail.com>
2022-08-01 10:38:36 -0500

    Working solution for color-assoc.js

---
+++ 7-kyu/color-assoc.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[][]} colorsArr 
+ * @returns {object[]}
+ */
+function colourAssociation(colorsArr) {
+    return colorsArr.map((colorTuple) => {
+        const [color, assoc] = colorTuple;
+
+        return {
+            [color]: assoc
+        };
+    });
+}
+
+console.log( colourAssociation([["red", "energy"],["yellow", "creativity"],["brown" , "friendly"],["green", "growth"]]) );
+// [{red: "energy"}, {yellow: "creativity"}, {brown: "friendly"}, {green: "growth"}]
