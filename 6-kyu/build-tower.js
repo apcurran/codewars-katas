15e467b3ff2dcd8fbcf31d5d71f009cb205feb1d
Alex Curran <alexcurran.va@gmail.com>
2022-08-05 14:24:38 -0500

    Working solution for build-tower.js

---
+++ 6-kyu/build-tower.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2) - string is an iterable created inside of a loop
+ * space: O(n^2) - string is an iterable created inside of a loop
+ * 
+ * @param {number} nFloors 
+ * @returns {string[]}
+ */
+function towerBuilder(nFloors) {
+    const totalCharsPerLevel = (nFloors * 2) - 1;
+    let finishedTower = [];
+    // iterate nFloors times
+
+    for (let i = 1; i <= nFloors; i++) {
+        // for each floor, create level
+        const levelStarsCt = (i * 2) - 1;
+        const levelStarsStr = "*".repeat(levelStarsCt);
+        const spacesCt = (totalCharsPerLevel - levelStarsCt) / 2;
+        const spacesStr = " ".repeat(spacesCt);
+        const levelStr = `${spacesStr}${levelStarsStr}${spacesStr}`;
+        finishedTower.push(levelStr);
+    }
+
+    return finishedTower;
+}
+
+console.log(towerBuilder(3));
+// [
+//     "  *  ",
+//     " *** ",
+//     "*****"
+// ]
