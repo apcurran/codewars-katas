73563b53c772db41ea5ff131aee81d89bae88436
Alex Curran <alexcurran.va@gmail.com>
2022-08-09 09:35:15 -0500

    Working solution for 99-problems-last-in-list.js

---
+++ 7-kyu/99-problems-last-in-list.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @returns {number|null}
+ */
+function last(arr) {
+    if (arr.length === 0) return null;
+
+    return arr[arr.length - 1];
+}
+
+console.log( last([1, 2, 3]) ); // 3
+console.log( last([]) ); // null
