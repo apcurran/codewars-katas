1e52db93c6950a35532ef5e4b60cd0d9f8634ae4
Alex Curran <alexcurran.va@gmail.com>
2022-08-15 10:01:26 -0500

    Working solution for arrh-grabscrab.js

---
+++ 6-kyu/arrh-grabscrab.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2 * log n)
+ * space: O(n^2)
+ * 
+ * @param {string} anagram 
+ * @param {string[]} arr 
+ * @returns {string[]}
+ */
+function grabscrab(anagram, arr) {
+    const sortedAnagram = anagram
+                            .split("")
+                            .sort((a, b) => {
+                                if (a < b) return -1;
+
+                                if (a > b) return 1;
+
+                                return 0;
+                            })
+                            .join("");
+    // iterate arr
+    return arr.filter((word) => {
+        // check that all letters are in the arr val
+        // if yes, add to return resArr
+        // if no, skip
+        const sortedWord = word
+                            .split("")
+                            .sort((a, b) => {
+                                if (a < b) return -1;
+
+                                if (a > b) return 1;
+
+                                return 0;
+                            })
+                            .join("");
+
+        if (sortedWord === sortedAnagram) return word;
+    });
+}
+
+console.log( grabscrab("ortsp", ["sport", "parrot", "ports", "matey"]) ); // ["sport", "ports"]
