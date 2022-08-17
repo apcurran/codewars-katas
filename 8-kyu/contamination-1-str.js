ec05cc44f619946238a7da0a54d351bb934505f3
Alex Curran <alexcurran.va@gmail.com>
2022-08-17 09:06:00 -0500

    Working solutions for contamination-1-str.js

---
+++ 8-kyu/contamination-1-str.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} text 
+//  * @param {string} replacementChar 
+//  * @returns {string}
+//  */
+// function contamination(text, replacementChar) {
+//     if (text === "" || replacementChar === "") return "";
+
+//     return text
+//             .split("")
+//             .map(() => replacementChar)
+//             .join("");
+// }
+
+/**
+ * solution 2
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} text 
+ * @param {string} replacementChar 
+ * @returns {string}
+ */
+function contamination(text, replacementChar) {
+    return replacementChar.repeat(text.length);
+}
+
+console.log( contamination("abc", "z") ); // "zzz"
+console.log( contamination("//case", " ") ); // "      "
