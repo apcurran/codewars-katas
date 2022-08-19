8205ebfac2ce98c1d6814ed46ffd4209db380aac
Alex Curran <alexcurran.va@gmail.com>
2022-08-19 09:31:12 -0500

    Working solution for  mult-word-in-str.js

---
+++ 7-kyu/mult-word-in-str.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = word arr len
+ * m = num
+ * time: O(n + m)
+ * space: O(n + m)
+ * 
+ * @param {string} str 
+ * @param {number} loc 
+ * @param {number} num 
+ * @returns {string}
+ */
+function modifyMultiply(str, loc, num) {
+    const wordArr = str.split(" ");
+    const targetWord = wordArr[loc];
+    let newWordArr = [];
+
+    for (let i = 0; i < num; i++) {
+        newWordArr.push(targetWord);
+    }
+
+    return newWordArr.join("-");
+}
+
+console.log( modifyMultiply("This is a string", 3, 5) ); // "string-string-string-string-string"
