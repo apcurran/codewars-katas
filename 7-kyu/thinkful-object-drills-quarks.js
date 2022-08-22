991f4926ada2498ec97fce8c57d47550604245a4
Alex Curran <alexcurran.va@gmail.com>
2022-08-22 10:23:41 -0500

    Working solution for thinkful-object-drills-quarks.js

---
+++ 7-kyu/thinkful-object-drills-quarks.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class Quark {
+    /**
+     * @param {string} color 
+     * @param {string} flavor 
+     */
+    constructor(color, flavor) {
+        this.color = color;
+        this.flavor = flavor;
+        this.baryon_number = 1 / 3;
+    }
+
+    /**
+     * exchange colors between objects
+     * @param {Quark} qrk particle object
+     */
+    interact(qrk) {
+        const tempColor = this.color;
+        this.color = qrk.color;
+        qrk.color = tempColor;
+    }
+}
+
+const q1 = new Quark("red", "up");
+const q2 = new Quark("blue", "strange");
+q1.interact(q2);
+console.log(q1.color);
+console.log(q2.color);
+
