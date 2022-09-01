01c1b2a2f970ebe816bf7bde20afbd3c2bacd26e
Alex Curran <alexcurran.va@gmail.com>
2022-09-01 09:40:30 -0500

    Working solution for smallest-val-of-an-arr.js

---
+++ 7-kyu/smallest-val-of-an-arr.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @param {string} toReturn 
+ * @returns {number}
+ */
+function min(arr, toReturn) {
+    const smallestNum = Math.min(...arr);
+
+    if (toReturn === "value") return smallestNum;
+
+    // get smallest index
+    return arr.indexOf(smallestNum);
+}
+
+console.log( min([1,2,3,4,5], "value") ); // 1
+console.log( min([1,2,3,4,5], "index") ); // 0
