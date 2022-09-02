4830fded78ab2da7b67bee5b28dbb3283fb4a6f4
Alex Curran <alexcurran.va@gmail.com>
2022-09-02 09:11:50 -0500

    Working solution for geometric-prog-seq.js

---
+++ 7-kyu/geometric-prog-seq.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} a 
+ * @param {number} r 
+ * @param {number} n 
+ * @returns {string}
+ */
+function geometricSequenceElements(a, r, n) {
+    // formula -> val = a * r^(n - 1)
+    let resStrArr = [];
+
+    for (let i = 1; i <= n; i++) {
+        const val = a * r ** (i - 1);
+        resStrArr.push(val);
+    }
+
+    return resStrArr.join(", ");
+}
+
+console.log( geometricSequenceElements(2, 3, 5) ); // '2, 6, 18, 54, 162'
