0e1c8467a9b803f52156e6d6e76aff1102be8022
Alex Curran <alexcurran.va@gmail.com>
2022-09-05 11:22:55 -0500

    Working solution for count-odd-nums-below-n.js

---
+++ 8-kyu/count-odd-nums-below-n.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * solution 1 -- math
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {number}
+ */
+function oddCount(n) {
+    return Math.floor(n / 2);
+}
