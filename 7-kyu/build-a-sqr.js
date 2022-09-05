a7f420f6b22f108cd001fdbf4ee52386df17274c
Alex Curran <alexcurran.va@gmail.com>
2022-09-05 11:35:28 -0500

    Working solution for build-a-sqr.js

---
+++ 7-kyu/build-a-sqr.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n^2)
+ * 
+ * @param {number} num 
+ * @returns {string}
+ */
+function generateShape(num) {
+    let resStr = "";
+
+    for (let i = 0; i < num; i++) {
+        const rowStr = "+".repeat(num);
+        resStr += rowStr + "\n";
+    }
+
+    return resStr.trim();
+}
+
+// /**
+//  * solution 2
+//  * time: O(n^2)
+//  * space: O(n^2)
+//  *
+//  * @param {number} num 
+//  * @returns {string}
+//  */
+// function generateShape(num) {
+//     let resStr = "";
+
+//     for (let i = 0; i < num; i++) {
+//         let rowStr = "";
+
+//         for (let i = 0; i < num; i++) {
+//             rowStr += "+";
+//         }
+
+//         const fullRowStr = i === num - 1 ? rowStr : rowStr + "\n";
+
+//         resStr += fullRowStr;
+//     }
+
+//     return resStr;
+// }
+
+console.log( generateShape(3) );
