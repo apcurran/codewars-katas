883b6180b4a6d4a12fa076d7794e333eedb2613d
Alex Curran <alexcurran.va@gmail.com>
2022-09-19 10:00:15 -0500

    O(n) time and O(1) solution for array-proto-rev.js

---
+++ 7-kyu/array-proto-rev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- two-pointer technique
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @returns {number[]}
+ */
+Array.prototype.reverse = function () {
+    let leftPointer = 0;
+    let rightPointer = this.length - 1;
+
+    while (leftPointer < rightPointer) {
+        const leftNum = this[leftPointer];
+        const rightNum = this[rightPointer];
+
+        // swap nums at left and right sides
+        this[leftPointer] = rightNum;
+        this[rightPointer] = leftNum;
+        // move pointers
+        leftPointer++;
+        rightPointer--;
+    }
+
+    return this;
+};
+
+console.log( [1, 2, 3, 4].reverse() ); // [4,3,2,1]
