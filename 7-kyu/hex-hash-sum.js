79f191f27857d4ab30f5db1fec619db143cd7a50
Alex Curran <alexcurran.va@gmail.com>
2022-09-22 09:17:57 -0500

    Working solution for 7-kyu/hex-hash-sum.js

---
+++ 7-kyu/hex-hash-sum.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = str len
+ * m = char hex len
+ * time: O(n + m)
+ * space: O(m)
+ * 
+ * @param {string} str 
+ * @returns {number} sum of the nums in the converted hex strings 
+ */
+function hexHash(str) {
+    let hexSum = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const charHex = str[i]
+                            .charCodeAt(0)
+                            .toString(16);
+        const charHexArr = charHex.split("");
+
+        for (let val of charHexArr) {
+            const currHexNum = Number(val);
+
+            if (!Number.isNaN(currHexNum)) {
+                hexSum += currHexNum;
+            }
+        }
+    }
+
+    return hexSum;
+}
+
+console.log( hexHash("Yo") ); // 20
+console.log( hexHash("Hello, World!") ); // 91
