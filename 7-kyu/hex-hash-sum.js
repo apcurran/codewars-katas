b988f49340f0f1994735c01a8b35c1c142d7610f
Alex Curran <alexcurran.va@gmail.com>
2022-09-22 09:27:22 -0500

    Slight alternative solution

--- 7-kyu/hex-hash-sum.js
+++ 7-kyu/hex-hash-sum.js
@@ -1,7 +1,38 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = str len
+//  * m = char hex len
+//  * time: O(n + m)
+//  * space: O(m)
+//  * 
+//  * @param {string} str 
+//  * @returns {number} sum of the nums in the converted hex strings 
+//  */
+// function hexHash(str) {
+//     let hexSum = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+//         const charHex = str[i]
+//                             .charCodeAt(0)
+//                             .toString(16);
+//         const charHexArr = charHex.split("");
+
+//         for (let val of charHexArr) {
+//             const currHexNum = Number(val);
+
+//             if (!Number.isNaN(currHexNum)) {
+//                 hexSum += currHexNum;
+//             }
+//         }
+//     }
+
+//     return hexSum;
+// }
+
 /**
- * solution 1
+ * solution 2 -- slight alternative
  * n = str len
  * m = char hex len
  * time: O(n + m)
@@ -17,15 +48,11 @@ function hexHash(str) {
         const charHex = str[i]
                             .charCodeAt(0)
                             .toString(16);
-        const charHexArr = charHex.split("");
-
-        for (let val of charHexArr) {
-            const currHexNum = Number(val);
-
-            if (!Number.isNaN(currHexNum)) {
-                hexSum += currHexNum;
-            }
-        }
+        const charHexNumsArr = charHex.replace(/\D/g, ""); // remove non-digits
+        const charHexArrSum = charHexNumsArr
+                                .split("")
+                                .reduce((sum, curr) => sum + Number(curr), 0);
+        hexSum += charHexArrSum;
     }
 
     return hexSum;
