062ed4d31e5fc5f504db413a1ce43d252050df86
Alex Curran <alexcurran.va@gmail.com>
2022-10-04 10:09:37 -0500

    Optimize solution 2 to be constant space

--- 7-kyu/determine-if-poker-hand-is-flush.js
+++ 7-kyu/determine-if-poker-hand-is-flush.js
@@ -1,18 +1,35 @@
 "use strict";
 
+// /**
+//  * solution 1 -- Set obj
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string[]} cards 
+//  * @returns {boolean}
+//  */
+// function isFlush(cards) {
+//     const cardSuits = cards.map((card) => card[card.length - 1]);
+//     const cardSuitsNoDups = new Set(cardSuits);
+    
+//     return cardSuitsNoDups.size === 1;
+// }
+
 /**
- * solution 1 -- Set obj
+ * solution 2 -- array.every() method
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {string[]} cards 
  * @returns {boolean}
  */
 function isFlush(cards) {
-    const cardSuits = cards.map((card) => card[card.length - 1]);
-    const cardSuitsNoDups = new Set(cardSuits);
-    
-    return cardSuitsNoDups.size === 1;
+    return cards.every((card, index, arr) => {
+        const currCardSuit = card[card.length - 1];
+        const firstCardSuit = arr[0][arr[0].length - 1];
+
+        return currCardSuit === firstCardSuit;
+    });
 }
 
 console.log( isFlush(["AS", "3S", "9S", "KS", "4S"]) ); // true
