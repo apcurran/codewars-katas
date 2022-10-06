1b8dcaf30eda788b263a583fe4ec0a13c3170deb
Alex Curran <alexcurran.va@gmail.com>
2022-10-06 09:26:09 -0500

    Working solution for tea-for-two.js

---
+++ 7-kyu/tea-for-two.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number|string} str 
+ * @returns {string}
+ */
+function tea42(str) {
+    return String(str).replace(/2/g, "t");
+}
+
+console.log( tea42("coffee") ); // "coffee"
+console.log( tea42("2ea") ); // "tea"
