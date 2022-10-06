a4651acea42a6c699b83bf7decb458ae09e5ba48
Alex Curran <alexcurran.va@gmail.com>
2022-10-06 09:12:45 -0500

    Working solution for especially-joyful-nums.js

---
+++ 7-kyu/especially-joyful-nums.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1 -- functional
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @returns {boolean}
+ */
+function numberJoy(n) {
+    const digitSum = String(n)
+                        .split("")
+                        .reduce((sum, curr) => sum + Number(curr), 0);
+    const reversedSum = +String(digitSum)
+                            .split("")
+                            .reverse()
+                            .join("");
+    const digitSumRevSumProd = digitSum * reversedSum;
+
+    return n === digitSumRevSumProd;
+}
+
+console.log( numberJoy(1729) ); // true
+console.log( numberJoy(1997) ); // false
