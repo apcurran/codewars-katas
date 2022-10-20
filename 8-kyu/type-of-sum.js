6c0ae1d1553beb3827579daf92003a32d2639268
Alex Curran <alexcurran.va@gmail.com>
2022-10-20 13:17:05 -0500

    Working solution for type-of-sum.js

---
+++ 8-kyu/type-of-sum.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {any} a 
+ * @param {any} b 
+ * @returns {string}
+ */
+function typeOfSum(a, b) {
+    return typeof (a + b);
+}
