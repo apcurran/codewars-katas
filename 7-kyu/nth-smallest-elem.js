c46d6049056baa8ae652fb8f4697c5e49dd7ee0c
Alex Curran <alexcurran.va@gmail.com>
2022-10-26 16:46:04 -0500

    Working solution for nth-smallest-elem.js

---
+++ 7-kyu/nth-smallest-elem.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @param {number} pos 
+ * @returns {number}
+ */
+function nthSmallest(arr, pos) {
+    // sort list ASC
+    arr.sort((a, b) => a - b);
+
+    // index to arr[pos - 1] since it's 0-indexed
+    return arr[pos - 1];
+}
+
+console.log( nthSmallest([3,1,2], 2) ); // 2
+console.log( nthSmallest([15,20,7,10,4,3], 3) ); // 7
+console.log( nthSmallest([3,1,2], 2) ); // 2
