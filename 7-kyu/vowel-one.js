7b1da83bc2b867cc56d9d82ce5de8c055839f69a
Alex Curran <alexcurran.va@gmail.com>
2022-11-02 09:32:03 -0500

    Working solutions for vowel-one.js

---
+++ 7-kyu/vowel-one.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// /**
+//  * solution 1 -- regex
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} s 
+//  * @returns {string}
+//  */
+// function vowelOne(s) {
+//     return s
+//             .replace(/[^aeiou]/gi, "0")
+//             .replace(/[^\d]/g, "1");
+// }
+
+/**
+ * solution 2
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s 
+ * @returns {string}
+ */
+function vowelOne(s) {
+    const lowerS = s.toLowerCase();
+    let resStr = "";
+
+    for (let char of lowerS) {
+        if (char === "a" ||
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u") {
+            resStr += "1";
+        } else {
+            resStr += "0";
+        }
+    }
+
+    return resStr;
+}
+
+console.log(vowelOne("abceios")); // "1001110"
+console.log(vowelOne("aeiou, abc")); // "1111100100"
