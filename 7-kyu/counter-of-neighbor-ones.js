d3a65d68bd4944c6945e3aff4f4a7367b1a70e60
Alex Curran <alexcurran.va@gmail.com>
2022-11-03 10:02:56 -0500

    Working solution for counter-of-neighbor-ones.js

---
+++ 7-kyu/counter-of-neighbor-ones.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} input
+ * @returns {number[]}
+ */
+function onesCounter(input) {
+    let consecOnes = 0;
+    let resArr = [];
+
+    for (let num of input) {
+        if (num !== 1) {
+            if (consecOnes !== 0) {
+                resArr.push(consecOnes);
+            }
+            // reset the ones counter if the curr num is not 1
+            consecOnes = 0;
+
+            continue;
+        }
+
+        consecOnes++;
+    }
+
+    if (consecOnes !== 0) {
+        resArr.push(consecOnes);
+    }
+
+    return resArr;
+}
+
+console.log( onesCounter([1, 1, 1, 0, 1]) ); // [3, 1]
+console.log( onesCounter([0, 0, 0, 0, 0, 0, 0, 0]) ); // []
