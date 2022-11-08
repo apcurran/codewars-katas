de6fd585f22b177cfb633173e72484f8f921fa19
Alex Curran <alexcurran.va@gmail.com>
2022-11-08 09:54:15 -0600

    Working solution for to-sqr-or-not-to-sqrt.js

---
+++ 8-kyu/to-sqr-or-not-to-sqrt.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function squareOrSquareRoot(arr) {
+    return arr.map((num) => {
+        const sqrt = Math.sqrt(num);
+        const hasSqrt = Number.isInteger(sqrt);
+
+        if (hasSqrt) return sqrt;
+
+        return num * num;
+    }); 
+}
+
+console.log( squareOrSquareRoot([4,3,9,7,2,1]) ); // [2,9,3,49,4,1]
