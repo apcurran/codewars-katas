fd2c5e4f242d14295158846459f4da688d95089b
Alex Curran <alexcurran.va@gmail.com>
2022-11-10 09:08:13 -0600

    Working solution for make-acronym.js

---
+++ 7-kyu/make-acronym.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} text 
+ * @returns {string}
+ */
+function toAcronym(text) {
+    return text
+            .split(" ")
+            .map((word) => word[0].toUpperCase())
+            .join("");
+}
+
+console.log( toAcronym("Code Wars") ); // "CW"
+console.log( toAcronym("Portable Network Graphics") ); // "PNG"
+console.log( toAcronym("PHP: Hypertext Preprocessor") ); // "PHP"
