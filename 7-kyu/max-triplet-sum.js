26cc66ffc9b7878d3e211e2be1b85f9c4b3810ea
Alex Curran <alexcurran.va@gmail.com>
2022-11-11 09:26:36 -0600

    Working solution for max-triplet-sum.js

---
+++ 7-kyu/max-triplet-sum.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n) -- due to sorting arr
+ * space: O(n)
+ * 
+ * @param {number[]} numbers 
+ * @returns {number}
+ */
+function maxTriSum(numbers) {
+    const sortedNumsNoDups = [...new Set(numbers)].sort((a, b) => a - b);
+    const largestThreeNums = sortedNumsNoDups.slice(-3);
+
+    return largestThreeNums.reduce((sum, currNum) => sum + currNum, 0);
+}
+
+console.log( maxTriSum([3,2,6,8,2,3]) ); // 17
+console.log( maxTriSum([-14,-12,-7,-42,-809,-14,-12]) ); // -33
