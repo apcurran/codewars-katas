edf9175d845c275d35b38c7ec8187321c3ea5223
Alex Curran <alexcurran.va@gmail.com>
2022-11-11 09:44:54 -0600

    Working solution for hells-kitchen.js

---
+++ 7-kyu/hells-kitchen.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function gordon(str){
+    return str
+            .toUpperCase()
+            .replace(/[AEIOU]/g, (letter) => letter === "A" ? "@" : "*")
+            .split(" ")
+            .map((word) => `${word}!!!!`)
+            .join(" ");
+}
+
+console.log( gordon("i am a chef") ); // '*!!!! @M!!!! @!!!! CH*F!!!!'
