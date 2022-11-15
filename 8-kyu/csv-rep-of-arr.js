18775ab9aeae26f7113ee142a671b9b645e1bdf7
Alex Curran <alexcurran.va@gmail.com>
2022-11-15 09:22:58 -0600

    Working solution for csv-rep-of-arr.js

---
+++ 8-kyu/csv-rep-of-arr.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m) 
+ * space: O(n + m)
+ * 
+ * @param {number[][]} arr 
+ * @returns {string}
+ */
+function toCsvText(arr) {
+    let res = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        const currArr = arr[i];
+        const numStr = currArr.join(",");
+
+        res += numStr;
+
+        if (i === arr.length - 1) continue;
+
+        res += "\n";
+    }
+
+    return res;
+}
+
+console.log( toCsvText(
+    [
+        [0, 1, 2, 3, 45],
+        [10, 11, 12, 13, 14],
+        [20, 21, 22, 23, 24],
+        [30, 31, 32, 33, 34]
+    ]
+) );
