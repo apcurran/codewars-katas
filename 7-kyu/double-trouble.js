4cc6139a92734e0ad5ae73eff49f9a551dc5e94f
Alex Curran <alexcurran.va@gmail.com>
2022-11-17 09:19:56 -0600

    Working solution for double-trouble.js

---
+++ 7-kyu/double-trouble.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2) -- splice causes O(n) shifting of elems in the front/middle of arr
+ * space: O(1)
+ * 
+ * @param {number[]} x 
+ * @param {number} t 
+ * @returns {number[]}
+ */
+function trouble(x, t) {
+    // iterate arr
+    for (let i = 0; i < x.length - 1; i++) {
+        const currNum = x[i];
+        const nextNum = x[i + 1];
+        // check if currNum and nextNum sum to target t
+        const sum = currNum + nextNum;
+        if (sum === t) {
+            x.splice(i + 1, 1);
+            i--;
+        }
+    }
+
+    return x;
+}
+
+console.log( trouble([1, 2, 3, 4, 5], 3) ); // [1, 3, 4, 5]
+console.log( trouble([4, 1, 1, 1, 4],2) ); // [4, 1, 4]
+console.log( trouble([1, 2, 3, 4, 5], 3) ); // [1, 3, 4, 5]
