711be2b8a10c852da50612800481b934a1e51e77
Alex Curran <alexcurran.va@gmail.com>
2022-11-17 09:27:22 -0600

    Working alt solution

--- 7-kyu/double-trouble.js
+++ 7-kyu/double-trouble.js
@@ -1,28 +1,55 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2) -- splice causes O(n) shifting of elems in the front/middle of arr
+//  * space: O(1)
+//  * 
+//  * @param {number[]} x 
+//  * @param {number} t 
+//  * @returns {number[]}
+//  */
+// function trouble(x, t) {
+//     // iterate arr
+//     for (let i = 0; i < x.length - 1; i++) {
+//         const currNum = x[i];
+//         const nextNum = x[i + 1];
+//         // check if currNum and nextNum sum to target t
+//         const sum = currNum + nextNum;
+//
+//         if (sum === t) {
+//             x.splice(i + 1, 1);
+//             i--;
+//         }
+//     }
+
+//     return x;
+// }
+
 /**
- * solution 1
- * time: O(n^2) -- splice causes O(n) shifting of elems in the front/middle of arr
- * space: O(1)
+ * solution 2
+ * time: O(n) -- no splicing of arr
+ * space: O(n)
  * 
  * @param {number[]} x 
  * @param {number} t 
  * @returns {number[]}
  */
 function trouble(x, t) {
+    let res = [x[0]];
     // iterate arr
     for (let i = 0; i < x.length - 1; i++) {
         const currNum = x[i];
         const nextNum = x[i + 1];
         // check if currNum and nextNum sum to target t
         const sum = currNum + nextNum;
-        if (sum === t) {
-            x.splice(i + 1, 1);
-            i--;
+
+        if (sum !== t) {
+            res.push(nextNum);
         }
     }
 
-    return x;
+    return res;
 }
 
 console.log( trouble([1, 2, 3, 4, 5], 3) ); // [1, 3, 4, 5]
