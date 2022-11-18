753dbb3fc5bf05c905982abd1a7d6872bc03340e
Alex Curran <alexcurran.va@gmail.com>
2022-11-18 09:35:22 -0600

    Loop-free solution for find-the-nth-digit-of-a-num.js

---
+++ 7-kyu/find-the-nth-digit-of-a-num.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} num 
+ * @param {number} nth 
+ * @returns {number}
+ */
+function findDigit(num, nth) {
+    if (nth <= 0) return -1;
+
+    const posNum = Math.abs(num);
+    const numStr = String(posNum);
+    const val = numStr[numStr.length - nth];
+    
+    if (val === undefined) return 0;
+
+    return Number(val);
+}
+
+console.log( findDigit(5673, 4) ); // 5
+console.log( findDigit(-2825, 3) ); // 8
+console.log( findDigit(65, 0) ); // -1
