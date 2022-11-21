b1204d13903185b7f642bed36f3713093f81fd8e
Alex Curran <alexcurran.va@gmail.com>
2022-11-21 09:13:28 -0600

    Working solution for enumerable-magic-does-my-list-include-this.js

---
+++ 8-kyu/enumerable-magic-does-my-list-include-this.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @param {number} item 
+ * @returns {boolean}
+ */
+function include(arr, item) {
+    return arr.includes(item);
+}
+
+console.log( include([1,2,3,4], 3) ); // true
+console.log( include([1,2,4,5], 3) ); // false
