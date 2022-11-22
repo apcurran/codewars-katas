e693b64681bfeb4166ff5036194a8ab1298fa61b
Alex Curran <alexcurran.va@gmail.com>
2022-11-22 09:51:36 -0600

    Working solution for how-many-consec-nums-are-needed.js

---
+++ 7-kyu/how-many-consec-nums-are-needed.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number} needed nums count
+ */
+function consecutive(arr) {
+    const numsSet = new Set(arr);
+    const minVal = Math.min(...arr);
+    const maxVal = Math.max(...arr);
+    let neededNumsCount = 0;
+
+    for (let i = minVal + 1; i < maxVal; i++) {
+        if (!numsSet.has(i)) neededNumsCount++;
+    }
+
+    return neededNumsCount;
+}
+
+console.log( consecutive([4,8,6]) ); // 2
+console.log( consecutive([1,2,3,4]) ); // 0
