2433218233ab976e4e32a0b8c64f0d0086939556
Alex Curran <alexcurran.va@gmail.com>
2022-11-22 09:41:25 -0600

    Working solution for unscrambled-eggs.js

---
+++ 7-kyu/unscrambled-eggs.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} word 
+ * @returns {string}
+ */
+function unscrambleEggs(word) {
+    return word.replace(/egg/gi, "");
+}
+
+console.log( unscrambleEggs("ceggodegge heggeregge") ); // "code here"
