5ec4a95ea00e87df9e4c25d1525f7194189d9bd6
Alex Curran <alexcurran.va@gmail.com>
2022-12-09 15:48:15 -0600

    Working solution for perfect-sqs-perfect-fun.js

---
+++ 7-kyu/perfect-sqs-perfect-fun.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n + sqrt(n))
+ * 
+ * @param {number} int 
+ * @returns {string}
+ */
+function squareIt(int) {
+    const strInt = String(int);
+    const intLen = strInt.length;
+    const squareRootLen = Math.sqrt(intLen);
+
+    if (!Number.isInteger(squareRootLen)) return "Not a perfect square!";
+
+    let res = "";
+
+    for (let i = 0; i < intLen; i += squareRootLen) {
+        const chunkAmount = squareRootLen;
+        const chunk = strInt.slice(i, i + chunkAmount);
+        res += chunk + "\n";
+    }
+
+    return res.trim();
+}
+
+console.log( squareIt(123123123) );
+// "123
+//  123
+//  123"
