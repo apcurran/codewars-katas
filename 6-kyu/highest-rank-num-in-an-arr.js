994d8035de053d1d5842bf661657013ebeb61e30
Alex Curran <alexcurran.va@gmail.com>
2022-12-15 12:10:50 -0600

    Working solution for highest-rank-num-in-an-arr.js

---
+++ 6-kyu/highest-rank-num-in-an-arr.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function highestRank(arr) {
+    let valsCache = new Map();
+
+    for (let val of arr) {
+        const previousFrequency = valsCache.get(val) || 0;
+        valsCache.set(val, previousFrequency + 1);
+    }
+
+    const maxFrequency = Math.max(...valsCache.values());
+    let maxValWithHighestFrequency = 0;
+
+    for (let [val, frequency] of valsCache) {
+        if (frequency === maxFrequency && val > maxValWithHighestFrequency) {
+            maxValWithHighestFrequency = val;
+        }
+    }
+
+    return maxValWithHighestFrequency;
+}
+
+console.log( highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]) ); // 12
+console.log( highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]) ); // 12
