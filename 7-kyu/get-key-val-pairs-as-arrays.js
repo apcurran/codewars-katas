b3eb32ad6f9d915ff5ac2f754837caa5ff780730
Alex Curran <alexcurran.va@gmail.com>
2022-12-16 15:52:57 -0600

    Working solution for get-key-val-pairs-as-arrays.js

---
+++ 7-kyu/get-key-val-pairs-as-arrays.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object} data 
+ * @returns {any[][]}
+ */
+function keysAndValues(data) {
+    const keysArr = Object.keys(data);
+    const valsArr = Object.values(data);
+
+    return [keysArr, valsArr];
+}
+
+console.log( keysAndValues({a: 1, b: 2, c: 3}) ); // [['a', 'b', 'c'], [1, 2, 3]]
