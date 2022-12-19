51088e3becdcf5149d298e971bb2aaeaa804994d
Alex Curran <alexcurran.va@gmail.com>
2022-12-19 14:25:14 -0600

    Working solution for mirror-mirror.js

---
+++ 6-kyu/mirror-mirror.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * 
+ * @param {object} obj 
+ * @returns {object}
+ */
+function evilTwin(obj) {
+    // must link to original obj,
+    // cannot create and return a new obj
+    return Object.create(obj, {
+        hasGoatee: {
+            value: true
+        }
+    });
+}
+
+const orig = { x: 5 };
+const twin = evilTwin(orig);
+console.log(twin.x); // -> 5
+console.log(twin.hasGoatee); 
