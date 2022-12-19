5526a6cf91daf2635efa1e91fac064c6f276cfcb
Alex Curran <alexcurran.va@gmail.com>
2022-12-19 12:07:51 -0600

    Working solution for once.js

---
+++ 6-kyu/once.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1 -- closure
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {function} fn 
+ * @returns {function}
+ */
+function once(fn) {
+    let counter = 0;
+
+    return (...args) => {
+        counter++;
+
+        if (counter > 1) return undefined;
+
+        return fn(...args);
+    };
+}
+
+const justLogOnce = once(console.log);
+justLogOnce("mustard");
+justLogOnce("ketchup");
