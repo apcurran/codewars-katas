93e0c2c7638bcbb75f6b1c9395c84c133ade1fd4
Alex Curran <alexcurran.va@gmail.com>
2022-12-19 09:53:24 -0600

    Working solution for js-filter-1.js

---
+++ 7-kyu/js-filter-1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[][]} logins 
+ * @returns {string[][]}
+ */
+function searchNames(logins) {
+    return logins.filter((strArr) => {
+        const login = strArr[0];
+        const hasUnderscore = login[login.length - 1] === "_";
+
+        return hasUnderscore;
+    });
+}
+
+console.log( searchNames([[ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ]]) );
+// [[ "bar_", "bar@bar.com" ]]
