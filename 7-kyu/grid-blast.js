4d40e7b2d9651cdc6ea92c480d733ad8131b87a1
Alex Curran <alexcurran.va@gmail.com>
2022-12-20 17:13:15 -0600

    Alternative

--- 7-kyu/grid-blast.js
+++ 7-kyu/grid-blast.js
@@ -1,5 +1,24 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(1)
+//  * space: O(1)
+//  * 
+//  * @param {number} x 
+//  * @param {number} y 
+//  * @returns {string}
+//  */
+// function fire(x, y) {
+//     const myGrid = [
+//         ["top left",    "top middle",    "top right",],
+//         ["middle left", "center",        "middle right"],
+//         ["bottom left", "bottom middle", "bottom right"]
+//     ];
+
+//     return myGrid[y][x];
+// }
+
 /**
  * solution 1
  * time: O(1)
@@ -10,13 +29,7 @@
  * @returns {string}
  */
 function fire(x, y) {
-    const myGrid = [
-        ["top left",    "top middle",    "top right",],
-        ["middle left", "center",        "middle right"],
-        ["bottom left", "bottom middle", "bottom right"]
-    ];
-
-    return myGrid[y][x];
+    return grid[x + (3 * y)];
 }
 
 console.log( fire(0, 0) ); // "top left"
