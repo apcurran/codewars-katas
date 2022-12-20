0641eebe9103aae7bfb05dce97a3eeed2d9e5d0c
Alex Curran <alexcurran.va@gmail.com>
2022-12-20 09:33:54 -0600

    Working solution for js-class-like-objects.js

---
+++ 7-kyu/js-class-like-objects.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class Animal {
+    /**
+     * @param {string} name 
+     * @param {string} type 
+     */
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    toString() {
+        return `${this.name} is a ${this.type}`;
+    }
+}
+
+const dog = new Animal("Max", "dog");
+console.log(dog.toString());
+console.log(dog.type);
+console.log(dog.name);
+dog.name = "Lassie";
+console.log(dog.name);
+
