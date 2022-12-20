1b9dd99d267469e7afdf9a8260fd7af657e8ff93
Alex Curran <alexcurran.va@gmail.com>
2022-12-20 17:11:24 -0600

    Working solution for grid-blast.js

---
+++ 7-kyu/grid-blast.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns {string}
+ */
+function fire(x, y) {
+    const myGrid = [
+        ["top left",    "top middle",    "top right",],
+        ["middle left", "center",        "middle right"],
+        ["bottom left", "bottom middle", "bottom right"]
+    ];
+
+    return myGrid[y][x];
+}
+
+console.log( fire(0, 0) ); // "top left"
+console.log( fire(1, 2) ); // "bottom middle"
