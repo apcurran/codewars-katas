1187fbdb0e74f745520a7595d7a29a5f3f1c7796
Alex Curran <alexcurran.va@gmail.com>
2022-12-22 10:24:53 -0600

    Working solution for prod-of-the-main-diag-of-a-sqr-mat.js

---
+++ 7-kyu/prod-of-the-main-diag-of-a-sqr-mat.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[][]} matrix 
+ * @returns {number}
+ */
+function mainDiagonalProduct(matrix) {
+    let product = 1;
+
+    for (let i = 0; i < matrix.length; i++) {
+        // get row column value
+        product *= matrix[i][i];
+    }
+
+    return product;
+}
+
+console.log( mainDiagonalProduct(
+    [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+) ); // 45
