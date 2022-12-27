bfdeb54dc97397992099d897ed690a23142ac910
Alex Curran <alexcurran.va@gmail.com>
2022-12-27 15:44:11 -0600

    Working solution for is-really-nan.js

---
+++ 8-kyu/is-really-nan.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {any} val 
+ * @returns {boolean}
+ */
+function isReallyNaN(val) {
+    return Number.isNaN(val);
+}
