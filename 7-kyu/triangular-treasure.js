beffa67a6f45d16eaf0854a4f70b0858907537b3
Alex Curran <alexcurran.va@gmail.com>
2022-12-28 09:19:28 -0600

    Working O(1) solution for triangular-treasure.js

--- 7-kyu/triangular-treasure.js
+++ 7-kyu/triangular-treasure.js
@@ -1,21 +1,35 @@
 "use strict";
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number} n 
+//  * @returns {number}
+//  */
+// function triangular(n) {
+//     let counter = 0;
+
+//     for (let i = n; i >= 0; i--) {
+//         counter += i;
+//     }
+
+//     return counter;
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n)
+ * solution 2 -- math formula
+ * time: O(1)
  * space: O(1)
  * 
  * @param {number} n 
  * @returns {number}
  */
 function triangular(n) {
-    let counter = 0;
+    if (n <= 0) return 0;
 
-    for (let i = n; i >= 0; i--) {
-        counter += i;
-    }
-
-    return counter;
+    return (n * n + n) / 2;
 }
 
 console.log( triangular(2) ); // 3
