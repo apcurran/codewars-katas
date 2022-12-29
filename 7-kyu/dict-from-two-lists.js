af67edbe280c4146274de3d0393082f33e2e61e5
Alex Curran <alexcurran.va@gmail.com>
2022-12-29 09:46:26 -0600

    Working solution for dict-from-two-lists.js

---
+++ 7-kyu/dict-from-two-lists.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = keys array length
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[]} keys 
+ * @param {number[]} values 
+ * @returns {object}
+ */
+function createDict(keys, values) {
+    let result = {};
+    
+    for (let i = 0; i < keys.length; i++) {
+        // add key/val pair to result object
+        // if the key does not have a corresponding value,
+        // assign null as the val
+        const key = keys[i];
+        const val = values[i] === undefined ? null : values[i]; // null fallback if val is undefined
+        result[key] = val;
+    }
+    
+    return result;
+}
+
+console.log( createDict(["a", "b", "c", "d"], [1, 2, 3]) ); // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+console.log( createDict(["a", "b", "c"], [1, 2, 3, 4]) ); // {'a': 1, 'b': 2, 'c': 3}
