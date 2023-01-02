6f789894d8f6fd8c90fe104cc343c58bcbd906c9
Alex Curran <alexcurran.va@gmail.com>
2023-01-02 10:01:53 -0600

    O(1) solution

--- 7-kyu/discover-the-original-price.js
+++ 7-kyu/discover-the-original-price.js
@@ -1,18 +1,33 @@
 "use strict";
 
+// /**
+//  * solution 1 
+//  * time: O(n) -- string conversion
+//  * space: O(n) -- string conversion
+//  * 
+//  * @param {number} discountedPrice 
+//  * @param {number} salePercentage 
+//  * @returns {number}
+//  */
+// function discoverOriginalPrice(discountedPrice, salePercentage) {
+//     const originalPrice = discountedPrice / (1 - (salePercentage / 100));
+
+//     return Number(originalPrice.toFixed(2));
+// }
+
 /**
- * solution 1 
- * time: O(n) -- string conversion
- * space: O(n) -- string conversion
+ * solution 2 -- number manipulation to get O(1) time
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} discountedPrice 
  * @param {number} salePercentage 
  * @returns {number}
  */
 function discoverOriginalPrice(discountedPrice, salePercentage) {
-    const originalPrice = discountedPrice / (1 - (salePercentage / 100));
+    const originalPrice = Math.round(discountedPrice / (1 - salePercentage / 100) * 100) / 100;
 
-    return Number(originalPrice.toFixed(2));
+    return originalPrice;
 }
 
 console.log( discoverOriginalPrice(75, 25) ); // 100
