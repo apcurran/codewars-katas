bc59299480156f44d196fa00f0c687395b2f9fcc
Alex Curran <alexcurran.va@gmail.com>
2023-01-06 09:21:42 -0600

    Working O(n) time/space solution for count-consonants.js

---
+++ 7-kyu/count-consonants.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function consonantCount(str) {
+    const consonants = str.match(/[bcdfghjklmnpqrstvwxyz]/gi) || [];
+
+    return consonants.length;
+}
+
+console.log( consonantCount("XaeiouX") ); // 2
+console.log( consonantCount("") ); // 0
+console.log( consonantCount("helLo world") ); // 7
+console.log( consonantCount("iiuoiouiuieauoeeesybknhlsrfyrbvhghbdjkxmybmsnqxvdvzvxxcqffzpfvztctfgbxbhqlftrcscrlddywcbhhbmzrtjyywsnrzybyz") ); // 90
