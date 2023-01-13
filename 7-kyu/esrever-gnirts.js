d739d48406e6ee510dd00808eff274a8915465dc
Alex Curran <alexcurran.va@gmail.com>
2023-01-13 16:56:18 -0600

    Working solution for esrever-gnirts.js

---
+++ 7-kyu/esrever-gnirts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @returns {string}
+ */
+String.prototype.reverse = function() {
+    const word = this;
+    let reversedWord = "";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        const character = word[i];
+        reversedWord += character;
+    }
+
+    return reversedWord;
+};
+
+var original = "String";
+var reversed = original.reverse();
+
+console.log(reversed); // "gnirtS"
