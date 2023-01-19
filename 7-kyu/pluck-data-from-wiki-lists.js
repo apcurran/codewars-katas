4af6acfa78ad561ab5e4d69d1a5fa8e3a168c0af
Alex Curran <alexcurran.va@gmail.com>
2023-01-19 16:59:24 -0600

    Working solution for pluck-data-from-wiki-lists.js

---
+++ 7-kyu/pluck-data-from-wiki-lists.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object[]} data 
+ * @param {string} property 
+ * @returns {number[]|string[]}
+ */
+function pluck(data, property) {
+    return data.map((obj) => obj[property]);
+}
+
+const gdpPerCapita = [{
+    "name": "Luxembourg",
+    "dollars": 111716
+}, {
+    "name": "Norway",
+    "dollars": 97013
+}, {
+    "name": "Qatar",
+    "dollars": 93965
+}];
+
+console.log( pluck(gdpPerCapita, "name") ); // ["Luxembourg","Norway","Qatar"]
