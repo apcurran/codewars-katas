5a67d8199670e6e998a70cf368b51b9d405d9d3a
Alex Curran <alexcurran.va@gmail.com>
2023-01-20 10:46:42 -0600

    Working solution for lowest-prod-of-4-consec-nums.js

---
+++ 6-kyu/lowest-prod-of-4-consec-nums.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} strNumber 
+ * @returns {number|string}
+ */
+function lowestProduct(strNumber) { 
+    if (strNumber.length < 4) return "Number is too small";
+
+    let smallestProduct = Infinity;
+
+    for (let i = 0; i < strNumber.length - 3; i++) {
+        const number1 = Number(strNumber[i]);
+        const number2 = Number(strNumber[i + 1]);
+        const number3 = Number(strNumber[i + 2]);
+        const number4 = Number(strNumber[i + 3]);
+        const numbersProduct = number1 * number2 * number3 * number4;
+
+        if (numbersProduct < smallestProduct) {
+            smallestProduct = numbersProduct;
+        }
+    }
+
+    return smallestProduct;
+}
+
+console.log( lowestProduct("123456789") ); // 24
+console.log( lowestProduct("35") ); // "Number is too small"
