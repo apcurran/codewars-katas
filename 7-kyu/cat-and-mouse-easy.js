792b0f9730c39b6d521be25a300d3432d7c0cbbf
Alex Curran <alexcurran.va@gmail.com>
2023-01-20 16:25:22 -0600

    Working solution for cat-and-mouse-easy.js

---
+++ 7-kyu/cat-and-mouse-easy.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} x 
+ * @returns {string}
+ */
+function catMouse(x){
+    let distance = 0;
+
+    for (let i = 1; i < x.length; i++) {
+        const currentCharacter = x[i];
+        
+        if (currentCharacter === ".") distance++;
+    }
+
+    return distance > 3 ? "Escaped!" : "Caught!";
+}
+
+console.log( catMouse("C....m") ); // "Escaped!";
+console.log( catMouse("C..m") ); // "Caught!";
