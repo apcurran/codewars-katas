4e1e1fbb320f4cba9d99c027680de8d53c3cce76
Alex Curran <alexcurran.va@gmail.com>
2023-01-23 10:42:20 -0600

    Working solution for linked-list-arr.js

---
+++ 7-kyu/linked-list-arr.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object} list 
+ * @returns {number[]}
+ */
+function listToArray(list) {
+    let results = [];
+    let currentNode = list;
+
+    while (currentNode !== null) {
+        // read current node value
+        results.push(currentNode.value);
+        // traverse to next node
+        currentNode = currentNode.next;
+    }
+
+    return results;
+}
