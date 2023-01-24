ab5773ceec362769cf0d4f63f52eeed195228b7b
Alex Curran <alexcurran.va@gmail.com>
2023-01-24 09:47:55 -0600

    Working solution for evil-or-odious.js

---
+++ 8-kyu/evil-or-odious.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @returns {string}
+ */
+function evil(n) {
+    const binaryStr = n.toString(2);
+    const onesDigitCount = binaryStr.match(/1/g)?.length;
+    const isEvenOnesDigitCount = onesDigitCount % 2 === 0;
+
+    return isEvenOnesDigitCount ? "It's Evil!" : "It's Odious!"; 
+}
+
+console.log( evil(1) ); // "It's Odious!"
