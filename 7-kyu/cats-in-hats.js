7d0d06b0b246a685e797b119ebf8e8812be765e5
Alex Curran <alexcurran.va@gmail.com>
2023-01-25 09:36:11 -0600

    Working solution for cats-in-hats.js

---
+++ 7-kyu/cats-in-hats.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * 
+ * @param {number} n 
+ * @returns {string}
+ */
+function height(n) {
+    let catInTheHatHeight = 2000000;
+    let totalHeight = 2000000;
+
+    for (let i = 1; i <= n; i++) {
+        const currentHatCatHeight = catInTheHatHeight / 2.5;
+        catInTheHatHeight = currentHatCatHeight;
+        totalHeight += currentHatCatHeight;
+    }
+
+    return totalHeight.toFixed(3);
+}
+
+console.log( height(7) ); // "3331148.800"
+console.log( height(0) ); // "2000000.000"
