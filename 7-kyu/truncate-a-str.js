1b260b8ea1872021db8e03caced58932f1cb44c5
Alex Curran <alexcurran.va@gmail.com>
2023-01-25 09:27:17 -0600

    Working solution for truncate-a-str.js

---
+++ 7-kyu/truncate-a-str.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @param {number} num 
+ * @returns {string}
+ */
+function truncateString(str, num) {
+    if (str.length <= num) return str;
+
+    if (num <= 3) return str.slice(0, num) + "...";
+
+    return str.slice(0, num - 3) + "...";
+}
+
+console.log( truncateString("codewars", 9) ); // 'codewars'
+console.log( truncateString("codewars", 7) ); // 'code...'
+console.log( truncateString("codewars", 2) ); // "co..."
