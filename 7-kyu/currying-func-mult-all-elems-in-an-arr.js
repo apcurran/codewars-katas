1a0875cf60b311d45f0cd1989aa830825a6f5599
Alex Curran <alexcurran.va@gmail.com>
2023-01-31 09:41:03 -0600

    Working solution for currying-func-mult-all-elems-in-an-arr.js

---
+++ 7-kyu/currying-func-mult-all-elems-in-an-arr.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * return a func that takes a single arg and multiplies the nums arr by this arg
+ * 
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @returns {function}
+ */
+function multiplyAll(nums) {
+    /**
+     * @param {number} multiplier
+     * @returns {number[]}
+     */
+    return function multiplyByThisArg(multiplier) {
+        return nums.map((num) => num * multiplier);
+    };
+}
+
+console.log( multiplyAll([1, 2, 3])(2) ); // [2, 4, 6]
