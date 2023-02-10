e05b381821461c4b49d6027f30a156a1219527b8
Alex Curran <alexcurran.va@gmail.com>
2023-02-10 09:50:29 -0600

    Working solution for str-transformer.js

---
+++ 6-kyu/str-transformer.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = length of wordsList
+ * m = length of characters in currentWord
+ * time: O(n * m)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function stringTransformer(str) {
+    const wordsList = str.split(" ");
+    let result = [];
+
+    for (let i = wordsList.length - 1; i >= 0; i--) {
+        const currentWord = wordsList[i];
+        let adjustedWordCasing = "";
+
+        for (let char of currentWord) {
+            if (char === char.toUpperCase()) {
+                adjustedWordCasing += char.toLowerCase();
+            } else {
+                adjustedWordCasing += char.toUpperCase();
+            }
+        }
+
+        result.push(adjustedWordCasing);
+    }
+
+    return result.join(" ");
+}
+
+console.log( stringTransformer("Example Input") ); // "iNPUT eXAMPLE"
