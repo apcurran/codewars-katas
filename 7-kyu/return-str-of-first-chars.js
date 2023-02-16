5bd760a1425fcec337934cb189158438bbfd2df0
Alex Curran <alexcurran.va@gmail.com>
2023-02-16 10:18:09 -0600

    Working solution for return-str-of-first-chars.js

---
+++ 7-kyu/return-str-of-first-chars.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function makeString(str) {
+    return str
+            .split(" ")
+            .map((word) => word[0])
+            .join("");
+}
+
+console.log( makeString("cars are very nice") ); // "cavn"
