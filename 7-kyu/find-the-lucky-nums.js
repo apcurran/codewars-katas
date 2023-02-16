657a89628e66e954868944c5a0bed10a5070a1e4
Alex Curran <alexcurran.va@gmail.com>
2023-02-16 09:31:14 -0600

    Working solution for find-the-lucky-nums.js

---
+++ 7-kyu/find-the-lucky-nums.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = nums array length
+ * m = num digits array length
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {number[]} nums 
+ * @returns {number[]}
+ */
+function filterLucky(nums) {
+    return nums.filter((num) => {
+        const strNumDigits = String(num).split("");
+        const strNumDigitsHasASeven = strNumDigits.includes("7");
+
+        if (strNumDigitsHasASeven) return true;
+
+        return false;
+    });
+}
+
+console.log( filterLucky([1,2,3,4,5,6,7,68,69,70,15,17]) ); // [7,70,17]
