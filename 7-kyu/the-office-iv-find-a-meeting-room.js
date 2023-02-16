70d7f4fef7a5f2afe464be3b9f245e5001d25791
Alex Curran <alexcurran.va@gmail.com>
2023-02-16 09:20:58 -0600

    Working solution for the-office-iv-find-a-meeting-room.js

---
+++ 7-kyu/the-office-iv-find-a-meeting-room.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string[]} rooms 
+ * @returns {number|string} index of the first empty meeting room, or "None available!"
+ */
+function meeting(rooms) {
+    const firstEmptyRoomIndex = rooms.findIndex((roomStatus) => roomStatus === "O");
+    
+    if (firstEmptyRoomIndex === -1) return "None available!";
+
+    return firstEmptyRoomIndex;
+}
+
+console.log( meeting(["X", "O", "X"]) ); // 1
+console.log( meeting(["X","X","X","X","X"]) ); // "None available!"
