abc2b48294fc04c0f7c00af641f62fa9505a3939
Alex Curran <alexcurran.va@gmail.com>
2023-02-17 14:23:55 -0600

    Working solution for fixme-get-full-name.js

---
+++ 7-kyu/fixme-get-full-name.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class Dinglemouse {
+    /**
+     * @param {string} firstName 
+     * @param {string} lastName 
+     */
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * solution 1
+     * time: O(n)
+     * space: O(n)
+     * 
+     * @returns {string}
+     */
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`.trim();
+    }
+}
