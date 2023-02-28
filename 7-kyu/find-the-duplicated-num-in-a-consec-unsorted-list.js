f767c51ae4a7909b48ad70e21df489641f56dbc7
Alex Curran <alexcurran.va@gmail.com>
2023-02-28 09:45:07 -0600

    Two more working solutions O(n) time/space

--- 7-kyu/find-the-duplicated-num-in-a-consec-unsorted-list.js
+++ 7-kyu/find-the-duplicated-num-in-a-consec-unsorted-list.js
@@ -1,24 +1,69 @@
 "use strict";
 
+// /**
+//  * solution 1 -- sort arr
+//  * time: O(n * log n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} arr 
+//  * @returns {number}
+//  */
+// function findDup(arr) {
+//     const sortedNums = arr.sort((a, b) => a - b);
+
+//     for (let i = 0; i < sortedNums.length - 1; i++) {
+//         const currentNum = sortedNums[i];
+//         const nextNum = sortedNums[i + 1];
+
+//         if (currentNum === nextNum) return currentNum;
+//     }
+
+//     return -1;
+// }
+
 /**
- * solution 1 -- sort arr
- * time: O(n * log n)
+ * solution 2 -- Set obj
+ * time: O(n)
  * space: O(n)
  * 
  * @param {number[]} arr 
  * @returns {number}
  */
 function findDup(arr) {
-    const sortedNums = arr.sort((a, b) => a - b);
+    let seenValues = new Set();
 
-    for (let i = 0; i < sortedNums.length - 1; i++) {
-        const currentNum = sortedNums[i];
-        const nextNum = sortedNums[i + 1];
+    for (let num of arr) {
+        if (seenValues.has(num)) return num;
 
-        if (currentNum === nextNum) return currentNum;
+        seenValues.add(num);
     }
 
     return -1;
 }
+
+// /**
+//  * solution 3 -- Set obj
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} arr 
+//  * @returns {number}
+//  */
+// function findDup(arr) {
+//     let arrSum = 0;
+    
+//     for (let num of arr) {
+//         arrSum += num;
+//     }
+    
+//     let uniqueValues = new Set(arr);
+//     let uniqueValuesSum = 0;
+
+//     for (let num of uniqueValues) {
+//         uniqueValuesSum += num;
+//     }   
+    
+//     return arrSum - uniqueValuesSum;
+// }
 
 console.log( findDup([1,3,2,5,4,5,7,6]) ); // 5
