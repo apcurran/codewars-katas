970102cc6008a2140bb6950cae602f29f4a3cd1d
Alex Curran <alexcurran.va@gmail.com>
2023-02-28 09:16:04 -0600

    Working solution for disarium-num.js

---
+++ 7-kyu/disarium-num.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} num 
+ * @returns {"Disarium !!"|"Not !!"}
+ */
+function disariumNumber(num) {
+    const strNum = String(num);
+    let digitsPoweredSum = 0;
+
+    for (let i = 0; i < strNum.length; i++) {
+        const digitPosition = i + 1;
+        const digit = Number(strNum[i]);
+        const digitPoweredToPosition = digit ** digitPosition;
+        digitsPoweredSum += digitPoweredToPosition;
+    }
+
+    return digitsPoweredSum === num ? "Disarium !!" : "Not !!";
+}
+
+console.log( disariumNumber(89) ); // "Disarium !!"
