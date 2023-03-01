4129538941b80ae7d5a343e064123366b2acdfd5
Alex Curran <alexcurran.va@gmail.com>
2023-03-01 13:35:58 -0600

    Working solution for i-am-very-very-very.js

---
+++ 6-kyu/i-am-very-very-very.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {string} input 
+ * @returns {string|function}
+ */
+function iam(input, veryWordCounter = 0) {
+    // return a string if input given
+    if (input) {
+        const veryPortion = "very ".repeat(veryWordCounter);
+
+        return `I am ${veryPortion}${input}`;
+    }
+    // otherwise, return a func to continue the sentence
+    return function calculateVery(input) {
+        return iam(input, veryWordCounter + 1);
+    };
+}
+
+console.log( iam("happy") ); // "I am happy"
+console.log( iam("excited") ); // "I am excited"
+console.log( iam()("scared") ); // "I am very scared"
