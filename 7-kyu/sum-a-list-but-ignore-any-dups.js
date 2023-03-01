861c987b2140e27f4df5260309e5eae324d9fa2a
Alex Curran <alexcurran.va@gmail.com>
2023-03-01 09:55:40 -0600

    Working solution for sum-a-list-but-ignore-any-dups.js

---
+++ 7-kyu/sum-a-list-but-ignore-any-dups.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} numList 
+ * @returns {number}
+ */
+function sumNoDuplicates(numList) {
+    const uniqueValues = new Map();
+
+    for (let value of numList) {
+        const previousValueCount = uniqueValues.get(value) || 0;
+        uniqueValues.set(value, previousValueCount + 1);
+    }
+
+    let sum = 0;
+    
+    for (let [value, count] of uniqueValues) {
+        if (count > 1) continue;
+
+        sum += value;
+    }
+
+    return sum;
+}
+
+console.log( sumNoDuplicates([1, 1, 2, 3]) ); // 5
+console.log( sumNoDuplicates([3, 4, 3, 6]) ); // 10
