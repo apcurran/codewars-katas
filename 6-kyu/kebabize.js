a3831452e2770af29ad326be1214490cadf00625
Alex Curran <alexcurran.va@gmail.com>
2023-03-06 10:17:50 -0600

    Working solution for kebabize.js

---
+++ 6-kyu/kebabize.js
@@ -0,0 +1,17 @@
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function kebabize(str) {
+    return str
+            .replace(/[^a-z]/gi, "")
+            .replace(/^[A-Z]/, (letter) => letter.toLowerCase())
+            .replace(/[A-Z]/g, (letter) => "-" + letter.toLowerCase());
+}
+
+console.log( kebabize("camelsHaveThreeHumps") ); // "camels-have-three-humps"
+console.log( kebabize("camelsHave3Humps") ); // "camels-have-humps"
