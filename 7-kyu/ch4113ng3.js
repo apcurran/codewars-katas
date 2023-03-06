e5d9d653dc12360d86f09a53d158e71249e73dbb
Alex Curran <alexcurran.va@gmail.com>
2023-03-06 09:34:49 -0600

    Working solution for ch4113ng3.js

---
+++ 7-kyu/ch4113ng3.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// /**
+//  * solution 1 -- regex
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} txt 
+//  * @returns {string}
+//  */
+// function nerdify(txt) {
+//     return txt
+//             .replace(/a/gi, "4")
+//             .replace(/e/gi, "3")
+//             .replace(/l/g, "1");
+// }
+
+/**
+ * solution 2 -- Map obj
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} text 
+ * @returns {string}
+ */
+function nerdify(text) {
+    const letterReplacements = new Map([
+        ["a", "4"],
+        ["A", "4"],
+        ["e", "3"],
+        ["E", "3"],
+        ["l", "1"]
+    ]);
+    let updatedText = "";
+
+    for (let letter of text) {
+        if (!letterReplacements.has(letter)) {
+            updatedText += letter;
+
+            continue;
+        }
+
+        const adjustedLetter = letterReplacements.get(letter);
+        updatedText += adjustedLetter;
+    }
+
+    return updatedText;
+}
+
+console.log( nerdify("Fundamentals") ); // "Fund4m3nt41s"
