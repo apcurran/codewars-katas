f7e43c5c3cffb5549d234df2dbebd96610c0ddcb
Alex Curran <alexcurran.va@gmail.com>
2023-03-07 11:15:44 -0600

    Working O(n) solution for is-int-arr.js

--- 6-kyu/is-int-arr.js
+++ 6-kyu/is-int-arr.js
@@ -1,9 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * m)
+//  * space: O(m)
+//  * 
+//  * @param {array} arr 
+//  * @returns {boolean}
+//  */
+// function isIntArray(arr) {
+//     if (!Array.isArray(arr)) return false;
+
+//     if (arr.length === 0) return true;
+
+//     // iterate through list
+//     return arr.every((elem) => {
+//         // check type of element for number
+//         if (typeof elem !== "number" || Number.isNaN(elem)) return false;
+
+//         // if a number, then convert to binary string
+//         const binaryIntStr = (parseInt(String(elem), 10)).toString(2);
+//         const binaryOriginalNumStr = elem.toString(2);
+//         // compare the int version binary string to converted binary string
+//         if (binaryIntStr !== binaryOriginalNumStr) {
+//             return false;
+//         }
+//         // if they are the same, return true for this element
+//         return true;
+//     });
+// }
+
 /**
- * solution 1
- * time: O(n * m)
- * space: O(m)
+ * solution 2
+ * time: O(n)
+ * space: O(1)
  * 
  * @param {array} arr 
  * @returns {boolean}
@@ -11,22 +41,8 @@
 function isIntArray(arr) {
     if (!Array.isArray(arr)) return false;
 
-    if (arr.length === 0) return true;
-
-    // iterate through list
     return arr.every((elem) => {
-        // check type of element for number
-        if (typeof elem !== "number" || Number.isNaN(elem)) return false;
-
-        // if a number, then convert to binary string
-        const binaryIntStr = (parseInt(String(elem), 10)).toString(2);
-        const binaryOriginalNumStr = elem.toString(2);
-        // compare the int version binary string to converted binary string
-        if (binaryIntStr !== binaryOriginalNumStr) {
-            return false;
-        }
-        // if they are the same, return true for this element
-        return true;
+        return Math.floor(elem) === elem;
     });
 }
 
