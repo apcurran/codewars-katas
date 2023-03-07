5b0005cd5462319e8e27e94ece133e61d328c367
Alex Curran <alexcurran.va@gmail.com>
2023-03-07 10:02:03 -0600

    Working solution for more-zeros-than-ones.js

---
+++ 6-kyu/more-zeros-than-ones.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(n + m)
+ * 
+ * @param {string} str 
+ * @returns {string[]}
+ */
+function moreZeros(str){
+    // create a Set to remove duplicate strings
+    const uniqueChars = new Set([...str]);
+    let charsWithMoreBinaryZeros = [];
+    // iterate Set chars
+    for (let char of uniqueChars) {
+        // convert each char to string ASCII value
+        const charCode = char.charCodeAt(0);
+        // convert ASCII value to binary
+        const charBinary = charCode.toString(2);
+        // remove all leading zeros with unary or Number constructor
+        const charBinaryWithoutLeadingZeros = String( Number(charBinary) );
+        
+        let binaryCharZerosCount = 0;
+        let binaryCharOnesCount = 0;
+        // iterate binary string
+        for (let binaryChar of charBinaryWithoutLeadingZeros) {
+            if (binaryChar === "0") {
+                // count the zeros
+                binaryCharZerosCount++;
+            } else {
+                // count the ones
+                binaryCharOnesCount++;
+            }
+        }
+
+        // if the zeros > ones, keep this char in a return array
+        if (binaryCharZerosCount > binaryCharOnesCount) {
+            charsWithMoreBinaryZeros.push(char);
+        }
+        // else, do not add this char to the return array
+    }
+
+    return charsWithMoreBinaryZeros;
+}
+
+console.log( moreZeros("abcde") ); // ['a','b','d']
+console.log( moreZeros("DIGEST") ); // ['D','I','E','T']
