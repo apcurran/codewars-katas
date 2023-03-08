bb801d41c5704634c347c89eec7f65f8bf756499
Alex Curran <alexcurran.va@gmail.com>
2023-03-08 10:00:26 -0600

    Working solution for noisy-cell-counts.js

---
+++ 7-kyu/noisy-cell-counts.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} data 
+ * @returns {number[]}
+ */
+function cleanedCounts(data) {
+    let updatedCounts = [];
+    let previousCellCount = 0;
+
+    for (let currentCellCount of data) {
+        if (previousCellCount > currentCellCount) {
+            updatedCounts.push(previousCellCount);
+        } else {
+            updatedCounts.push(currentCellCount);
+            previousCellCount = currentCellCount;
+        }
+    }
+
+    return updatedCounts;
+}
+
+console.log( cleanedCounts([1, 1, 2, 2, 1, 2, 2, 2, 2]) );
+// [1, 1, 2, 2, 2, 2, 2, 2, 2]
+
+console.log( cleanedCounts([5, 5, 6, 5, 5, 5, 5, 6]) );
+// [5, 5, 6, 6, 6, 6, 6, 6]
