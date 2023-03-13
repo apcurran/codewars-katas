55ff493e3462571a23246812fdd92635bee7b395
Alex Curran <alexcurran.va@gmail.com>
2023-03-13 09:18:12 -0500

    Working solution for the-first-non-rep-char-in-a-str.js

---
+++ 7-kyu/the-first-non-rep-char-in-a-str.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1 -- Set cache
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string|null}
+ */
+function firstNonRepeated(str) {
+    const uniqueCharsStr = [...new Set(str)].join("");
+
+    if (uniqueCharsStr === str) return str[0];
+    
+    let charCounts = new Map();
+
+    for (let char of str) {
+        const previousCharCount = charCounts.get(char) || 0;
+        charCounts.set(char, previousCharCount + 1);
+    }
+
+    for (let [char, count] of charCounts) {
+        if (count === 1) return char;
+    }
+
+    return null;
+}
+
+console.log( firstNonRepeated("test") ); // "e"
+console.log( firstNonRepeated("trend") ); // "t"
+console.log( firstNonRepeated("aabbcc") ); // null
