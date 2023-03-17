e43b6a5a33fcc565bddd74f1f9c529f3d3bd900a
Alex Curran <alexcurran.va@gmail.com>
2023-03-17 09:29:13 -0500

    Working solution for sum-of-mult.js

---
+++ 8-kyu/sum-of-mult.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @param {number} m excluded from multiples
+ * @returns {number|string} sum of all multiples
+ */
+function sumMul(n, m) {
+    let sum = 0;
+
+    for (let i = n; i < m; i += n) {
+        sum += i;
+    }
+
+    if (sum === 0) return "INVALID";
+
+    return sum;
+}
+
+console.log( sumMul(2, 9) ); // 2 + 4 + 6 + 8 = 20
+console.log( sumMul(4, -7) ); // "INVALID"
