f9702c15cc6d9d6404358fa448dcae23eb8a8658
Alex Curran <alexcurran.va@gmail.com>
2023-03-23 09:56:39 -0500

    Working O(n) time solution for fun-with-lists-any-match-all-match.js

---
+++ 7-kyu/fun-with-lists-any-match-all-match.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function ListNode(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {ListNode} head 
+ * @param {function} func 
+ * @returns {boolean}
+ */
+function anyMatch(head, func) {
+    // for each node in list
+    while (head !== null) {
+        const isMatch = func(head.data);
+        // check if current node passes the testing func
+        // if yes, return true
+        if (isMatch) return true;
+        // otherwise keep checking
+        head = head.next;
+    }
+
+    return false;
+}
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {ListNode} head 
+ * @param {function} func 
+ * @returns {boolean}
+*/
+function allMatch(head, func) {
+    while (head !== null) {
+        // compare values and return true if both are equal
+        const hasPassedTest = func(head.data);
+
+        if (!hasPassedTest) return false;
+
+        head = head.next;
+    }
+
+    return true;
+}
