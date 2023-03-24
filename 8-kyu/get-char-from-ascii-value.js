72acd914f1b5edb2f32f51535b2829ee562a4f14
Alex Curran <alexcurran.va@gmail.com>
2023-03-24 11:16:10 -0500

    Working solution for get-char-from-ascii-value.js

---
+++ 8-kyu/get-char-from-ascii-value.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} charCodeNum 
+ * @returns {string}
+ */
+function getChar(charCodeNum) {
+    return String.fromCharCode(charCodeNum);
+}
+
+console.log( getChar(65) ); // "A"
