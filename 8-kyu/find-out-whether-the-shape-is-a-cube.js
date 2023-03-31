012e2dbcb4d4aa1ab653697b361f06353ca8e338
Alex Curran <alexcurran.va@gmail.com>
2023-03-31 11:10:28 -0500

    Math.cbrt() solution for find-out-whether-the-shape-is-a-cube.js

---
+++ 8-kyu/find-out-whether-the-shape-is-a-cube.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} volume 
+ * @param {number} side 
+ * @returns {boolean}
+ */
+function cubeChecker(volume, side) {
+    if (volume <= 0 || side <= 0) return false;
+
+    const cubeRoot = Math.cbrt(volume);
+
+    return cubeRoot === side;
+}
+
+console.log( cubeChecker(56.3, 1) ); // false
+console.log( cubeChecker(8, 2) ); // true
