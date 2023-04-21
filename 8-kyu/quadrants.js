1cbc541de30569cf23f70b84714627b70f6725d4
Alex Curran <alexcurran.va@gmail.com>
2023-04-21 09:48:00 -0500

    Working solution for quadrants.js

---
+++ 8-kyu/quadrants.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns {number}
+ */
+function quadrant(x, y) {
+    if (x > 0 && y > 0) {
+        return 1;
+    } else if (x < 0 && y > 0) {
+        return 2;
+    } else if (x < 0 && y < 0) {
+        return 3;
+    } else if (x > 0 && y < 0) {
+        return 4;
+    }
+}
