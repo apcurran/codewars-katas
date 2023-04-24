c56fa97c7302a7b15e4814c8c3f667986613f3e5
Alex Curran <alexcurran.va@gmail.com>
2023-04-24 09:27:16 -0500

    Working recursive solution for sum-factorial.js

---
+++ 7-kyu/sum-factorial.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- recursion
+ * time: O(n * m)
+ * space: O(m) -- due to recursive call stack
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function sumFactorial(arr) {
+    let sum = 0;
+
+    for (let num of arr) {
+        const factorial = calcFactorial(num);
+        sum += factorial;
+    }
+
+    return sum;
+}
+
+/**
+ * @param {number} num 
+ * @param {Map} cache
+ * @returns {number}
+ */
+function calcFactorial(num) {
+    if (num <= 1) return 1;
+
+    return num * calcFactorial(num - 1);
+}
+
+console.log( sumFactorial([4, 6]) ); // 744
+console.log( sumFactorial([5,4,1]) ); // 145
