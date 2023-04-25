0bea7e14a415d14cedfbac3870b28075139cdc19
Alex Curran <alexcurran.va@gmail.com>
2023-04-25 09:52:55 -0500

    More readable solution

--- 7-kyu/all-inclusive.js
+++ 7-kyu/all-inclusive.js
@@ -1,7 +1,34 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = str length
+//  * m = arr length
+//  * time: O((n^2) + (n * m))
+//  * space: O(n)
+//  * 
+//  * @param {string} str 
+//  * @param {string[]} arr 
+//  * @returns {boolean}
+//  */
+// function containAllRots(str, arr) {
+//     let letters = [...str];
+
+//     if (!arr.includes(str)) return false;
+
+//     for (let i = 0; i < str.length - 1; i++) {
+//         const endingLetter = letters.pop();
+//         letters.unshift(endingLetter);
+//         const updatedWord = letters.join("");
+
+//         if (!arr.includes(updatedWord)) return false;
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 1
+ * solution 2 -- more readable implementation
  * n = str length
  * m = arr length
  * time: O((n^2) + (n * m))
@@ -12,16 +39,10 @@
  * @returns {boolean}
  */
 function containAllRots(str, arr) {
-    let letters = [...str];
+    for (let i = 0; i < str.length; i++) {
+        const word = str.slice(i) + str.slice(0, i);
 
-    if (!arr.includes(str)) return false;
-
-    for (let i = 0; i < str.length - 1; i++) {
-        const endingLetter = letters.pop();
-        letters.unshift(endingLetter);
-        const updatedWord = letters.join("");
-
-        if (!arr.includes(updatedWord)) return false;
+        if (!arr.includes(word)) return false;
     }
 
     return true;
