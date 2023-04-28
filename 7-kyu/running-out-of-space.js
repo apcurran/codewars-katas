71394874105616dfde64252a460721743bb85450
Alex Curran <alexcurran.va@gmail.com>
2023-04-28 09:12:26 -0500

    Improved solution

--- 7-kyu/running-out-of-space.js
+++ 7-kyu/running-out-of-space.js
@@ -1,10 +1,38 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = words length
+//  * m = chunk length
+//  * time: O(n^2)
+//  * space: O(n + m)
+//  * 
+//  * @param {string[]} words 
+//  * @returns {string[]}
+//  */
+// function spacey(words){
+//     let results = [];
+
+//     for (let i = 0; i < words.length; i++) {
+//         let chunks = [];
+
+//         for (let j = 0; j < i + 1; j++) {
+//             const currentWord = words[j];
+//             chunks.push(currentWord);
+//         }
+
+//         const chunkNoSpace = chunks.join("");
+//         results.push(chunkNoSpace);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1
+ * solution 2
  * n = words length
- * m = chunk length
- * time: O(n^2)
+ * m = partial length
+ * time: O(n * m)
  * space: O(n + m)
  * 
  * @param {string[]} words 
@@ -12,17 +40,11 @@
  */
 function spacey(words){
     let results = [];
+    let partial = "";
 
-    for (let i = 0; i < words.length; i++) {
-        let chunks = [];
-
-        for (let j = 0; j < i + 1; j++) {
-            const currentWord = words[j];
-            chunks.push(currentWord);
-        }
-
-        const chunkNoSpace = chunks.join("");
-        results.push(chunkNoSpace);
+    for (let word of words) {
+        partial += word;
+        results.push(partial);
     }
 
     return results;
