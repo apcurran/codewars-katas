7aa3c801a1103f89bb3fbdedc5d4b29da8518241
Alex Curran <alexcurran.va@gmail.com>
2023-05-05 09:57:35 -0500

    Working solution for nickname-generator.js

---
+++ 7-kyu/nickname-generator.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {string} name 
+ * @returns {string}
+ */
+function nicknameGenerator(name) {
+    if (name.length < 4) return "Error: Name too short";
+
+    const thirdLetter = name[2];
+
+    if (thirdLetter === "a" ||
+        thirdLetter === "e" ||
+        thirdLetter === "i" ||
+        thirdLetter === "o" ||
+        thirdLetter === "u"
+    ) {
+        return name.slice(0, 4);
+    }
+
+    return name.slice(0, 3);
+}
+
+console.log( nicknameGenerator("Robert") ); // "Rob"
+console.log( nicknameGenerator("Jeannie") ); // "Jean"
