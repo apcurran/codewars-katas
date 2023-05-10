4d8a03b69b653f2fa7c84819d94f5c56a7bf4ced
Alex Curran <alexcurran.va@gmail.com>
2023-05-10 10:27:27 -0500

    Fun without numbers -- solution for 5-without-numbers.js

---
+++ 8-kyu/5-without-numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * solution 1
+ * restriction -- no numbers used
+ * 
+ * @returns {number}
+ */
+function unusualFive() {
+    return "hello".length;
+}
+
+console.log( unusualFive() ); // 5
