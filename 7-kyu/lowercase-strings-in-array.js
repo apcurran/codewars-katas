e2bc8181854bf31c7e4d4ee4cb5bd3e8c9233dd1
Alex Curran <alexcurran.va@gmail.com>
2023-05-26 09:17:10 -0500

    Working solution for lowercase-strings-in-array.js

---
+++ 7-kyu/lowercase-strings-in-array.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = arr length
+ * m = string element length
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {any[]} arr 
+ * @returns {any[]}
+ */
+function arrayLowerCase(arr) {
+    return arr.map((element) => {
+        if (typeof element !== "string") return element;
+
+        return element.toLowerCase();
+    });
+}
+
+console.log( arrayLowerCase(["Red", "Green"]) ); // ['red', 'green']
+console.log( arrayLowerCase([1, "Green"]) ); // [1, 'green']
