bcd0b0e9b1920a83c56ccc413566ec804fd46a2a
Alex Curran <alexcurran.va@gmail.com>
2023-05-26 11:05:39 -0500

    Working solution for zebulans-nightmare.js

---
+++ 7-kyu/zebulans-nightmare.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * 
+ * @param {string} functionName 
+ * @returns {string}
+ */
+function zebulansNightmare(functionName) {
+    const strings = functionName.split("_");
+
+    return strings
+            .map((str, index) => {
+                if (index === 0) return str;
+
+                return titleCaseString(str);
+            })
+            .join("");
+}
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+function titleCaseString(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+console.log( zebulansNightmare("camel_case") ); // 'camelCase'
+console.log( zebulansNightmare("main") ); // 'main'
