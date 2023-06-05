035fc0b44f158e44ff118aa5b0de8e71b8805d79
Alex Curran <alexcurran.va@gmail.com>
2023-06-05 09:10:40 -0500

    Working solution for eliminate-the-intruders-bit-manipulation.js

---
+++ 7-kyu/eliminate-the-intruders-bit-manipulation.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} strNum 
+ * @returns {number}
+ */
+function eliminateUnsetBits(strNum) {
+    const noZerosStrNum = strNum.replaceAll("0", "");
+
+    if (noZerosStrNum === "") return 0;
+
+    return parseInt(noZerosStrNum, 2);
+}
+
+console.log( eliminateUnsetBits("11010101010101") ); // 255 (= 11111111)
+console.log( eliminateUnsetBits("111") ); // 7
+console.log( eliminateUnsetBits("000") ); // 0
