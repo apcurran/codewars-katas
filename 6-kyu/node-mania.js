88bbb17ac981f5e7dd60c052b15333b9f7e0df72
Alex Curran <alexcurran.va@gmail.com>
2023-06-08 09:39:13 -0500

    Working O(n) recursive solution

--- 6-kyu/node-mania.js
+++ 6-kyu/node-mania.js
@@ -16,8 +16,33 @@ a.next = b;
 b.next = c;
 c.next = d;
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {ListNode} linkedList 
+//  * @param {number} k 
+//  */
+// function searchKFromEnd(linkedList, k) {
+//     let nodes = [];
+//     let current = linkedList;
+
+//     while (current !== null) {
+//         // add each node to array
+//         nodes.push(current.data);
+//         // traverse to next node
+//         current = current.next;
+//     }
+
+//     if (k > nodes.length) return null;
+
+//     // pick kth to last node elem of array to return
+//     return nodes[nodes.length - k];
+// }
+
 /**
- * solution 1
+ * solution 1 -- recursive
  * time: O(n)
  * space: O(n)
  * 
@@ -25,20 +50,27 @@ c.next = d;
  * @param {number} k 
  */
 function searchKFromEnd(linkedList, k) {
-    let nodes = [];
-    let current = linkedList;
-
-    while (current !== null) {
-        // add each node to array
-        nodes.push(current.data);
-        // traverse to next node
-        current = current.next;
-    }
-
-    if (k > nodes.length) return null;
+    const nodeValues = recurse(linkedList, []);
+    
+    if (k > nodeValues.length) return null;
 
     // pick kth to last node elem of array to return
-    return nodes[nodes.length - k];
+    return nodeValues[nodeValues.length - k];
+}
+
+/**
+ * 
+ * @param {ListNode} head 
+ * @param {string[]} nodeValues
+ * @returns {string[]}
+ */
+function recurse(head, nodeValues) {
+    if (head === null) return nodeValues;
+
+    // add current value to list
+    nodeValues.push(head.data);
+    // continue to next node
+    return recurse(head.next, nodeValues);
 }
 
 console.log( searchKFromEnd(a, 1) ); // "d"
