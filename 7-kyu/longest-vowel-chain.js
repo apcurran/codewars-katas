e195f1831dcefa671ab48d5ba23e716267f17c1d
Alex Curran <alexcurran.va@gmail.com>
2023-06-27 09:21:27 -0500

    Working O(n) time and O(1) space solution for longest-vowel-chain.js

---
+++ 7-kyu/longest-vowel-chain.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function solve(str) {
+    const vowels = "aeiou";
+    let maxVowelSubstrCount = 0;
+    let currentVowelSubstrCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+
+        if (vowels.includes(char)) {
+            currentVowelSubstrCount++;
+        } else {
+            // record max
+            maxVowelSubstrCount = Math.max(maxVowelSubstrCount, currentVowelSubstrCount);
+            // reset current counter
+            currentVowelSubstrCount = 0;
+        }
+    }
+
+    maxVowelSubstrCount = Math.max(maxVowelSubstrCount, currentVowelSubstrCount);
+
+    return maxVowelSubstrCount;
+}
+
+console.log( solve("codewarriors") ); // 2
+console.log( solve("suoidea") ); // 3
