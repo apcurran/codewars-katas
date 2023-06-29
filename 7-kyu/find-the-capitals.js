9c5f73b155b48d5b48124ab9f729a8dd83ffffb7
Alex Curran <alexcurran.va@gmail.com>
2023-06-29 10:05:35 -0500

    Working solution for find-the-capitals.js

---
+++ 7-kyu/find-the-capitals.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object[]} capitals 
+ * @returns {string[]}
+ */
+function capital(capitals) {
+    return capitals.map((obj) => {
+        const capital = obj.capital;
+        const stateOrCountry = obj.state || obj.country;
+
+        return `The capital of ${stateOrCountry} is ${capital}`;
+    });
+}
+
+console.log( capital([{state: "Maine", capital: "Augusta"}])[0] ); // "The capital of Maine is Augusta"
+console.log( capital([{"country" : "Spain", "capital" : "Madrid"}])[0] ); // "The capital of Spain is Madrid"
