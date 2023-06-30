839bc8b2a991dd0d3aece49aa015070e1f25aa3c
Alex Curran <alexcurran.va@gmail.com>
2023-06-30 09:17:05 -0500

    Working solution with O(n) time O(1) space for check-three-and-two.js

---
+++ 7-kyu/check-three-and-two.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = arr length
+ * time: O(n)
+ * space: O(1) -- only "a", "b", and "c" chars
+ * 
+ * @param {string[]} arr 
+ * @returns {boolean}
+ */
+function checkThreeAndTwo(arr) {
+    let lettersFrequencies = new Map();
+
+    for (let char of arr) {
+        const previousCharCount = lettersFrequencies.get(char) || 0;
+        lettersFrequencies.set(char, previousCharCount + 1);
+    }
+
+    let isThreeOfAKind = false;
+    let isTwoOfAKind = false;
+
+    for (let [, charCount] of lettersFrequencies) {
+        if (charCount === 3) {
+            isThreeOfAKind = true;
+        }
+
+        if (charCount === 2) {
+            isTwoOfAKind = true;
+        }
+    }
+
+    return isThreeOfAKind && isTwoOfAKind;
+}
+
+console.log( checkThreeAndTwo(["a", "a", "a", "b", "b"]) ); // true
+console.log( checkThreeAndTwo(["a", "b", "c", "b", "c"]) ); // false
