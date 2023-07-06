f4dc6afcdbe15cd99cf79a0b4cfb702054f1e030
Alex Curran <alexcurran.va@gmail.com>
2023-07-06 16:50:15 -0500

    Working solution for homogenous-arrays.js

---
+++ 7-kyu/homogeneous-arrays.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = outer array
+ * m = subarray
+ * time: O(n * m)
+ * space: O(n * m)
+ * 
+ * @param {array[]} arrays 
+ * @returns {array[]}
+ */
+function filterHomogenous(arrays) {
+    return arrays.filter((arr) => {
+        if (arr.length === 0) return false;
+
+        const firstValueType = typeof arr[0];
+
+        for (let i = 1; i < arr.length; i++) {
+            const value = arr[i];
+
+            if (typeof value !== firstValueType) {
+                return false;
+            }
+        }
+
+        return true;
+    });
+}
+
+console.log( filterHomogenous( [[1, 5, 4], ["a", 3, 5], ["b"], [], ["1", 2, 3]]) ); //  [[1, 5, 4], ['b']]
