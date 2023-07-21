d3d9d8dd5f1743330f6c9fbc658b3a94eb73d601
Alex Curran <alexcurran.va@gmail.com>
2023-07-21 10:48:47 -0500

    Working solution for string-chunks.js

---
+++ 7-kyu/string-chunks.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(s * n)
+ * space: O(s)
+ * 
+ * @param {string} str 
+ * @param {number} n 
+ * @returns {string[]}
+ */
+function stringChunk(str, n) {
+    if (typeof n !== "number" || n <= 0) {
+        return [];
+    }
+
+    let results = [];
+
+    for (let i = 0; i < str.length; i += n) {
+        const chunk = str.slice(i, i + n);
+        results.push(chunk);
+    }
+
+    return results;
+}
+
+console.log(stringChunk("codewars", 2)); // ['co', 'de', 'wa', 'rs']
+console.log(stringChunk("thiskataeasy", 4)); // ['this', 'kata', 'easy']
+console.log(stringChunk("hello world", 3)); // ['hel', 'lo ', 'wor', 'ld']
+console.log(stringChunk("sunny day", 0)); // []
