3185c3461db7c46770cfe99e084c4cb2b4a69616
Alex Curran <alexcurran.va@gmail.com>
2023-07-31 09:27:32 -0500

    Working solution for fast-cooking-pancakes.js

---
+++ 7-kyu/fast-cooking-pancakes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @param {number} m 
+ * @returns {number}
+ */
+function cookPancakes(n, m) {
+    if (n < m) return 2;
+
+    return Math.ceil(n / m * 2);
+}
+
+console.log(cookPancakes(1, 2)); // 2
+console.log(cookPancakes(2, 2)); // 2
+console.log(cookPancakes(4, 2)); // 4
