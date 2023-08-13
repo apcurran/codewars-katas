de75582dd756609a042be0ea465f0f0965f3af88
Alex Curran <alexcurran.va@gmail.com>
2023-08-13 13:07:00 -0500

    Working solution for add-a-property-to-an-object.js

---
+++ 7-kyu/add-a-property-to-an-object.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {object} obj 
+ * @param {string} prop 
+ * @param {string|number} value 
+ */
+function addProperty(obj, prop, value) {
+    if (Object.hasOwn(obj, prop)) {
+        throw new Error("Property already exists.");
+    }
+
+    obj[prop] = value;
+}
+
+let obj = {};
+addProperty(obj, "name", "Bob");
+console.log(obj);
+obj.name = "Smith";
+console.log(obj);
