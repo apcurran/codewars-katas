4987e99030b9f6c284466b0a9a21042f9cbf0ee2
Alex Curran <alexcurran.va@gmail.com>
2023-08-24 09:34:06 -0500

    Working solution for help-grinch-steal-christmas-from-js-programmers.js

---
+++ 7-kyu/help-grinch-steal-christmas-from-js-programmers.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @returns {number}
+ */
+Date.prototype.getDate = function getGrinchDate() {
+    const month = this.getMonth();
+    const day = this.getUTCDate();
+
+    return month === 11 && day === 25 ? 26 : day;
+};
+
+const firstDate = new Date("1907-10-25");
+console.log(firstDate.getMonth()); // 9 
+console.log(firstDate.getDate()); // 25 
