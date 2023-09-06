ff28b73a45f50879b6b57d4098e5d7b5a959b6cd
Alex Curran <alexcurran.va@gmail.com>
2023-09-06 09:48:54 -0500

    Regex solution for all-star-code-challenge-3.js

---
+++ 7-kyu/all-star-code-challenge-3.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function removeVowels(str) {
+    const vowels = /[aeiou]/gi;
+
+    return str.replace(vowels, "");
+}
+
+console.log(removeVowels("drake")); // "drk"
