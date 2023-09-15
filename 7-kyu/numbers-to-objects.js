ac8235a50c18e0f8689912cbea17a8de62474727
Alex Curran <alexcurran.va@gmail.com>
2023-09-15 10:02:08 -0500

    Working linear time/space solution for numbers-to-objects.js

---
+++ 7-kyu/numbers-to-objects.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums 
+ * @returns {object[]}
+ */
+function numObj(nums) {
+    let results = [];
+
+    for (let num of nums) {
+        const numCharCodeValue = String.fromCharCode(num);
+        const numObj = {
+            [num]: numCharCodeValue
+        };
+        results.push(numObj);
+    }
+
+    return results;
+}
+
+console.log(numObj([118,117,120])); // [{'118':'v'}, {'117':'u'}, {'120':'x'}]
