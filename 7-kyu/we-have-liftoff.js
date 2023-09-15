d79413bf6e2f0ac029b195bd0b04ed73d89ca905
Alex Curran <alexcurran.va@gmail.com>
2023-09-15 09:47:05 -0500

    Working time(n * log n) solution for we-have-liftoff.js

---
+++ 7-kyu/we-have-liftoff.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} instructions 
+ * @returns {string}
+ */
+function liftoff(instructions) {
+    instructions.sort((a, b) => b - a);
+    const instructionsStr = instructions.join(" ");
+
+    return `${instructionsStr} liftoff!`;
+}
+
+console.log(liftoff([8,1,10,2,7,9,6,3,4,5])); // "10 9 8 7 6 5 4 3 2 1 liftoff!"
