14628c64c9df56881fa9a5a399fe0205b24c582b
Alex Curran <alexcurran.va@gmail.com>
2023-09-21 09:19:30 -0500

    Working solution for hot-singles.js

---
+++ 7-kyu/hot-singles.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * n = arr1 length
+ * m = arr2 length
+ * time: O(n + m)
+ * space: O(n + m)
+ * 
+ * @param {any[]} arr1 
+ * @param {any[]} arr2 
+ * @returns 
+ */
+function hotSingles(arr1, arr2) {
+    const arr1Set = new Set(arr1);
+    const arr2Set = new Set(arr2);
+    let singles = [];
+
+    for (let value of arr1Set) {
+        if (!arr2Set.has(value)) singles.push(value);
+    }
+
+    for (let value of arr2Set) {
+        if (!arr1Set.has(value)) singles.push(value);
+    }
+
+    return singles;
+}
+
+console.log(hotSingles([1, 2, 3, 3], [3, 2, 1, 4, 5]));
+// [4, 5]
+
+console.log(hotSingles(["tartar", "blanket", "cinnamon"], ["cinnamon", "blanket", "domino"]));
+// ["tartar", "domino"]
