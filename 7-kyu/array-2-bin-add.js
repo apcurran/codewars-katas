b45d334ddc9f7f7c74c30056fc76215b063b62f7
Alex Curran <alexcurran.va@gmail.com>
2023-09-22 09:20:08 -0500

    Working linear time/space solution for array-2-bin-add.js

---
+++ 7-kyu/array-2-bin-add.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {any[]} arr 
+ * @returns {string|false}
+ */
+function arr2bin(arr) {
+    let sum = 0;
+
+    for (let value of arr) {
+        if (typeof value !== "number") return false;
+
+        sum += value;
+    }
+
+    return sum.toString(2);
+}
+
+console.log(arr2bin([1,2])); // "11"
+console.log(arr2bin([1,2,"a"])); // false
