da6b36f1a4d8d17f811348c44381bcb2ff03f8b4
Alex Curran <alexcurran.va@gmail.com>
2023-09-25 13:55:39 -0500

    Working solution for string-reordering.js

---
+++ 7-kyu/string-reordering.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {object[]} list 
+ * @returns {string}
+ */
+function sentence(list) {
+    let words = [];
+    let listEntries = list.map((obj) => Object.entries(obj));
+    listEntries.sort((tupleA, tupleB) => {
+        return Number(tupleA[0][0]) - Number(tupleB[0][0]);
+    });
+    
+    for (let entry of listEntries) {
+        words.push(entry[0][1]);
+    }
+
+    return words.join(" ");
+}
+
+const list = [
+    { "4": "dog" },
+    { "2": "took" },
+    { "3": "his" },
+    { "-2": "Vatsan" },
+    { "5": "for" },
+    { "6": "a" },
+    { "12": "spin" }
+];
+console.log(sentence(list)); // "Vatsan took his dog for a spin"
