9211a1d750301b8189e88f5e83abb2113b866604
Alex Curran <alexcurran.va@gmail.com>
2023-10-04 10:06:33 -0500

    Working solution for ordering-the-words.js

---
+++ 7-kyu/ordering-the-words.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function orderWord(str) {
+    if (!str) return "Invalid String!";
+
+    return str
+            .split("")
+            .sort()
+            .join("");
+}
+
+console.log(orderWord("")); // "Invalid String!"
+console.log(orderWord("hello world")); // " dehllloorw"
+console.log(orderWord("!Hi You!")); // " !!HYiou"
