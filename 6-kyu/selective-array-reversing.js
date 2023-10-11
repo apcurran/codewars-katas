03525d04e477a3307f2d9323c43045c0b2be3be7
Alex Curran <alexcurran.va@gmail.com>
2023-10-11 11:10:51 -0500

    Slight optimizations for solution 2 (no extra array copy needed -- skips array.flat() method call)

--- 6-kyu/selective-array-reversing.js
+++ 6-kyu/selective-array-reversing.js
@@ -1,7 +1,32 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = array length
+//  * m = length
+//  * time: O(n * m)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} array 
+//  * @param {number} length 
+//  */
+// function selReverse(array, length) {
+//     if (length === 0) return array;
+
+//     let reversedSections = [];
+
+//     for (let i = 0; i < array.length; i += length) {
+//         const reversedSection = array
+//                                     .slice(i, i + length)
+//                                     .reverse();
+//         reversedSections.push(reversedSection);
+//     }
+
+//     return reversedSections.flat();
+// }
+
 /**
- * solution 1
+ * solution 2 -- slight optimization (no array.flat() copy needed)
  * n = array length
  * m = length
  * time: O(n * m)
@@ -16,13 +41,15 @@ function selReverse(array, length) {
     let reversedSections = [];
 
     for (let i = 0; i < array.length; i += length) {
-        const reversedSection = array
-                                    .slice(i, i + length)
-                                    .reverse();
-        reversedSections.push(reversedSection);
+        const segment = array.slice(i, i + length);
+
+        for (let i = segment.length - 1; i >= 0; i--) {
+            reversedSections.push(segment.pop());
+        }
     }
 
-    return reversedSections.flat();
+    return reversedSections;
 }
 
 console.log(selReverse([1,2,3,4,5,6], 2)); // [2,1, 4,3, 6,5]
+console.log(selReverse([2,4,6,8,10,12,14,16], 3)); // [6,4,2, 12,10,8, 16,14]
