04d100c9fe49d4f46ae70407cf189d8ad16f7381
Alex Curran <alexcurran.va@gmail.com>
2023-11-07 09:36:18 -0600

    Linear solution for min-to-multiple.js

---
+++ 7-kyu/min-to-multiple.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @param {number} x 
+ * @returns {number}
+ */
+function minimum(a, x) {
+    let addToAmount = 0;
+    let subtractFromAmount = 0;
+
+    let addACopy = a;
+
+    while (addACopy % x !== 0) {
+        addToAmount++;
+        addACopy++;
+    }
+    
+    let subtractACopy = a;
+
+    while (subtractACopy % x !== 0) {
+        subtractFromAmount--;
+        subtractACopy--;
+    }
+
+    return Math.min(Math.abs(addToAmount), Math.abs(subtractFromAmount));
+}
+
+console.log(minimum(10, 6)); // 2
