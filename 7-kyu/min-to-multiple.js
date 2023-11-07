12d6189d210ca3f0cbffed8b703e8f84966725be
Alex Curran <alexcurran.va@gmail.com>
2023-11-07 09:39:00 -0600

    Constant time solution for min-to-multiple.js

--- 7-kyu/min-to-multiple.js
+++ 7-kyu/min-to-multiple.js
@@ -1,8 +1,38 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number} a 
+//  * @param {number} x 
+//  * @returns {number}
+//  */
+// function minimum(a, x) {
+//     let addToAmount = 0;
+//     let subtractFromAmount = 0;
+
+//     let addACopy = a;
+
+//     while (addACopy % x !== 0) {
+//         addToAmount++;
+//         addACopy++;
+//     }
+    
+//     let subtractACopy = a;
+
+//     while (subtractACopy % x !== 0) {
+//         subtractFromAmount--;
+//         subtractACopy--;
+//     }
+
+//     return Math.min(Math.abs(addToAmount), Math.abs(subtractFromAmount));
+// }
+
 /**
- * solution 1
- * time: O(n)
+ * solution 2
+ * time: O(1)
  * space: O(1)
  * 
  * @param {number} a 
@@ -10,24 +40,10 @@
  * @returns {number}
  */
 function minimum(a, x) {
-    let addToAmount = 0;
-    let subtractFromAmount = 0;
+    const subtractFromAmount = a % x;
+    const addToAmount = x - subtractFromAmount;
 
-    let addACopy = a;
-
-    while (addACopy % x !== 0) {
-        addToAmount++;
-        addACopy++;
-    }
-    
-    let subtractACopy = a;
-
-    while (subtractACopy % x !== 0) {
-        subtractFromAmount--;
-        subtractACopy--;
-    }
-
-    return Math.min(Math.abs(addToAmount), Math.abs(subtractFromAmount));
+    return Math.min(subtractFromAmount, addToAmount);
 }
 
 console.log(minimum(10, 6)); // 2
