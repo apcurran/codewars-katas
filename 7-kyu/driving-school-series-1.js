f205523914c96249d3b06f8da8e4b40309bece9f
Alex Curran <alexcurran.va@gmail.com>
2023-11-10 09:34:10 -0600

    Working linear time/space solution for driving-school-series-1.js

---
+++ 7-kyu/driving-school-series-1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} list 
+ * @returns {number|"No pass scores registered."}
+ */
+function passed(list) {
+    const validScores = list.filter((score) => score <= 18);
+
+    if (validScores.length === 0) return "No pass scores registered.";
+
+    const validScoresSum = validScores.reduce((sum, curr) => sum + curr, 0);
+
+    return Math.round(validScoresSum / validScores.length);
+}
+
+console.log(passed([10,10,10,18,20,20])); // 12
