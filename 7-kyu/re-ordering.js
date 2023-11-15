c95b32bb2fce3f0f17f6063fc810ad0e6e3c7811
Alex Curran <alexcurran.va@gmail.com>
2023-11-15 10:07:33 -0600

    Working linear solution for re-ordering.js

---
+++ 7-kyu/re-ordering.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} text 
+ * @returns {string}
+ */
+function reOrdering(text) {
+    if (text[0] === text[0].toUpperCase()) {
+        return text;
+    }
+
+    let words = text.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (word[0] === word[0].toUpperCase()) {
+            // uppercase first letter word
+            const removedWord = words.splice(i, 1);
+            // move to front
+            words.unshift(removedWord);
+        }
+    }
+
+    return words.join(" ");
+}
+
+console.log(reOrdering("ming Yao")); // "Yao ming"
