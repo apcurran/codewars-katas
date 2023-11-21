494d914bbd515323a38efc97877b9a00518546d4
Alex Curran <alexcurran.va@gmail.com>
2023-11-21 09:59:58 -0600

    Working solution for sort-by-example.js

---
+++ 7-kyu/sort-by-example.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @param {number[]} exampleArr 
+ * @returns {number[]}
+ */
+function exampleSort(arr, exampleArr) {
+    return arr.sort((a, b) => exampleArr.indexOf(a) - exampleArr.indexOf(b));
+}
+
+console.log(exampleSort([1,3,4,4,4,4,5], [4,1,2,3,5]));
+// [4,4,4,4,1,3,5]
