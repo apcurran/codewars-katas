0afce475f0ab17e2a6f37762fcbad38a2dcfbbd6
Alex Curran <alexcurran.va@gmail.com>
2023-11-24 16:02:01 -0600

    Working solution for sort-reloaded-method.js

---
+++ 7-kyu/sort-reloaded-method.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {"asc"|"desc"} dir 
+ * @returns {number[]|false}
+ */
+Array.prototype.sortReloaded = function(dir) {
+    const currentArr = this;
+
+    if (dir && dir !== "asc" && dir !== "desc") {
+        return false;
+    }
+
+    return currentArr.slice().sort((a, b) => {
+        if (dir === "asc" || !dir) {
+            return a - b;
+        } else {
+            return b - a;
+        }
+    });
+};
+
+console.log([3,1,5,3,6,1,2].sortReloaded("asc")); // [1,1,2,3,3,5,6]
