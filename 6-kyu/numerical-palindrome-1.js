e7ba667146986df653e936f26b7b11840b2b34c6
Alex Curran <alexcurran.va@gmail.com>
2023-11-27 16:50:04 -0600

    Working solution for numerical-palindrome-1.js

---
+++ 6-kyu/numerical-palindrome-1.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1 -- two pointers
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} num 
+ * @returns {boolean|"Not valid"}
+ */
+function palindrome(num) {
+    if (num < 0 || !Number.isInteger(num)) {
+        return "Not valid";
+    }
+
+    const strNum = String(num);
+    let left = 0;
+    let right = strNum.length - 1;
+
+    while (left < right) {
+        const leftValue = strNum[left];
+        const rightValue = strNum[right];
+
+        if (leftValue !== rightValue) return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(palindrome(2332)); // true
