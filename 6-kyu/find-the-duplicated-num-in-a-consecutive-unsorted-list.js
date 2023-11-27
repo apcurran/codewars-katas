93621ac7cc3a9749a1b578c2d6a42ed9c799197f
Alex Curran <alexcurran.va@gmail.com>
2023-11-27 16:34:45 -0600

    Hashset solution for find-the-duplicated-num-in-a-consecutive-unsorted-list.js

---
+++ 6-kyu/find-the-duplicated-num-in-a-consecutive-unsorted-list.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function findDup(arr) {
+    let seenNums = new Set();
+
+    for (let num of arr) {
+        if (seenNums.has(num)) return num;
+
+        seenNums.add(num);
+    }
+
+    return -1;
+}
+
+console.log(findDup([1,3,2,5,4,5,7,6])); // 5
