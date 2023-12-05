b1de87ce9cd4625a147a7a830191307f7a94dbd8
Alex Curran <alexcurran.va@gmail.com>
2023-12-05 10:42:59 -0600

    Working linear solution for excessively-abundant-nums.js

---
+++ 7-kyu/excessively-abundant-nums.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function abundantNumber(num) {
+    let divisorsSum = 0;
+
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) {
+            // add divisor to sum
+            divisorsSum += i;
+        }
+    }
+
+    return divisorsSum > num;
+}
+
+console.log(abundantNumber(12)); // true
