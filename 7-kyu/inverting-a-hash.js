a85751f53dcfde5eb28e9d40c6fc93e4a58bea7d
Alex Curran <alexcurran.va@gmail.com>
2023-12-12 10:26:50 -0600

    Working solution for inverting-a-hash.js

---
+++ 7-kyu/inverting-a-hash.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Object} hash 
+ * @returns {Object}
+ */
+function invertHash(hash) {
+    let result = {};
+
+    for (let key in hash) {
+        const value = hash[key];
+        result[value] = key;
+    }
+
+    return result;
+}
+
+console.log(invertHash({ a: "1", b: "2", c: "3" })); // { 1: 'a', 2: 'b', 3: 'c' }
