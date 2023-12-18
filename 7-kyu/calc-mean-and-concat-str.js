dece18d2371728ee25661e410a67efc6d8c3c508
Alex Curran <alexcurran.va@gmail.com>
2023-12-18 10:24:53 -0600

    Working linear solution for calc-mean-and-concat-str.js

---
+++ 7-kyu/calc-mean-and-concat-str.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {any[]} list 
+ * @returns {[number, string]}
+ */
+function mean(list) {
+    let sum = 0;
+    let letters = "";
+
+    for (let value of list) {
+        const numValue = Number(value);
+
+        if (Number.isNaN(numValue)) {
+            letters += value;
+        } else {
+            sum += numValue;
+        }
+    }
+
+    const mean = sum / 10;
+
+    return [mean, letters];
+}
+
+console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]));
+// [3.6, "udiwstagwo"]
