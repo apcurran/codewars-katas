7ac7184f67b406c639f6195ca6f93a1a0c9e74f8
Alex Curran <alexcurran.va@gmail.com>
2023-12-22 10:10:17 -0600

    Working linear time/space solution for exclamation-marks-series-4.js

---
+++ 8-kyu/exclamation-marks-series-4.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function remove(str) {
+    const removedMarks = str.replaceAll("!", "");
+
+    return `${removedMarks}!`;
+}
+
+console.log(remove("Hi!!!")); // "Hi!"
+console.log(remove("Hi! Hi!")); // "Hi Hi!"
