7516c456496aaaca4b0f44591218716b4bdbfae2
Alex Curran <alexcurran.va@gmail.com>
2023-12-26 10:03:00 -0600

    Optimized, non-regexp solution

--- 7-kyu/exclamation-marks-13.js
+++ 7-kyu/exclamation-marks-13.js
@@ -1,20 +1,43 @@
 "use strict";
 
+// /**
+//  * solution 1 -- regex
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} string 
+//  * @returns {number}
+//  */
+// function product(string) {
+//     const questionMarks = string.match(/\?/g);
+//     const exclamationMarks = string.match(/!/g);
+
+//     if (!questionMarks || !exclamationMarks) return 0;
+    
+//     return questionMarks.length * exclamationMarks.length;
+// }
+
 /**
- * solution 1
+ * solution 2 -- optimized
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {string} string 
  * @returns {number}
  */
 function product(string) {
-    const questionMarks = string.match(/\?/g);
-    const exclamationMarks = string.match(/!/g);
+    let exclamationMarksCount = 0;
+    let questionMarksCount = 0;
 
-    if (!questionMarks || !exclamationMarks) return 0;
-    
-    return questionMarks.length * exclamationMarks.length;
+    for (let char of string) {
+        if (char === "!") {
+            exclamationMarksCount++;
+        } else if (char === "?") {
+            questionMarksCount++;
+        }
+    }
+
+    return exclamationMarksCount * questionMarksCount;
 }
 
 console.log(product("")); // 0
