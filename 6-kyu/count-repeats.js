483227653928e1181fdcab9216494e76a354f366
Alex Curran <alexcurran.va@gmail.com>
2023-12-27 09:34:37 -0600

    Working linear time & constant space solution for count-repeats.js

---
+++ 6-kyu/count-repeats.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function countRepeats(str) {
+    let count = 0;
+    let previousChar = null;
+    
+    for (let char of str) {
+        if (char === previousChar) {
+            count++;
+        } else {
+            previousChar = char;
+        }
+    }
+
+    return count;
+}
+
+console.log(countRepeats("abc")); // 0
+console.log(countRepeats("abbbbc")); // 3
