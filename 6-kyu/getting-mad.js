123d88fc71b393895911e2a3adf63fb3d8ba8bad
Alex Curran <alexcurran.va@gmail.com>
2024-01-03 09:58:43 -0600

    Optimize sort solution for getting-mad.js

--- 6-kyu/getting-mad.js
+++ 6-kyu/getting-mad.js
@@ -1,26 +1,51 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} arr 
+//  * @returns {number} min abs diff
+//  */
+// function getting_mad(arr) {
+//     let minAbsDiff = Math.abs(arr[0] - arr[1]);
+
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length; j++) {
+//             // do not check for outer current value with itself
+//             if (j === i) continue;
+
+//             const num1 = arr[i];
+//             const num2 = arr[j];
+//             const absDiff = Math.abs(num1 - num2);
+//             minAbsDiff = Math.min(minAbsDiff, absDiff);
+//         }
+//     }
+
+//     return minAbsDiff;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
- * space: O(1)
+ * solution 2 -- sort
+ * time: O(n * log n)
+ * space: O(n)
  * 
  * @param {number[]} arr 
  * @returns {number} min abs diff
  */
 function getting_mad(arr) {
-    let minAbsDiff = Math.abs(arr[0] - arr[1]);
+    arr.sort(function sortDesc(a, b) {
+        return b - a;
+    });
+    
+    let minAbsDiff = Infinity;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            // do not check for outer current value with itself
-            if (j === i) continue;
-
-            const num1 = arr[i];
-            const num2 = arr[j];
-            const absDiff = Math.abs(num1 - num2);
-            minAbsDiff = Math.min(minAbsDiff, absDiff);
-        }
+    for (let i = 0; i < arr.length - 1; i++) {
+        const currentNum = arr[i];
+        const nextNum = arr[i + 1];
+        const diff = currentNum - nextNum;
+        minAbsDiff = Math.min(minAbsDiff, diff);
     }
 
     return minAbsDiff;
