5412de27cfca4eb9b3f00fa40d7f7f30ee7f73c4
Alex Curran <alexcurran.va@gmail.com>
2024-01-05 11:07:34 -0600

    Working linear time/space solution for sum-of-a-beach.js

---
+++ 7-kyu/sum-of-a-beach.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} beach 
+ * @returns {number}
+ */
+function sumOfABeach(beach) {
+    const matches = beach.match(/(sand|water|fish|sun)/gi);
+
+    return matches ? matches.length : 0;
+}
+
+console.log(sumOfABeach("GolDeNSanDyWateRyBeaChSuNN")); // 3
+console.log(sumOfABeach("gOfIshsunesunFiSh")); // 4
+console.log(sumOfABeach("cItYTowNcARShoW")); // 0
