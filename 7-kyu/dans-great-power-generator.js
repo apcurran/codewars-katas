43c436a03dd40df069496bd6ed436b6d22a57d71
Alex Curran <alexcurran.va@gmail.com>
2024-01-11 09:52:16 -0600

    Working solution for dans-great-power-generator.js

---
+++ 7-kyu/dans-great-power-generator.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} num 
+ * @param {number} power 
+ * @returns {number}
+ */
+function danspower(num, power) {
+    const result = num ** power;
+
+    if (result % 2 === 0) {
+        // even value
+        return result;
+    } else {
+        return Math.round(result / 10) * 10;
+    }
+}
+
+console.log(danspower(7, 2)); // 50
+console.log(danspower(8, 1)); // 8
