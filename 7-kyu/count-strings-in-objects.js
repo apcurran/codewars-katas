9e03e3adaf12b42224a908a96b7877b8c0334f0c
Alex Curran <alexcurran.va@gmail.com>
2024-01-12 10:21:15 -0600

    Working solution for count-strings-in-objects.js

---
+++ 7-kyu/count-strings-in-objects.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {object} obj 
+ * @returns {number}
+ */
+function strCount(obj) {
+    let stringsCount = 0;
+
+    /**
+     * @param {object} obj 
+     * @returns {void}
+     */
+    function recurse(obj) {
+        for (let key in obj) {
+            if (typeof obj[key] === "object" && obj[key] !== null) {
+                recurse(obj[key]);
+            } else {
+                if (typeof obj[key] === "string") {
+                    stringsCount++;
+                }
+            }
+        }
+    }
+
+    recurse(obj);
+
+    return stringsCount;
+}
+
+console.log(strCount(
+    {
+        first: "1",
+        second: "2",
+        third: false,
+        fourth: ["anytime", 2, 3, 4],
+        fifth: null
+    }
+));
