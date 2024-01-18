87b224ce66bb6f69d252f26932e9826f47dcf908
Alex Curran <alexcurran.va@gmail.com>
2024-01-18 13:39:16 -0600

    Working solution for lucky-bus-ticket.js

---
+++ 7-kyu/lucky-bus-ticket.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1) -- st length 6
+ * space: O(1) -- str length 6
+ * 
+ * @param {string} ticket 
+ * @returns {boolean}
+ */
+function isLucky(ticket) {
+    // ticket must be 6 digits long
+    if (ticket.length !== 6) return false;
+
+    // must not include letters
+    if (ticket.match(/[a-z]/g)) return false;
+
+    const firstThreeDigitsSum = Number(ticket[0]) + Number(ticket[1]) + Number(ticket[2]);
+    const lastThreeDigitsSum = Number(ticket[3]) + Number(ticket[4]) + Number(ticket[5]);
+
+    return firstThreeDigitsSum === lastThreeDigitsSum;
+}
+
+console.log(isLucky("123321")); // true
+console.log(isLucky("12a21a")); // false
