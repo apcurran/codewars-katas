65f23b206ccfbe631898cb7806a9b7c8b543d7a3
Alex Curran <alexcurran.va@gmail.com>
2024-01-23 09:33:08 -0600

    Working solution for sum-times-tables.js

---
+++ 7-kyu/sum-times-tables.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * t = tables length
+ * time: O(t)
+ * space: O(1)
+ * 
+ * @param {number[]} tables 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+function sumTimesTables(tables, min, max) {
+    let sum = 0;
+
+    for (let value of tables) {
+        sum += value;
+    }
+
+    return sum * (min + max) / 2 * (max - min + 1);
+}
+
+console.log(sumTimesTables([2, 3], 1, 3)); // 30
+console.log(sumTimesTables([1, 3, 5], 1, 1)); // 9
+console.log(sumTimesTables([1, 3, 5], 1, 10)); // 495
