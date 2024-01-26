dd9c305eab67577c6d7ee0a0c9d323ca8d2b76ee
Alex Curran <alexcurran.va@gmail.com>
2024-01-26 12:03:31 -0600

    Working linear time/space solution for nothing-special.js

---
+++ 7-kyu/nothing-special.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function nothingSpecial(str) {
+    if (typeof str !== "string") return "Not a string!";
+
+    const specialChars = /[^a-z\d\s]/gi;
+
+    return str.replace(specialChars, "");
+}
+
+console.log(nothingSpecial("%^Take le$ft ##quad%r&a&nt")); // "Take left quadrant"
