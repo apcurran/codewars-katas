37c8e1ad60a4c69c2e8ac8e7d7e6f91cf7dd47e9
Alex Curran <alexcurran.va@gmail.com>
2024-01-26 16:57:16 -0600

    Working solution for monkeys-math-01-how-many-zeros.js

---
+++ 7-kyu/monkeys-math-01-how-many-zeros.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * time: O(1)
+ * 
+ * @param {string} s 
+ * @returns {number}
+ */
+function countzero(s) {
+    const oneZeroChars = "abdegopq069DOPQR";
+    const twoZerosChars = "%&B8";
+    let totalZeros = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i];
+
+        if (currentChar === "(" && s[i + 1] === ")" || oneZeroChars.includes(currentChar)) {
+            totalZeros += 1;
+        } else if (twoZerosChars.includes(currentChar)) {
+            totalZeros += 2;
+        }
+    }
+
+    return totalZeros;
+}
+
+console.log(countzero("")); // 0
+console.log(countzero("0")); // 1
+console.log(countzero("1234567890")); // 5
