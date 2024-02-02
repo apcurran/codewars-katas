8aae9cdbf67cbdecb69eebdb73954e81ea59a4d8
Alex Curran <alexcurran.va@gmail.com>
2024-02-02 12:19:21 -0600

    Working solution for what-is-type-of-var.js

---
+++ 7-kyu/what-is-type-of-var.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {any} value 
+ * @returns {string}
+ */
+function type(value) {
+    if (Array.isArray(value)) {
+        return "array";
+    } else if (value instanceof Date) {
+        return "date";
+    } else if (value === null) {
+        return "null";
+    }
+
+    return typeof value;
+}
