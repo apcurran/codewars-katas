ffca19d19fa66047953fd179024cc819525e332f
Alex Curran <alexcurran.va@gmail.com>
2024-02-05 10:26:55 -0600

    Alt solution in O(n * log n) time

--- 7-kyu/mirror-mirror-on-the-wall.js
+++ 7-kyu/mirror-mirror-on-the-wall.js
@@ -1,31 +1,49 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2) -- array.unshift() within loop
+//  * space: O(n)
+//  * 
+//  * @param {number[]} data 
+//  * @returns {number[]}
+//  */
+// function mirror(data) {
+//     if (data.length === 0) return [];
+
+//     const sortedData = data
+//         .slice()
+//         .sort(function sortDesc(a, b) {
+//             return b - a;
+//         });
+//     const middleValue = sortedData[0];
+//     let results = [middleValue];
+
+//     for (let i = 1; i < sortedData.length; i++) {
+//         const value = sortedData[i];
+//         results.push(value);
+//         results.unshift(value);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1
- * time: O(n^2) -- array.unshift() within loop
+ * solution 2
+ * time: O(n * log n) -- sort
  * space: O(n)
  * 
  * @param {number[]} data 
  * @returns {number[]}
  */
 function mirror(data) {
-    if (data.length === 0) return [];
-
     const sortedData = data
         .slice()
-        .sort(function sortDesc(a, b) {
-            return b - a;
+        .sort(function sortAsc(a, b) {
+            return a - b;
         });
-    const middleValue = sortedData[0];
-    let results = [middleValue];
 
-    for (let i = 1; i < sortedData.length; i++) {
-        const value = sortedData[i];
-        results.push(value);
-        results.unshift(value);
-    }
-
-    return results;
+    return [...sortedData, ...sortedData.reverse().slice(1)];
 }
 
 console.log(mirror([-8, 42, 18, 0, -16])); // [-16, -8, 0, 18, 42, 18, 0, -8, -16]
