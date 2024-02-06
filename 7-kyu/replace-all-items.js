55acfc2e916ab8ec5dcce023aa7c73a6e85a7981
Alex Curran <alexcurran.va@gmail.com>
2024-02-06 09:28:12 -0600

    Working solution for replace-all-items.js

---
+++ 7-kyu/replace-all-items.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {array|string} seq 
+ * @param {number} find 
+ * @param {number} replace 
+ * @returns {array|string}
+ */
+function replaceAll(seq, find, replace) {
+    let results = [];
+
+    for (let i = 0; i < seq.length; i++) {
+        const current = seq[i];
+
+        if (current === find) {
+            results.push(replace);
+        } else {
+            results.push(current);
+        }
+    }
+
+    if (typeof seq === "string") {
+        return results.join("");
+    } else {
+        return results;
+    }
+}
+
+console.log(replaceAll([], 1, 2)); // []
+console.log(replaceAll([1,2,2], 1, 2)); // [2, 2, 2]
