3321c4566527f639022e795e8008cb25683d77ad
Alex Curran <alexcurran.va@gmail.com>
2024-02-12 09:13:06 -0600

    Working solution for flick-switch.js

---
+++ 8-kyu/flick-switch.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[]} words 
+ * @returns {boolean[]}
+ */
+function flickSwitch(words) {
+    let flickFlag = true;
+    let results = [];
+
+    for (let word of words) {
+        if (word === "flick") {
+            flickFlag = !flickFlag;
+        }
+
+        results.push(flickFlag);
+    }
+
+    return results;
+}
+
+console.log(flickSwitch(["codewars", "flick", "code", "wars"])); // [True, False, False, False]
