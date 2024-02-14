a532b8477102ba922c4e21f2fd65d57da24b037a
Alex Curran <alexcurran.va@gmail.com>
2024-02-14 13:46:01 -0600

    Working solution for object-extend.js

---
+++ 5-kyu/object-extend.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = objects length
+ * m = properties in largest object
+ * x = total unique properties within all objects
+ * time: O(n * m)
+ * space: O(x)
+ * 
+ * @param {...object} objects
+ * @returns {object}
+ */
+function extend(...objects) {
+    return objects
+            .filter(isObject)
+            .reduce((fullObject, currentObject) => {
+                return { ...currentObject, ...fullObject };
+            }, {});
+}
+
+console.log(extend({a: 1, b: 2}, {c: 3}));
+// {a: 1, b: 2, c: 3}
