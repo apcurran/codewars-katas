caa21bbabf51b63c15244586a7559b86049d8b8f
Alex Curran <alexcurran.va@gmail.com>
2024-02-21 09:27:23 -0600

    Working solution for simple-fun-152-invite-more-women.js

---
+++ 7-kyu/simple-fun-152-invite-more-women.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} people 
+ * @returns {boolean}
+ */
+function inviteMoreWomen(people) {
+    let womenCount = 0;
+    let menCount = 0;
+
+    for (let person of people) {
+        if (person === -1) {
+            womenCount++;
+        }
+
+        if (person === 1) {
+            menCount++;
+        }
+    }
+
+    return womenCount < menCount;
+}
+
+console.log(inviteMoreWomen([1, -1, 1])); // true
+console.log(inviteMoreWomen([1, 1, 1])); // true
+console.log(inviteMoreWomen([-1, -1, -1])); // false
+console.log(inviteMoreWomen([1, -1])); // false
