45c3b8f08157ea1da2ff9bb8883d278d6fd895c3
Alex Curran <alexcurran.va@gmail.com>
2024-02-23 09:32:54 -0600

    Working solution for show-multiples-of-2-nums-within-a-range.js

---
+++ 7-kyu/show-multiples-of-2-nums-within-a-range.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- not including results
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @param {number} upperLimit 
+ * @returns {number[]}
+ */
+function multiples(a, b, upperLimit) {
+    let multiples = [];
+
+    for (let i = 1; i < upperLimit; i++) {
+        if (i % a === 0 && i % b === 0) {
+            multiples.push(i);
+        }
+    }
+
+    return multiples;
+}
+
+console.log(multiples(2, 4, 40)); // [4, 8, 12, 16, 20, 24, 28, 32, 36]
