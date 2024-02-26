35dcf5fd5f8c072f712c8459e4822406309c70a9
Alex Curran <alexcurran.va@gmail.com>
2024-02-26 11:18:23 -0600

    Working linear time solution for return-a-strings-even-chars.js

---
+++ 7-kyu/return-a-strings-even-chars.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- not including results
+ * 
+ * @param {string} str 
+ * @returns {string[]|"invalid string"}
+ */
+function evenChars(str) {
+    if (str.length < 2 || str.length > 100) return "invalid string";
+
+    let evenValues = [];
+
+    for (let i = 1; i < str.length; i += 2) {
+        evenValues.push(str[i]);
+    }
+
+    return evenValues;
+}
+
+console.log(evenChars("abcdefghijklm")); // ["b", "d", "f", "h", "j", "l"]
+console.log(evenChars("a")); // "invalid string"
