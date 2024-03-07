c33757e67dcbe0ed59d0163478b1f010d09db33c
Alex Curran <alexcurran.va@gmail.com>
2024-03-07 10:13:17 -0600

    Working linear solution for from-a-to-z.js

---
+++ 7-kyu/from-a-to-z.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- without counting result space
+ * 
+ * @param {string} lettersRange 
+ * @returns {string}
+ */
+function gimmeTheLetters(lettersRange) {
+    const startCharCode = lettersRange[0].charCodeAt(0);
+    const endCharCode = lettersRange[2].charCodeAt(0);
+    let result = "";
+
+    for (let i = startCharCode; i <= endCharCode; i++) {
+        const letter = String.fromCharCode(i);
+        result += letter;
+    }
+
+    return result;
+}
+
+console.log(gimmeTheLetters("a-z")); // "abcdefghijklmnopqrstuvwxyz"
+console.log(gimmeTheLetters("h-o")); // "hijklmno"
+console.log(gimmeTheLetters("Q-Z")); // "QRSTUVWXYZ"
+console.log(gimmeTheLetters("J-J")); // "J"
