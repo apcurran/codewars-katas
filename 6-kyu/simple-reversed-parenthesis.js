6b69ffce04714a32d821ac39b1d56cf5e3655f1a
Alex Curran <alexcurran.va@gmail.com>
2024-03-22 12:03:07 -0500

    Counters and formula solution for simple-reversed-parenthesis.js

---
+++ 6-kyu/simple-reversed-parenthesis.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- counters and formula
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} s 
+ * @returns {number}
+ */
+function solve(s) {
+    if (s.length % 2 !== 0) return -1;
+
+    let result = 0;
+    let k = 0;
+
+    for (let paren of s) {
+        if (paren === "(") {
+            k++;
+        } else if (paren === ")" && k > 0) {
+            k--;
+        } else {
+            k++;
+            result++;
+        }
+    }
+
+    return result + k / 2;
+}
