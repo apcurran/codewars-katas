3865da2cd8de036c1f75b96c167a503148a886c0
Alex Curran <alexcurran.va@gmail.com>
2024-03-25 10:26:05 -0500

    charCodeAt method solution for is-it-a-letter.js

---
+++ 7-kyu/is-it-a-letter.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1 -- charCodeAt method
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {string} character 
+ * @returns {boolean}
+ */
+function isItLetter(character) {
+    const charCode = character.charCodeAt(0);
+    const isUppercaseLetter = charCode >= 65 && charCode <= 90;
+    const isLowercaseLetter = charCode >= 97 && charCode <= 122;
+
+    return isUppercaseLetter || isLowercaseLetter;
+}
+
+console.log(isItLetter("a")); // true
+console.log(isItLetter("1")); // false
