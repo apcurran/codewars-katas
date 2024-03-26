a010c96921f6e86b3ab2954832f0739f4bc203e7
Alex Curran <alexcurran.va@gmail.com>
2024-03-26 09:14:54 -0500

    Linear time/space solution for working-with-arrays.js

---
+++ 7-kyu/working-with-arrays.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]} without the last element
+ */
+function withoutLast(arr) {
+    return arr.slice(0, -1);
+}
+
+console.log(withoutLast([1, 2, 3, 4, 5])); // [1, 2, 3, 4]
+
