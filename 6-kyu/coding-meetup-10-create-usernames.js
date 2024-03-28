9e1b49518d12a85509e4cd1718a28002a249731e
Alex Curran <alexcurran.va@gmail.com>
2024-03-28 17:01:40 -0500

    Working solution for coding-meetup-10-create-usernames.js

---
+++ 6-kyu/coding-meetup-10-create-usernames.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/**
+ * solution 1 -- map method
+ * n = list length
+ * m = person's first and last names
+ * time: O(n * m)
+ * space: O(n + m)
+ * 
+ * @param {{
+ *  firstName: String,
+ *  lastName: String,
+ *  country: String,
+ *  continent: String,
+ *  age: Number,
+ *  language: String
+ * }[]} list
+ * @returns {{
+ *  firstName: String,
+ *  lastName: String,
+ *  country: String,
+ *  continent: String,
+ *  age: Number,
+ *  language: String,
+ *  username: String
+ * }[]}
+ */
+function addUsername(list) {
+    const CURRENT_YEAR = 2020;
+
+    return list.map(function updateWithUsername(person) {
+        const birthYear = CURRENT_YEAR - person.age;
+        const username = person.firstName.toLowerCase() + person.lastName[0].toLowerCase() + birthYear;
+
+        return {
+            ...person,
+            username
+        };
+    });
+}
+
+const list1 = [
+    { firstName: "Emily", lastName: "N.", country: "Ireland", continent: "Europe", age: 30, language: "Ruby" },
+    { firstName: "Nor", lastName: "E.", country: "Malaysia", continent: "Asia", age: 20, language: "Clojure" }
+];
+console.log(addUsername(list1));
+// [
+//     {
+//         firstName: "Emily", lastName: "N.", country: "Ireland", continent: "Europe", age: 30, language: "Ruby",
+//         username: "emilyn1990"
+//     },
+//     {
+//         firstName: "Nor", lastName: "E.", country: "Malaysia", continent: "Asia", age: 20, language: "Clojure",
+//         username: "nore2000"
+//     }
+// ];
