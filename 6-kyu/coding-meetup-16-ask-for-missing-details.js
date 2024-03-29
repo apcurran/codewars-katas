2ebd1bf0d87cfb45f080668da76471b022288e90
Alex Curran <alexcurran.va@gmail.com>
2024-03-29 14:12:45 -0500

    Working filter + map solution for coding-meetup-16-ask-for-missing-details.js

---
+++ 6-kyu/coding-meetup-16-ask-for-missing-details.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/**
+ * @typedef {{
+ *   firstName: String,
+ *   lastName: String,
+ *   country: String,
+ *   continent: String,
+ *   age: Number,
+ *   language: String,
+ * }} Person
+*/
+
+/**
+ * solution 1
+ * n = list length
+ * m = Person properties
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {Person[]} list 
+ * @returns {{
+ *  firstName: String,
+ *   lastName: String,
+ *   country: String,
+ *   continent: String,
+ *   age: Number,
+ *   language: String,
+ *   question: String,
+ * }[]}
+ */
+function askForMissingDetails(list) {
+    // filter
+    return list
+        .filter(function getMissingDetailsDevs(person) {
+            for (let prop in person) {
+                const value = person[prop];
+
+                if (value === null) {
+                    // keep in array
+                    return true;
+                }
+            }
+        })
+        .map(function addQuestionFieldToMissingDetailsDevs(person) {
+            let missingProp = "";
+
+            for (let prop in person) {
+                const value = person[prop];
+
+                if (value === null) {
+                    missingProp = prop;
+
+                    break;
+                }
+            }
+
+            const questionValue = `Hi, could you please provide your ${missingProp}.`;
+
+            return {
+                ...person,
+                question: questionValue
+            };
+        });
+}
+
+const list1 = [
+    { firstName: null, lastName: "I.", country: "Argentina", continent: "Americas", age: 35, language: "Java" },
+    { firstName: "Lukas", lastName: "X.", country: "Croatia", continent: "Europe", age: 35, language: null },
+    { firstName: "Madison", lastName: "U.", country: "United States", continent: "Americas", age: 32, language: "Ruby" }
+];
+
+console.log(askForMissingDetails(list1));
+// [
+//     {
+//         firstName: null, lastName: "I.", country: "Argentina", continent: "Americas", age: 35, language: "Java",
+//         question: "Hi, could you please provide your firstName."
+//     },
+//     {
+//         firstName: "Lukas", lastName: "X.", country: "Croatia", continent: "Europe", age: 35, language: null,
+//         question: "Hi, could you please provide your language."
+//     }
+// ];
