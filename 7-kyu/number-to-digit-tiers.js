d32d2876cb0ed1e39bd7407bb8baf1f040c02c59
Alex Curran <alexcurran.va@gmail.com>
2024-04-01 11:12:43 -0500

    Working iterative solution for number-to-digit-tiers.js

---
+++ 7-kyu/number-to-digit-tiers.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = strNum length
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number} num 
+ * @returns {string[]}
+ */
+function createArrayOfTiers(num) {
+    const strNum = String(num);
+    let results = [];
+
+    for (let i = 1; i < strNum.length + 1; i++) {
+        const chunk = strNum.slice(0, i);
+        results.push(chunk);
+    }
+
+    return results;
+}
+
+console.log(createArrayOfTiers(2017)); // ["2", "20", "201", "2017"]
