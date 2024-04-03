3c96cc8cd6d4c367dc2e506810b2b1e853fa679f
Alex Curran <alexcurran.va@gmail.com>
2024-04-03 09:44:08 -0500

    Log solution for plant-doubling.js

---
+++ 7-kyu/plant-doubling.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(log n)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {number}
+ */
+function plantDoubling(n) {
+    let plantsCount = 1;
+
+    while (n !== 1) {
+        // there is a remainder
+        if (n % 2 === 1) {
+            n--;
+            plantsCount++;
+        } else {
+            // no remainder
+            n /= 2;
+        }
+    }
+
+    return plantsCount;
+}
+
+console.log(plantDoubling(5)); // 2
+console.log(plantDoubling(8)); // 1
