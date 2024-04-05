9734ad1877b13cad563df670b045e5b2054aa696
Alex Curran <alexcurran.va@gmail.com>
2024-04-05 09:40:20 -0500

    Sting.slice() solution for simple-string-reversal-2.js

---
+++ 7-kyu/simple-string-reversal-2.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {string}
+ */
+function solve(str, a, b) {
+    const startChunk = str.slice(0, a);
+    const reversedChunk = str.slice(a, b + 1).split("").reverse().join("");
+    const endChunk = str.slice(b + 1);
+
+    return startChunk + reversedChunk + endChunk;
+}
+
+console.log(solve("codewars", 1, 5)); // "cawedors"
+console.log(solve("cODEWArs", 1, 5)); // "cAWEDOrs"
+
