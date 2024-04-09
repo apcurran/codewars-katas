125043c753f50fe13a3883d544dbb5223bd2d14e
Alex Curran <alexcurran.va@gmail.com>
2024-04-09 09:26:06 -0500

    Optimal solution

--- 7-kyu/lucky-number.js
+++ 7-kyu/lucky-number.js
@@ -1,22 +1,34 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number} n 
+//  * @returns {boolean}
+//  */
+// function isLucky(n) {
+//     const digitsStr = String(n).split("");
+//     let digitsSum = 0;
+
+//     for (let digitStr of digitsStr) {
+//         digitsSum += Number(digitStr);
+//     }
+
+//     return digitsSum === 0 || digitsSum % 9 === 0;
+// }
+
 /**
- * solution 1
- * time: O(n)
- * space: O(n)
+ * solution 2
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} n 
  * @returns {boolean}
  */
 function isLucky(n) {
-    const digitsStr = String(n).split("");
-    let digitsSum = 0;
-
-    for (let digitStr of digitsStr) {
-        digitsSum += Number(digitStr);
-    }
-
-    return digitsSum === 0 || digitsSum % 9 === 0;
+    return n === 0 || n % 9 === 0;
 }
 
 console.log(isLucky(1892376)); // 36 (true)
