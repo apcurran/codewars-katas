5b020d701c3442c3f2fd5a0d7705ade44da07822
Alex Curran <alexcurran.va@gmail.com>
2024-04-11 11:50:49 -0500

    Working hashset solution for find-twins.js

---
+++ 7-kyu/find-twins.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number|null}
+ */
+function elimination(arr) {
+    let seenNums = new Set();
+
+    for (let num of arr) {
+        if (seenNums.has(num)) return num;
+
+        seenNums.add(num);
+    }
+
+    return null;
+}
+
+console.log(elimination([2, 3, 6, 34, 7, 8, 2])); // 2
+console.log(elimination([2,5,34,1,22])); // null
