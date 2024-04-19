4fe9ecdc705daedf9068a30a1f57f85199ba2a41
Alex Curran <alexcurran.va@gmail.com>
2024-04-19 09:26:12 -0500

    Working generator func for multiplication-generators-2.js

---
+++ 7-kyu/multiplication-generators-2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- iteration
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @returns {Generator} "a x b = c" format
+ */
+function* generator(a) {
+    let b = 1;
+
+    while (true) {
+        const c = a * b;
+
+        yield `${a} x ${b} = ${c}`;
+
+        b++;
+    }
+}
+
+const myGenerator = generator(1);
+console.log(myGenerator.next().value);
+console.log(myGenerator.next().value);
+console.log(myGenerator.next().value);
+console.log(myGenerator.next().value);
+console.log(myGenerator.next().value);
