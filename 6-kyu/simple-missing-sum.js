6d047fbcf177f255915364d30fd05e2339dc21d4
Alex Curran <alexcurran.va@gmail.com>
2024-04-22 15:08:46 -0500

    Sort solution for simple-missing-sum.js

---
+++ 6-kyu/simple-missing-sum.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function solve(arr) {
+    arr.sort(function sortAsc(a, b) {
+        return a - b;
+    });
+    let sum = 1; // default smallest
+
+    for (let i = 0; i < arr.length && arr[i] <= sum; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+console.log(solve([1,2,8,7])); // 4
+console.log(solve([4,1,2,3,12])); // 11
+console.log(solve([2,3,2,3,4,2,12,3])); // 1
