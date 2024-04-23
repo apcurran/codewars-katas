9a2e4c62611523d52da2c6b960f297a47888547d
Alex Curran <alexcurran.va@gmail.com>
2024-04-23 09:46:30 -0500

    Working solution for sum-array-with-different-bases.js

---
+++ 7-kyu/sum-array-with-different-bases.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {[string, number][]} numbersWithBases 
+ * @returns {number}
+ */
+function sumItUp(numbersWithBases) {
+    let sum = 0;
+
+    for (let [strNum, base] of numbersWithBases) {
+        const value = parseInt(strNum, base);
+        sum += value;
+    }
+
+    return sum;
+}
+
+console.log(sumItUp([["101",16],["7640",8],["1",9]])); // 4258
