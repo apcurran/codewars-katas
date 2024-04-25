e56ccc4e8890e22249142b6902beeb3e54b0f2a1
Alex Curran <alexcurran.va@gmail.com>
2024-04-25 09:15:46 -0500

    Linear time, constant space solution for a-letters-best-friend.js

---
+++ 7-kyu/a-letters-best-friend.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} text 
+ * @param {string} a 
+ * @param {string} b 
+ * @returns {boolean}
+ */
+function bestFriend(text, a, b) {
+    for (let i = 0; i < text.length; i++) {
+        const currentLetter = text[i];
+        const nextLetter = text[i + 1];
+
+        if (currentLetter === a && nextLetter !== b) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(bestFriend("he headed to the store", "h", "e")); // true
+console.log(bestFriend("abcdee", "e", "e")); // false
