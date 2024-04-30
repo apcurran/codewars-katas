d255111e52592d1f6582fe2d26da4e73ce605ee8
Alex Curran <alexcurran.va@gmail.com>
2024-04-30 09:53:37 -0500

    Working sqrt(n) time solution for simple-maths-test.js

---
+++ 7-kyu/simple-maths-test.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(sqrt(n))
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {[boolean, boolean, boolean]}
+ */
+function numberProperty(n) {
+    const isNPrime = isPrime(n);
+    const isNEven = isEven(n);
+    const isNMultipleOf10 = isMultipleOf10(n);
+
+    return [isNPrime, isNEven, isNMultipleOf10];
+}
+
+console.log(numberProperty(7)); // [true, false, false]
+
+/**
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isPrime(num) {
+    let s = Math.sqrt(num);
+
+    for (let i = 2; i <= s; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return num >= 2;
+}
+
+/**
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+/**
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isMultipleOf10(num) {
+    return num % 10 === 0;
+}
