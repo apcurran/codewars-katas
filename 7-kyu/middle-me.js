e11feabf3a6519dddd57835f373841a0ca6e0190
Alex Curran <alexcurran.va@gmail.com>
2024-05-06 09:36:45 -0500

    Working linear time/space solution for middle-me.js

---
+++ 7-kyu/middle-me.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @param {string} x 
+ * @param {string} y 
+ */
+function middleMe(n, x, y) {
+    // cannot place x in the middle of an odd repeated chunk
+    if (n % 2 !== 0) return x;
+
+    const repeatedChunk = y.repeat(n / 2);
+
+    return `${repeatedChunk}${x}${repeatedChunk}`;
+}
+
+console.log(middleMe(10, "A", "*")); // '*****A*****'
