27c12540be87a133e49619e0c937c086a080c190
Alex Curran <alexcurran.va@gmail.com>
2024-05-09 09:32:46 -0500

    Hashset solution for difference-between-two-collections.js

---
+++ 7-kyu/difference-between-two-collections.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * time: O(a + b)
+ * space: O(a + b)
+ * 
+ * @param {string[]} a 
+ * @param {string[]} b 
+ * @returns {string[]}
+ */
+function diff(a, b) {
+    const setA = new Set(a);
+    const setB = new Set(b);
+    let results = [];
+
+    for (let char of setA) {
+        if (!setB.has(char)) {
+            results.push(char);
+        }
+    }
+
+    for (let char of setB) {
+        if (!setA.has(char)) {
+            results.push(char);
+        }
+    }
+
+    return results.sort();
+}
+
+const arrayA = ["a", "a", "t", "e", "f", "i", "j"];
+const arrayB = ["t", "g", "g", "i", "k", "f"];
+console.log(diff(arrayA, arrayB)); // ["a", "e", "g", "j", "k"]
