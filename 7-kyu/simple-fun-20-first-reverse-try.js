60f507faabe337e663253d10a59954197d4c4ee9
Alex Curran <alexcurran.va@gmail.com>
2024-05-17 10:49:42 -0500

    Working solution for simple-fun-20-first-reverse-try.js

---
+++ 7-kyu/simple-fun-20-first-reverse-try.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]} 
+ */
+function firstReverseTry(arr) {
+    if (arr.length === 0) return arr;
+
+    const firstNum = arr[0];
+    const lastNum = arr[arr.length - 1];
+
+    arr[0] = lastNum;
+    arr[arr.length - 1] = firstNum;
+
+    return arr;
+}
+
+console.log(firstReverseTry([1, 2, 3, 4, 5])); // [5, 2, 3, 4, 1]
