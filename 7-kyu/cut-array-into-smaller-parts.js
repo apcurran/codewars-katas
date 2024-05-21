dc9eff5735c47d3a1796f300d370f38d79d7fc1b
Alex Curran <alexcurran.va@gmail.com>
2024-05-21 16:50:32 -0500

    Working solution for cut-array-into-smaller-parts.js

---
+++ 7-kyu/cut-array-into-smaller-parts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = arr length
+ * m = chunkSize
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @param {number} chunkSize 
+ * @returns {number[][]}
+ */
+function makeParts(arr, chunkSize) {
+    let results = [];
+
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        results.push(chunk);
+    }
+
+    return results;
+}
+
+console.log(makeParts([1,2,3,4,5], 2)); // [[1,2],[3,4],[5]]
