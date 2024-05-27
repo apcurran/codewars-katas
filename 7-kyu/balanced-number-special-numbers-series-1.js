99f46146ac18553f478cbff331c438a6d7120c16
Alex Curran <alexcurran.va@gmail.com>
2024-05-27 09:58:18 -0500

    Simplified solution 2

--- 7-kyu/balanced-number-special-numbers-series-1.js
+++ 7-kyu/balanced-number-special-numbers-series-1.js
@@ -1,7 +1,50 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number} num 
+//  * @returns {string}
+//  */
+// function balancedNum(num) {
+//     const strNum = String(num);
+//     // determine if oddLen or evenLen
+//     const isOddLength = strNum.length % 2 === 1;
+//     let leftDigitsSum = 0;
+//     let rightDigitsSum = 0;
+
+//     let left = 0;
+//     let right = strNum.length - 1;
+//     // odd length -> one middle digit
+//     if (isOddLength) {
+//         while (left < right) {
+//             const leftDigit = Number(strNum[left]);
+//             const rightDigit = Number(strNum[right]);
+//             leftDigitsSum += leftDigit;
+//             rightDigitsSum += rightDigit;
+    
+//             left++;
+//             right--;
+//         }
+//     } else {
+//         while (left < right - 1) {
+//             const leftDigit = Number(strNum[left]);
+//             const rightDigit = Number(strNum[right]);
+//             leftDigitsSum += leftDigit;
+//             rightDigitsSum += rightDigit;
+    
+//             left++;
+//             right--;
+//         }
+//     }
+
+//     return leftDigitsSum === rightDigitsSum ? "Balanced" : "Not Balanced";
+// }
+
 /**
- * solution 1
+ * solution 2 -- simplified
  * time: O(n)
  * space: O(n)
  * 
@@ -10,34 +53,15 @@
  */
 function balancedNum(num) {
     const strNum = String(num);
-    // determine if oddLen or evenLen
-    const isOddLength = strNum.length % 2 === 1;
     let leftDigitsSum = 0;
     let rightDigitsSum = 0;
 
-    let left = 0;
-    let right = strNum.length - 1;
-    // odd length -> one middle digit
-    if (isOddLength) {
-        while (left < right) {
-            const leftDigit = Number(strNum[left]);
-            const rightDigit = Number(strNum[right]);
-            leftDigitsSum += leftDigit;
-            rightDigitsSum += rightDigit;
-    
-            left++;
-            right--;
-        }
-    } else {
-        while (left < right - 1) {
-            const leftDigit = Number(strNum[left]);
-            const rightDigit = Number(strNum[right]);
-            leftDigitsSum += leftDigit;
-            rightDigitsSum += rightDigit;
-    
-            left++;
-            right--;
-        }
+    for (let i = 0; i < (strNum.length / 2) - 1; i++) {
+        const leftDigit = Number(strNum[i]);
+        const rightDigit = Number(strNum[strNum.length - 1 - i]);
+
+        leftDigitsSum += leftDigit;
+        rightDigitsSum += rightDigit;
     }
 
     return leftDigitsSum === rightDigitsSum ? "Balanced" : "Not Balanced";
