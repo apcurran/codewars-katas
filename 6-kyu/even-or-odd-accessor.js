f6af23e45de6eba8766f13cdb2e72d380ff94a01
Alex Curran <alexcurran.va@gmail.com>
2024-05-30 11:03:08 -0500

    Proxy constructor solution for even-or-odd-accessor.js

---
+++ 6-kyu/even-or-odd-accessor.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1 -- new Proxy() constructor
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {"Even"|"Odd"}
+ */
+function target(n) {
+    return n % 2 === 0 ? "Even" : "Odd";
+}
+
+const handler = {
+    get(target, prop) {
+        return target(prop);
+    }
+};
+
+const evenOrOdd = new Proxy(target, handler);
