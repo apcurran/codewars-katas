f22baf890fc86a05cfa95cd0dfb5b171f731b12c
Alex Curran <alexcurran.va@gmail.com>
2024-05-31 11:30:29 -0500

    Working linear time/space solution for random-case.js

---
+++ 7-kyu/random-case.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} x 
+ * @returns {strin}
+ */
+function randomCase(x) {
+    let result = "";
+
+    for (let char of x) {
+        if (char !== " ") {
+            const randomChance = Math.random();
+        
+            if (randomChance < .5) {
+                result += char.toLowerCase();
+            } else {
+                result += char.toUpperCase();
+            }
+        } else {
+            // empty space
+            result += char;
+        }
+    }
+
+    return result;
+}
+
+console.log(randomCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit"));
