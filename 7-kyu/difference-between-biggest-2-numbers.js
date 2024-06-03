a57c1611686b1b2da937d26becc63ecd4ca4a5e8
Alex Curran <alexcurran.va@gmail.com>
2024-06-03 10:38:12 -0500

    Bubble sort solution for difference-between-biggest-2-numbers.js

--- 7-kyu/difference-between-biggest-2-numbers.js
+++ 7-kyu/difference-between-biggest-2-numbers.js
@@ -1,29 +1,62 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} arr 
+//  * @returns {number}
+//  */
+// function diffBig2(arr) {
+//     let largest = 0;
+//     let secondLargest = 0;
+
+//     for (let num of arr) {
+//         if (num >= largest) {
+//             // update second largest value
+//             secondLargest = largest;
+//             // update largest value
+//             largest = num;
+//         } else if (num > secondLargest) {
+//             secondLargest = num;
+//         }
+//     }
+
+//     return largest - secondLargest;
+// }
+
 /**
  * solution 1
- * time: O(n)
+ * time: O(n^2)
  * space: O(1)
  * 
  * @param {number[]} arr 
  * @returns {number}
  */
 function diffBig2(arr) {
-    let largest = 0;
-    let secondLargest = 0;
+    const sorted = bubbleSort(arr);
 
-    for (let num of arr) {
-        if (num >= largest) {
-            // update second largest value
-            secondLargest = largest;
-            // update largest value
-            largest = num;
-        } else if (num > secondLargest) {
-            secondLargest = num;
+    return sorted[sorted.length - 1] - sorted[sorted.length - 2];
+}
+
+/**
+ * @param {number[]} nums 
+ * @returns {number[]}
+ */
+function bubbleSort(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                // swap nums
+                const tempCurrent = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = tempCurrent;
+            }
         }
     }
-
-    return largest - secondLargest;
+    
+    return nums;
 }
 
 console.log(diffBig2([2, 1])); // 1
