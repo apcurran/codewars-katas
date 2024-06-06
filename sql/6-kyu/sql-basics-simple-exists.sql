1078528f3ca3cfa3597a435382b935178440d287
Alex Curran <alexcurran.va@gmail.com>
2024-06-06 09:58:17 -0500

    Working SQL query with EXISTS statement usage

---
+++ sql/6-kyu/sql-basics-simple-exists.sql
@@ -0,0 +1,12 @@
+SELECT
+    id,
+    name
+FROM departments
+WHERE EXISTS
+    (
+        SELECT price
+        FROM sales
+        WHERE
+            departments.id = sales.department_id AND
+            sales.price > 98
+    )
