1a7fdd2caf771c611ff20b7647faf142df505f54
Alex Curran <alexcurran.va@gmail.com>
2024-06-13 11:28:13 -0500

    Regex solution for sir-showme-yourid.js

---
+++ 7-kyu/sir-showme-yourid.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = words length
+ * m = word length
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {string} yourID 
+ * @returns {boolean}
+ */
+function showMe(yourID) {
+    return yourID
+            // if word does not have a dash,
+            // this will keep the word full (even keeping empty spaces)
+            .split("-")
+            .every(function isName(word) {
+                return /^[A-Z][a-z]+$/.test(word);
+            });
+}
+
+console.log(showMe("Francis")); // true
+console.log(showMe("Jean-Eluard")); // true
+console.log(showMe("Le Mec")); // false
