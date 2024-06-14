e02c665a76c9caa557c45683aaf428d7f06e747d
Alex Curran <alexcurran.va@gmail.com>
2024-06-14 09:24:43 -0500

    Linear time, constant space solution for geometric-sequence-sum-of-all-elements.js

---
+++ 7-kyu/geometric-sequence-sum-of-all-elements.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} firstTerm 
+ * @param {number} ratio 
+ * @param {number} nTerms 
+ * @returns {number}
+ */
+function GeometricSequenceSum(firstTerm, ratio, nTerms) {
+    let sum = firstTerm;
+    let term = firstTerm;
+
+    for (let i = 0; i < nTerms - 1; i++) {
+        term *= ratio;
+        sum += term;
+    }
+
+    return sum;
+}
+
+console.log(GeometricSequenceSum(2, 3, 5)); // 242
