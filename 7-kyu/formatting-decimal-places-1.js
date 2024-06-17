9a4bf42b2d66eba2339acbca961e5da29ae0c262
Alex Curran <alexcurran.va@gmail.com>
2024-06-17 10:11:12 -0500

    Math.trunc() solution for formatting-decimal-places-1.js

---
+++ 7-kyu/formatting-decimal-places-1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} num 
+ * @returns {number}
+ */
+function twoDecimalPlaces(num) {
+    return Math.trunc(num * 100) / 100;
+}
+
+console.log(twoDecimalPlaces(32.8493)); // 32.84
+console.log(twoDecimalPlaces(14.3286)); // 14.32
