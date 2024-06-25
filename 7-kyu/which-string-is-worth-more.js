eca39beec9a6e9c70e076e29782ef8cf32b6afdd
Alex Curran <alexcurran.va@gmail.com>
2024-06-25 11:42:31 -0500

    Working solution for which-string-is-worth-more.js

---
+++ 7-kyu/which-string-is-worth-more.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} strA 
+ * @param {string} strB 
+ * @returns {string}
+ */
+function highestValue(strA, strB) {
+    let strACharCodeSum = 0;
+
+    for (let i = 0; i < strA.length; i++) {
+        strACharCodeSum += strA.charCodeAt(i);
+    }
+
+    let strBCharCodeSum = 0;
+
+    for (let i = 0; i < strB.length; i++) {
+        strBCharCodeSum += strB.charCodeAt(i);
+    }
+
+    if (strACharCodeSum >= strBCharCodeSum) {
+        return strA;
+    } else {
+        return strB;
+    }
+}
+
+console.log(highestValue("HELLO", ""));
