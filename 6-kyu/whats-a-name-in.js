9363f4860272f6005467f0c8258241277f8fa222
Alex Curran <alexcurran.va@gmail.com>
2024-06-27 10:08:24 -0500

    Pointer technique for whats-a-name-in.js

---
+++ 6-kyu/whats-a-name-in.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- pointer technique
+ * time: O(n + m) -- name copies
+ * space: O(n + m) -- str and name copies with lowercasing func
+ * 
+ * @param {string} str 
+ * @param {string} name 
+ * @returns {boolean}
+ */
+function nameInStr(str, name) {
+    const lowerStr = str.toLowerCase();
+    const lowerName = name.toLowerCase();
+    // pointer technique for name str
+    let nameCharPointer = 0;
+
+    for (let strChar of lowerStr) {
+        if (strChar === lowerName[nameCharPointer]) {
+            nameCharPointer++;
+        }
+    }
+
+    return nameCharPointer === name.length;
+}
+
+console.log(nameInStr("Across the rivers", "chris")); // true
