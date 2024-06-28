138b1fcb8aca93f33be88269c95ce0f908bfbe0d
Alex Curran <alexcurran.va@gmail.com>
2024-06-28 10:01:45 -0500

    Reverse and complement

---
+++ 7-kyu/reverse-complement-dna.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} dna 
+ * @returns {string}
+ */
+function reverseComplement(dna) {
+    const reverseSequence = dna.split("").reverse();
+    let results = "";
+
+    for (let char of reverseSequence) {
+        if (char === "C") {
+            results += "G";
+        } else if (char === "G") {
+            results += "C";
+        } else if (char === "A") {
+            results += "T";
+        } else if (char === "T") {
+            results += "A";
+        } else {
+            return "Invalid sequence";
+        }
+    }
+
+    return results;
+}
+
+console.log(reverseComplement("TTCCGGAA")); // "TTCCGGAA"
+console.log(reverseComplement("GACTGACTGTA")); // "TACAGTCAGTC"
+console.log(reverseComplement("")); // ""
+console.log(reverseComplement("XYZ")); // "Invalid sequence"
+
