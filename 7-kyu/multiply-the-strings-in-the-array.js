f85818b06faa545e0b6a84b4b4a86894400995e7
Alex Curran <alexcurran.va@gmail.com>
2024-07-01 10:27:31 -0500

    Working constant time/space solution for multiply-the-strings-in-the-array.js

---
+++ 7-kyu/multiply-the-strings-in-the-array.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {[string, string]} arr 
+ * @returns {string} str num
+ */
+function arrMultiply(arr) {
+    const num1 = Number(arr[0]);
+    const num2 = Number(arr[1]);
+    const product = num1 * num2;
+
+    return String(product);
+}
+
+console.log(arrMultiply(["9","6"])); // "54"
