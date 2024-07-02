c5d678d783aab0012ae6022704ab889b70bcedf3
Alex Curran <alexcurran.va@gmail.com>
2024-07-02 09:26:46 -0500

    Array.filter() solution for squeaky-clean.js

---
+++ 7-kyu/squeaky-clean.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Array} arr 
+ * @returns {Array} array with "", 0, and null removed
+ */
+function squeakyClean(arr) {
+    return arr.filter(function removeNullLikeValues(value) {
+        if (value === "" || value === null || value === 0 || value === undefined) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+}
+
+console.log(squeakyClean(["click1","click2",null,"","","submitForm"]));
+// ['click1', 'click2','submitForm']
