79e77f90197158cdfcb542b7a4939ddea365ae2d
Alex Curran <alexcurran.va@gmail.com>
2024-07-02 10:19:19 -0500

    Regex solution for COFFEE.js

---
+++ 7-kyu/COFFEE.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function coffee(str) {
+    return str.replace(/coffee/gi, "COFFEE");
+}
+
+console.log(coffee("Coffee! Buy me a COFFEE!")); // "COFFEE! Buy me a COFFEE!"
