63b003e3c3663a34f82c116573f4cdc2ccd66bfd
Alex Curran <alexcurran.va@gmail.com>
2024-07-03 11:15:31 -0500

    Alternative solution

--- 6-kyu/only-duplicates.js
+++ 6-kyu/only-duplicates.js
@@ -1,7 +1,48 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {string} str 
+//  * @returns {string}
+//  */
+// function onlyDuplicates(str) {
+//     let results = "";
+
+//     for (let i = 0; i < str.length; i++) {
+//         const targetChar = str[i];
+//         const isTargetDuplicate = isCharDuplicate(targetChar, i, str);
+
+//         if (isTargetDuplicate) {
+//             results += targetChar;
+//         }
+//     }
+
+//     return results;
+// }
+
+// /**
+//  * @param {string} targetChar 
+//  * @param {number} targetCharIndex 
+//  * @param {string} str 
+//  * @returns {boolean}
+//  */
+// function isCharDuplicate(targetChar, targetCharIndex, str) {
+//     for (let i = 0; i < str.length; i++) {
+//         const currentChar = str[i];
+
+//         if (currentChar === targetChar && i !== targetCharIndex) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * solution 1
+ * solution 2
  * time: O(n^2)
  * space: O(n)
  * 
@@ -13,32 +54,13 @@ function onlyDuplicates(str) {
 
     for (let i = 0; i < str.length; i++) {
         const targetChar = str[i];
-        const isTargetDuplicate = isCharDuplicate(targetChar, i, str);
-
-        if (isTargetDuplicate) {
+        
+        if (str.indexOf(targetChar) !== str.lastIndexOf(targetChar)) {
             results += targetChar;
         }
     }
 
     return results;
-}
-
-/**
- * @param {string} targetChar 
- * @param {number} targetCharIndex 
- * @param {string} str 
- * @returns {boolean}
- */
-function isCharDuplicate(targetChar, targetCharIndex, str) {
-    for (let i = 0; i < str.length; i++) {
-        const currentChar = str[i];
-
-        if (currentChar === targetChar && i !== targetCharIndex) {
-            return true;
-        }
-    }
-
-    return false;
 }
 
 console.log(onlyDuplicates("abccdefee")); // "cceee"
