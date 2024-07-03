629e55a2d607b1bf351ace5675410add506c23c1
Alex Curran <alexcurran.va@gmail.com>
2024-07-03 09:43:06 -0500

    Hashset solution for noobcode-03-check-these-letters.js

---
+++ 7-kyu/noobcode-03-check-these-letters.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * n = str1 length
+ * m = str2 length
+ * time: O(n + m)
+ * space: O(n)
+ * 
+ * @param {[string, string]} arr 
+ * @returns {boolean}
+ */
+function letterCheck([str1, str2]) {
+    let str1Chars = new Set(str1.toLowerCase());
+
+    for (let char of str2) {
+        if (!str1Chars.has(char.toLowerCase())) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(letterCheck(["ab", "aaa"])); // true
+console.log(letterCheck(["compadres", "DRAPES"])); // true
+console.log(letterCheck(["parses", "parsecs"])); // false
