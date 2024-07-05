37e48fdb69c791097f3bceaaa284c75bced958ba
Alex Curran <alexcurran.va@gmail.com>
2024-07-05 09:48:37 -0500

    Working solution for negation-of-a-value.js

---
+++ 7-kyu/negation-of-a-value.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {string} string 
+ * @param {any} value 
+ * @returns {boolean}
+ */
+function negationValue(string, value) {
+    return string.length % 2 === 0 ? !!value : !value;
+}
+
+console.log(negationValue("!", false)); //=> true
+console.log(negationValue("!!!!!", true)); //=> false
+console.log(negationValue("!!", [])); //=> true
