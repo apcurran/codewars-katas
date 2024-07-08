ac16ecd28a4b6f5db57a2a6de2371eb8ce457057
Alex Curran <alexcurran.va@gmail.com>
2024-07-08 14:53:15 -0500

    Log solution for simple-fun-18-candles.js

---
+++ 7-kyu/simple-fun-18-candles.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(log base m of n)
+ * space: O(1)
+ * 
+ * @param {number} candlesNumber 
+ * @param {number} makeNew 
+ * @returns {number}
+ */
+function candles(candlesNumber, makeNew) {
+    let result = 0;
+    let empty = 0;
+
+    while (candlesNumber > 0) {
+        result += candlesNumber;
+        empty += candlesNumber;
+        candlesNumber = Math.floor(empty / makeNew);
+        empty = empty % makeNew;
+    }
+
+    return result;
+}
+
+console.log(candles(5, 2)); // 9
+console.log(candles(1, 2)); // 1
+console.log(candles(3, 3)); // 4
