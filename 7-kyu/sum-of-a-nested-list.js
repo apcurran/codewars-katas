385cd88bee006f9808474e8e22dc107c0f14fa7d
Alex Curran <alexcurran.va@gmail.com>
2024-07-16 10:43:08 -0500

    Array flatten and sum for sum-of-a-nested-list.js

---
+++ 7-kyu/sum-of-a-nested-list.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {array} arr 
+ * @returns {number}
+ */
+function sumNested(arr) {
+    return arr
+            .flat(Infinity)
+            .reduce((sum, currentNum) => sum + currentNum, 0);
+}
+
+console.log(sumNested([1, [2, [3, [4]]]])); // 10
