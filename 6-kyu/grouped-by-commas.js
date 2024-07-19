1d36954916a2b0dcedd4f002dea8647a6f0f23b1
Alex Curran <alexcurran.va@gmail.com>
2024-07-19 16:52:40 -0500

    Intl.NumberFormat() API for number formatting with commas

---
+++ 6-kyu/grouped-by-commas.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n) -- stringify num
+ * space: O(n) -- stringify num
+ * 
+ * @param {number} n 
+ * @returns {string}
+ */
+function groupByCommas(n) {
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "decimal"
+    });
+
+    return formatter.format(n);
+}
+
+console.log(groupByCommas(200000));
