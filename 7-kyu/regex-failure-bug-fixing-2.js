6276349d158757eb9116892ead0f0ac97ddb3288
Alex Curran <alexcurran.va@gmail.com>
2024-07-22 11:48:13 -0500

    Regex replace with global and case-insensitive flags enabled

---
+++ 7-kyu/regex-failure-bug-fixing-2.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1 -- regex replace
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} phrase 
+ * @returns {string}
+ */
+function filterWords(phrase) {
+    return phrase.replace(/(bad|mean|ugly|horrible|hideous)/gi, "awesome");
+}
+
+console.log(filterWords("You're Bad! timmy!")); // "You're awesome! timmy!"
+console.log(filterWords("You're MEAN! timmy!")); // "You're awesome! timmy!"
