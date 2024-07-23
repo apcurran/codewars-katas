f08aa83474d903dcbca1ad10cd267267a5de6742
Alex Curran <alexcurran.va@gmail.com>
2024-07-23 10:28:04 -0500

    Constant time/space solution for hello-world-without-strings.js

---
+++ 7-kyu/hello-world-without-strings.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @returns {string}
+ */
+function helloWorld() {
+    return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+}
+
+console.log(helloWorld()); // "Hello, World!"
