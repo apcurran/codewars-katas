6830b3322fd41bd1e1ffd154e77f90f3cbf9efa9
Alex Curran <alexcurran.va@gmail.com>
2024-07-24 11:39:12 -0500

    Regex solution for dropcaps.js

---
+++ 7-kyu/dropcaps.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n * m)
+ * space: O(n + m)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function dropCap(str) {
+    return str.replace(/\b\w{3,}\b/g, function capitalizeFirstLetterLowerRest(word) {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+}
+
+console.log(dropCap("apple")); // "Apple"
+console.log(dropCap("one   space")); // "One   Space"
