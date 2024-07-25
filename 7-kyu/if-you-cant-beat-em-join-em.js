1881b834149e9e57c60bd5d4bcf635234b96bf2f
Alex Curran <alexcurran.va@gmail.com>
2024-07-25 10:44:35 -0500

    Array.sort() with sum of arrA and arrB comparison func

---
+++ 7-kyu/if-you-cant-beat-em-join-em.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O((n * log n) * max(a, b))
+ * space: O(n)
+ * 
+ * @param {number[][]} nums 
+ * @returns {number[]}
+ */
+function cantBeatSoJoin(nums) {
+    // sort array groups by largest sums
+    nums.sort(function sortArraysByInnerSums(arrA, arrB) {
+        const arrASum = arrA.reduce((sum, currentNum) => sum + currentNum, 0);
+        const arrBSum = arrB.reduce((sum, currentNum) => sum + currentNum, 0);
+
+        return arrBSum - arrASum;
+    });
+    
+    // flatten to 1-d return array
+    return nums.flat(Infinity);
+}
+
+console.log(cantBeatSoJoin([[1,2], [3,4], [5,6], [7,8], [9]])); // [7, 8, 5, 6, 9, 3, 4, 1, 2]
