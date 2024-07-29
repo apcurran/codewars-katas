e3ab0d4b5bb8df60196c8fcbeee97d1d8011c62b
Alex Curran <alexcurran.va@gmail.com>
2024-07-29 09:38:29 -0500

    Closure-based solution for test-your-knowledge-of-function-scope.js

---
+++ 7-kyu/test-your-knowledge-of-function-scope.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1 -- closure
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @returns {function}
+ */
+function add(a) {
+    /**
+     * @param {number} b
+     * @returns {number} sum
+     */
+    return function sum(b) {
+        return a + b;
+    };
+}
+
+console.log(add(3)(4)); // 7
+console.log(add(12)(20)); // 32
