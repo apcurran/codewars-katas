c76e3dd4a95abacedc4f2bbf3822e0d11f7b8a4d
Alex Curran <alexcurran.va@gmail.com>
2024-07-30 10:26:29 -0500

    Working solution for is-it-negative-zero.js

---
+++ 7-kyu/is-it-negative-zero.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * spaces: O(1)
+ * 
+ * @param {number} n 
+ * @returns {boolean} true if -0, otherwise false
+ */
+function isNegativeZero(n) {
+    return Object.is(n, -0);
+}
+
+console.log(isNegativeZero(-0)); // true
+console.log(isNegativeZero(0)); // false
+console.log(isNegativeZero(3)); // false
