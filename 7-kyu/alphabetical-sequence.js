6ff8f7bdac32f58b2cf0f8227af9b27721bb3c66
Alex Curran <alexcurran.va@gmail.com>
2024-07-31 11:06:27 -0500

    Working solution for alphabetical-sequence.js

---
+++ 7-kyu/alphabetical-sequence.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function alphaSeq(str) {
+    let alphabeticalStrList = str
+                            .toLowerCase()
+                            .split("")
+                            .sort();
+    let result = [];
+
+    for (let char of alphabeticalStrList) {
+        const charN = char.charCodeAt(0) - 96;
+        const uppercaseChar = char.toUpperCase();
+        const repeatingLowercaseCharChunk = char.repeat(charN - 1);
+        const completeChunk = uppercaseChar + repeatingLowercaseCharChunk;
+        result.push(completeChunk);
+    }
+
+    return result.join(",");
+}
+
+console.log(alphaSeq("ZpglnRxqenU"));
+// "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
