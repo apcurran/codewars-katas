2eb5ef4efa54ad2f8e3358e5aaf96291912f12a3
Alex Curran <alexcurran.va@gmail.com>
2024-08-05 10:57:01 -0500

    Math-based solution without string conversion

--- 7-kyu/simple-fun-3-late-ride.js
+++ 7-kyu/simple-fun-3-late-ride.js
@@ -1,9 +1,31 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number} totalMinutes 
+//  * @returns {number}
+//  */
+// function lateRide(totalMinutes) {
+//     const hours = Math.floor(totalMinutes / 60);
+//     const minutes = totalMinutes % 60;
+//     const time = String(hours) + String(minutes);
+//     let timeDigitsSum = 0;
+
+//     for (let digitStr of time) {
+//         const digitNum = Number(digitStr);
+//         timeDigitsSum += digitNum;
+//     }
+
+//     return timeDigitsSum;
+// }
+
 /**
- * solution 1
- * time: O(n)
- * space: O(n)
+ * solution 2 -- math
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} totalMinutes 
  * @returns {number}
@@ -11,15 +33,13 @@
 function lateRide(totalMinutes) {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
-    const time = String(hours) + String(minutes);
-    let timeDigitsSum = 0;
+    
+    const hourDigit1 = Math.floor(hours / 10);
+    const hourDigit2 = hours % 10;
+    const minuteDigit1 = Math.floor(minutes / 10);
+    const minuteDigit2 = minutes % 10;
 
-    for (let digitStr of time) {
-        const digitNum = Number(digitStr);
-        timeDigitsSum += digitNum;
-    }
-
-    return timeDigitsSum;
+    return hourDigit1 + hourDigit2 + minuteDigit1 + minuteDigit2;
 }
 
 console.log(lateRide(240)); // 4
