c48128359a3de37d1d3795908185ab470e4ef3ab
Alex Curran <alexcurran.va@gmail.com>
2024-08-06 09:33:42 -0500

    Two solution (math and iterative) for unique-pairs.js

---
+++ 7-kyu/unique-pairs.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- math
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {number}
+ */
+function projectPartners(n) {
+    return n * (n - 1) / 2;
+}
+
+// /**
+//  * solution 2 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number} n 
+//  * @returns {number}
+//  */
+// function projectPartners(n) {
+//     let pairsCount = 0;
+
+//     for (let i = 1; i < n; i++) {
+//         pairsCount += i;
+//     }
+
+//     return pairsCount;
+// }
+
+console.log(projectPartners(2)); // 1
+console.log(projectPartners(4)); // 6
