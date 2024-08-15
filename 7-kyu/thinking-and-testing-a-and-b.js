e8e29b815c595ff9e35e883dedab6117747e5e53
Alex Curran <alexcurran.va@gmail.com>
2024-08-15 11:20:24 -0500

    Bitwise OR solution for thinking-and-testing-a-and-b.js

---
+++ 7-kyu/thinking-and-testing-a-and-b.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1 -- bitwise OR operator
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function testit(a, b) {
+    return a | b;
+}
+
+console.log(testit(10, 20)); // 30
+
