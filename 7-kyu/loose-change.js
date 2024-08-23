83dbe0cc8f0754abf02c489af05fc42831c6b324
Alex Curran <alexcurran.va@gmail.com>
2024-08-23 10:21:17 -0500

    Iterative solution for loose-change.js

---
+++ 7-kyu/loose-change.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const CHANGE = {
+    penny: 0.01,
+    nickel: 0.05,
+    dime: 0.10,
+    quarter: 0.25,
+    dollar: 1.00
+};
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} change 
+ * @returns {string}
+ */
+function changeCount(change) {
+    const changeItems = change.split(" ");
+    let total = 0;
+
+    for (let changeItem of changeItems) {
+        const amount = CHANGE[changeItem];
+        total += amount;
+    }
+
+    return "$" + total.toFixed(2);
+}
+
+console.log(changeCount("dime penny dollar")); // "$1.11"
+
