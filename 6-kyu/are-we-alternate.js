5b995d37db5d6828c39a665d7b983a72ee12b4c6
Alex Curran <alexcurran.va@gmail.com>
2024-08-26 09:20:52 -0500

    Linear time/constant space solution for are-we-alternate.js

---
+++ 6-kyu/are-we-alternate.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} word 
+ * @returns {boolean}
+ */
+function isAlt(word) {
+    const vowels = "aeiouAEIOU";
+    let previous;
+    let current;
+
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+
+        if (vowels.includes(letter)) {
+            current = "vowel";
+        } else {
+            current = "consonant";
+        }
+
+        if (current === previous) {
+            return false;
+        }
+
+        // set previous to current value for next iteration
+        previous = current;
+    }
+
+    return true;
+}
+
+console.log(isAlt("amazon")); // true
+console.log(isAlt("apple")); // false
+console.log(isAlt("banana")); // true
