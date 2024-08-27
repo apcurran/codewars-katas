9feafbbf3ce01b60ca33ef1c9039879149d81c2b
Alex Curran <alexcurran.va@gmail.com>
2024-08-27 09:49:55 -0500

    Iterative solution for guess-the-word-count-matching-letters.js

---
+++ 7-kyu/guess-the-word-count-matching-letters.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} correctWord 
+ * @param {string} guess 
+ * @returns {number}
+ */
+function countCorrectCharacters(correctWord, guess) {
+    if (correctWord.length !== guess.length) {
+        throw new Error("Invalid word lengths.");
+    }
+
+    let correctLettersCount = 0;
+
+    for (let i = 0; i < correctWord.length; i++) {
+        const correctWordLetter = correctWord[i];
+        const guessWordLetter = guess[i];
+
+        if (correctWordLetter === guessWordLetter) {
+            correctLettersCount++;
+        }
+    }
+
+    return correctLettersCount;
+}
+
+console.log(countCorrectCharacters("dog", "car")); // 0
+console.log(countCorrectCharacters("dog", "god")); // 1
+console.log(countCorrectCharacters("dog", "cog")); // 2
+console.log(countCorrectCharacters("dog", "cod")); // 1
+console.log(countCorrectCharacters("dog", "bog")); // 2
+console.log(countCorrectCharacters("dog", "dog")); // 3
