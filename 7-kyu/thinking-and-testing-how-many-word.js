26b086dd773c91674d7212c549442031c3bb6913
Alex Curran <alexcurran.va@gmail.com>
2024-08-30 09:32:49 -0500

    Regex solution for thinking-and-testing-how-many-word.js

---
+++ 7-kyu/thinking-and-testing-how-many-word.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function testit(str) {
+    const wordMatches = str.match(/w.*?o.*?r.*?d/gi) || [];
+
+    return wordMatches.length;
+}
+
+console.log(testit("word")); // 1
