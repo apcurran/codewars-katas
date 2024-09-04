2ed8e716b0d4cc9b6385c5c6f23c5840ca7f156f
Alex Curran <alexcurran.va@gmail.com>
2024-09-04 13:40:47 -0500

    'for...in' iteration solution for swap-items-in-a-dictionary.js

---
+++ 7-kyu/swap-items-in-a-dictionary.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n) -- if result object is counted
+ * 
+ * @param {object} dict 
+ * @returns {object}
+ */
+function switchDict(dict) {
+    let result = {};
+
+    for (let prop in dict) {
+        const value = dict[prop];
+
+        if (!Object.hasOwn(result, value)) {
+            result[value] = [];
+        }
+
+        result[value].push(prop);
+    }
+
+    return result;
+}
+
+console.log(switchDict({"Ice": "Cream", "Age": "21", "Light": "Cream", "Double": "Cream"}));
+// {'Cream': ['Ice', 'Double', 'Light'], '21': ['Age']}
