0522d336df36dc0375413ee059d5a77ba01d12fe
Alex Curran <alexcurran.va@gmail.com>
2024-09-05 11:47:21 -0500

    Array .every() and .some() methods for solving some-but-not-all.js

---
+++ 7-kyu/some-but-not-all.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string|array} seq 
+ * @param {function} pred 
+ * @returns {boolean}
+ */
+function someButNotAll(seq, pred) {
+    const list = Array.from(seq);
+
+    return list.some(pred) && !list.every(pred);
+}
+
+console.log(someButNotAll([4, 1], (x) => x>3)); // true (one, but not both, has a value greater than 3)
+console.log(someButNotAll([1, 1], (x) => x>3)); // false
+console.log(someButNotAll([4, 4], (x) => x>3)); // false
