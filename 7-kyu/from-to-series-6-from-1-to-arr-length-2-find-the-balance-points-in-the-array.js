ebd12dd02d303a678da1bfd96e583a9db246e4a7
Alex Curran <alexcurran.va@gmail.com>
2024-09-09 12:05:49 -0500

    Nested loops solution for from-to-series-6-from-1-to-arr-length-2-find-the-balance-points-in-the-array.js

---
+++ 7-kyu/from-to-series-6-from-1-to-arr-length-2-find-the-balance-points-in-the-array.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function findBalance(arr) {
+    let balancePoints = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        let leftSum = 0;
+        let leftCount = 0;
+        
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+            leftCount++;
+        }
+ 
+        let rightSum = 0;
+        let rightCount = 0;
+        
+        for (let j = i + 1; j < arr.length; j++) {
+            rightSum += arr[j];
+            rightCount++;
+        }
+        
+        const leftAvg = Math.round(leftSum / leftCount);
+        const rightAvg = Math.round(rightSum / rightCount);
+        
+        if (leftAvg === rightAvg) {
+            balancePoints.push(i);
+        }
+    }
+
+    return balancePoints;
+}
+
+console.log(findBalance([1,2,3])); // []
+console.log(findBalance([1,2,3,2,1])); // [2]
+console.log(findBalance([1,1,1,1,1])); // [1, 2, 3]
