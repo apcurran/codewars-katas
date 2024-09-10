1e27d4aa05291c088db67583bfc2368b84d5bfc0
Alex Curran <alexcurran.va@gmail.com>
2024-09-10 11:11:22 -0500

    Math solution for rounding to nearest num divisible by 5 for round-to-nearest-0-or-5.js

---
+++ 7-kyu/round-to-nearest-0-or-5.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} numbers 
+ * @returns {number[]}
+ */
+function roundToFive(numbers) {
+    return numbers.map(function roundToNearestDivisibleByFive(num) {
+        return Math.round(num / 5) * 5;
+    });
+}
+
+console.log(roundToFive([34.5, 56.2, 11, 13])); // [35, 55, 10, 15]
