3e5170b2f4d4f5037e11a8a1d76976bc62c82c79
Alex Curran <alexcurran.va@gmail.com>
2024-09-11 11:47:35 -0500

    Hashset solution for the-office-vi-sabbatical.js

---
+++ 7-kyu/the-office-vi-sabbatical.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n) -- str.toLowerCase() call
+ * 
+ * @param {string} s 
+ * @param {number} val 
+ * @param {number} happiness 
+ * @returns {string}
+ */
+function sabb(s, val, happiness){
+    const sabbaticalLetters = new Set("sabbatical");
+    const loweredStr = s.toLowerCase();
+    let lettersFromSInSabbaticalCount = 0;
+
+    for (let char of loweredStr) {
+        if (sabbaticalLetters.has(char)) {
+            lettersFromSInSabbaticalCount++;
+        }
+    }
+
+    const sum = lettersFromSInSabbaticalCount + val + happiness;
+    
+    if (sum > 22) {
+        return "Sabbatical! Boom!";
+    } else {
+        return "Back to your desk, boy.";
+    }
+}
+
+console.log(sabb("Can I have a sabbatical?", 5, 5)); // 'Sabbatical! Boom!'
