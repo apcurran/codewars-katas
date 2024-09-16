30d6f79c5e5ae2a2e676395d18a9fd9445dbba9f
Alex Curran <alexcurran.va@gmail.com>
2024-09-16 10:29:19 -0500

    Less efficient, but more terse regex-based solution

--- 7-kyu/password-validator.js
+++ 7-kyu/password-validator.js
@@ -1,7 +1,48 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string} str 
+//  * @returns {boolean}
+//  */
+// function password(str) {
+//     if (str.length < 8) {
+//         return false;
+//     }
+
+//     let hasUppercaseLetter = false;
+//     let hasLowercaseLetter = false;
+//     let hasNumber = false;
+
+//     for (let char of str) {
+//         const charCode = char.charCodeAt(0);
+
+//         if (charCode >= "A".charCodeAt(0) && charCode <= "Z".charCodeAt(0)) {
+//             hasUppercaseLetter = true;
+//         }
+
+//         if (charCode >= "a".charCodeAt(0) && charCode <= "z".charCodeAt(0)) {
+//             hasLowercaseLetter = true;
+//         }
+
+//         if (charCode >= "0".charCodeAt(0) && charCode <= "9".charCodeAt(0)) {
+//             hasNumber = true;
+//         }
+
+//         if (hasUppercaseLetter && hasLowercaseLetter && hasNumber) {
+//             // early return
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * solution 1
+ * solution 2 -- regex
  * time: O(n)
  * space: O(1)
  * 
@@ -12,33 +53,15 @@ function password(str) {
     if (str.length < 8) {
         return false;
     }
+    
+    if (!/[A-Z]/.test(str)) return false;
+    
+    if (!/[a-z]/.test(str)) return false;
 
-    let hasUppercaseLetter = false;
-    let hasLowercaseLetter = false;
-    let hasNumber = false;
+    if (!/[0-9]/.test(str)) return false;
 
-    for (let char of str) {
-        const charCode = char.charCodeAt(0);
-
-        if (charCode >= "A".charCodeAt(0) && charCode <= "Z".charCodeAt(0)) {
-            hasUppercaseLetter = true;
-        }
-
-        if (charCode >= "a".charCodeAt(0) && charCode <= "z".charCodeAt(0)) {
-            hasLowercaseLetter = true;
-        }
-
-        if (charCode >= "0".charCodeAt(0) && charCode <= "9".charCodeAt(0)) {
-            hasNumber = true;
-        }
-
-        if (hasUppercaseLetter && hasLowercaseLetter && hasNumber) {
-            // early return
-            return true;
-        }
-    }
-
-    return false;
+    // all passed validation
+    return true;
 }
 
 console.log(password("Abcd1234")); // true
