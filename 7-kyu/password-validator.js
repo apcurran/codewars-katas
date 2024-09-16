43f5271c5984f75c04dd45893567f411e4f45426
Alex Curran <alexcurran.va@gmail.com>
2024-09-16 10:26:29 -0500

    Iterative with conditionals, no regex solution

---
+++ 7-kyu/password-validator.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @returns {boolean}
+ */
+function password(str) {
+    if (str.length < 8) {
+        return false;
+    }
+
+    let hasUppercaseLetter = false;
+    let hasLowercaseLetter = false;
+    let hasNumber = false;
+
+    for (let char of str) {
+        const charCode = char.charCodeAt(0);
+
+        if (charCode >= "A".charCodeAt(0) && charCode <= "Z".charCodeAt(0)) {
+            hasUppercaseLetter = true;
+        }
+
+        if (charCode >= "a".charCodeAt(0) && charCode <= "z".charCodeAt(0)) {
+            hasLowercaseLetter = true;
+        }
+
+        if (charCode >= "0".charCodeAt(0) && charCode <= "9".charCodeAt(0)) {
+            hasNumber = true;
+        }
+
+        if (hasUppercaseLetter && hasLowercaseLetter && hasNumber) {
+            // early return
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(password("Abcd1234")); // true
+console.log(password("Abcd123")); // false
