2660afebabf0c96fa356d3bc971a45e401d24b0f
Alex Curran <alexcurran.va@gmail.com>
2024-09-17 09:39:12 -0500

    Complete solution for every-nth-array-element-basic.js

---
+++ 7-kyu/every-nth-array-element-basic.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr 
+ * @param {number} interval 
+ * @param {number} start 
+ * @returns {number[]}
+ */
+function every(arr, interval, start) {
+    // one argument
+    if (!interval && !start) {
+        return arr;
+    }
+
+    // two arguments
+    if (!start) {
+        let results = [];
+
+        for (let i = 0; i < arr.length; i += interval) {
+            results.push(arr[i]);
+        }
+
+        return results;
+    }
+
+    // three arguments
+    let results = [];
+
+    for (let i = start; i < arr.length; i += interval) {
+        results.push(arr[i]);
+    }
+
+    return results;
+}
+
+console.log(every([0, 1, 2, 3, 4])); // [0,1,2,3,4]
+console.log(every([0, 1, 2, 3, 4], 2)); // [0,2,4]
+console.log(every([0, 1, 2, 3, 4], 3, 1)); // [1,4]
+
