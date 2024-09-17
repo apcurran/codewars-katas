dcef501b8ac72455f3f71a35ccf55dd49c5ddb7d
Alex Curran <alexcurran.va@gmail.com>
2024-09-17 09:40:44 -0500

    Refactor solution for conciseness

--- 7-kyu/every-nth-array-element-basic.js
+++ 7-kyu/every-nth-array-element-basic.js
@@ -1,7 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} arr 
+//  * @param {number} interval 
+//  * @param {number} start 
+//  * @returns {number[]}
+//  */
+// function every(arr, interval, start) {
+//     // one argument
+//     if (!interval && !start) {
+//         return arr;
+//     }
+
+//     // two arguments
+//     if (!start) {
+//         let results = [];
+
+//         for (let i = 0; i < arr.length; i += interval) {
+//             results.push(arr[i]);
+//         }
+
+//         return results;
+//     }
+
+//     // three arguments
+//     let results = [];
+
+//     for (let i = start; i < arr.length; i += interval) {
+//         results.push(arr[i]);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1
+ * solution 2 -- refactor
  * time: O(n)
  * space: O(n)
  * 
@@ -11,26 +48,9 @@
  * @returns {number[]}
  */
 function every(arr, interval, start) {
-    // one argument
-    if (!interval && !start) {
-        return arr;
-    }
-
-    // two arguments
-    if (!start) {
-        let results = [];
-
-        for (let i = 0; i < arr.length; i += interval) {
-            results.push(arr[i]);
-        }
-
-        return results;
-    }
-
-    // three arguments
     let results = [];
 
-    for (let i = start; i < arr.length; i += interval) {
+    for (let i = start || 0; i < arr.length; i += interval || 1) {
         results.push(arr[i]);
     }
 
