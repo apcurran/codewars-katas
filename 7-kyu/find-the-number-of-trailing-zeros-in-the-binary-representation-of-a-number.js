99e7ee45e29f12c78ed0ae4cdde4870b59a222c3
Alex Curran <alexcurran.va@gmail.com>
2024-09-20 10:51:22 -0500

    Convert to bin str and iterate solution for find-the-number-of-trailing-zeros-in-the-binary-representation-of-a-number.js

---
+++ 7-kyu/find-the-number-of-trailing-zeros-in-the-binary-representation-of-a-number.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @returns {number} - binary zeros count
+ */
+function trailingZeros(n) {
+    const nBinary = n.toString(2);
+    let binZerosCount = 0;
+
+    for (let i = nBinary.length - 1; i >= 0; i--) {
+        const strDigit = nBinary[i];
+
+        if (strDigit === "0") {
+            binZerosCount++;
+        } else {
+            break;
+        }
+    }
+
+    return binZerosCount;
+}
+
+console.log(trailingZeros(4)); // 2
+console.log(trailingZeros(5)); // 0
