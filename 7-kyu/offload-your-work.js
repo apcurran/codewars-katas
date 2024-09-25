94b03f3e5f05cdc14879ebfb0ee0aba08167baf4
Alex Curran <alexcurran.va@gmail.com>
2024-09-25 09:58:19 -0500

    Linear time and constant space solution for offload-your-work.js

---
+++ 7-kyu/offload-your-work.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = freelancersTime array length
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} projectMinutes 
+ * @param {number[][]} freelancersTime 
+ * @returns {string}
+ */
+function workNeeded(projectMinutes, freelancersTime) {
+    let totalTimeLeft = projectMinutes;
+
+    for (let [freelancerHours, freelancerMinutes] of freelancersTime) {
+        const totalFreelancerTimeInMinutes = (freelancerHours * 60) + freelancerMinutes;
+        totalTimeLeft -= totalFreelancerTimeInMinutes;
+
+        if (totalTimeLeft <= 0) {
+            break;
+        }
+    }
+
+    if (totalTimeLeft <= 0) {
+        return "Easy Money!";
+    } else {
+        const [hours, minutes] = convertMinutesToHoursAndMinutes(totalTimeLeft);
+
+        return `I need to work ${hours} hour(s) and ${minutes} minute(s)`;
+    }
+}
+
+console.log(workNeeded(60, [[1,0]])); // "Easy Money!"
+console.log(workNeeded(60, [[0,0]])); // "I need to work 1 hour(s) and 0 minute(s)"
+
+/**
+ * @param {number} timeInMinutes 
+ * @returns {[number, number]} [hours, minutes]
+ */
+function convertMinutesToHoursAndMinutes(timeInMinutes) {
+    const MINUTES_IN_HOUR = 60;
+    const hours = Math.floor(timeInMinutes / MINUTES_IN_HOUR);    
+    const minutes = timeInMinutes % MINUTES_IN_HOUR;
+
+    return [hours, minutes];
+}
