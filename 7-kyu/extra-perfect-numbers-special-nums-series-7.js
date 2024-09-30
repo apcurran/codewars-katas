4f852ff08d532a9060eaa2ee307bcf1a5af271f0
Alex Curran <alexcurran.va@gmail.com>
2024-09-30 15:34:58 -0500

    Solution using iterative approach for extra-perfect-numbers-special-nums-series-7.js

---
+++ 7-kyu/extra-perfect-numbers-special-nums-series-7.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @returns {number[]}
+ */
+function extraPerfect(n) {
+    let xtraPerfectNums = [];
+
+    for (let i = 1; i <= n; i++) {
+        const binStr = i.toString(2);
+
+        if (binStr[0] === "1" && binStr[binStr.length - 1] === "1") {
+            xtraPerfectNums.push(i);
+        }
+    }
+
+    return xtraPerfectNums;
+}
+
+console.log(extraPerfect(3)); // [1, 3]
+console.log(extraPerfect(5)); // [1, 3, 5]
