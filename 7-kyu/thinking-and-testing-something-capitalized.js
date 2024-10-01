71affb1b4f8cacd3f49ac78e2193f259c6f65e24
Alex Curran <alexcurran.va@gmail.com>
2024-10-01 10:10:41 -0500

    Iterative approach to thinking-and-testing-something-capitalized.js

---
+++ 7-kyu/thinking-and-testing-something-capitalized.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s 
+ * @returns {string}
+ */
+function testit(s) {
+    let str = "";
+
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i];
+        const nextChar = s[i + 1];
+        const lastIndex = s.length - 1;
+
+        if (nextChar === " " || i === lastIndex) {
+            str += currentChar.toUpperCase();
+        } else {
+            str += currentChar;
+        }
+    }
+
+    return str;
+}
+
+console.log(testit("")); // ""
+console.log(testit("a")); // "A"
+console.log(testit("a a")); // "A A"
+console.log(testit("a b c")); // "A B C"
