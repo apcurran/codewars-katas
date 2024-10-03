76b484bad71eb0a820a8c84d7bd607a4e2fa92d1
Alex Curran <alexcurran.va@gmail.com>
2024-10-03 11:27:52 -0500

    CAST ints to TEXT datatype and then get string length for easy-sql-moving-values.sql

---
+++ sql/7-kyu/easy-sql-moving-values.sql
@@ -0,0 +1,7 @@
+SELECT
+    LENGTH(name) AS id,
+    LENGTH(CAST(legs AS TEXT)) AS name,
+    LENGTH(CAST(arms AS TEXT)) AS legs,
+    LENGTH(characteristics) AS arms,
+    LENGTH(CAST(id AS TEXT)) AS characteristics
+FROM monsters;
