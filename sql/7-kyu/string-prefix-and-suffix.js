5e67b93790931c876e5adfdde168fe6aa4297a5f
Alex Curran <alexcurran.va@gmail.com>
2024-10-03 11:59:12 -0500

    Working solution for string-prefix-and-suffix.js

---
+++ sql/7-kyu/string-prefix-and-suffix.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {number}
+ */
+function solve(str) {
+    let maxPrefixSuffixLength = 0;
+
+    for (let i = 1; i <= str.length / 2; i++) {
+        const prefix = str.slice(0, i);
+        const suffix = str.slice(str.length - i);
+
+        if (prefix === suffix) {
+            maxPrefixSuffixLength = Math.max(maxPrefixSuffixLength, prefix.length);
+        }
+    }
+
+    return maxPrefixSuffixLength;
+}
+
+console.log(solve("abcda")); // 1
+console.log(solve("abcabca")); // 1
+console.log(solve("aaaa")); // 2
