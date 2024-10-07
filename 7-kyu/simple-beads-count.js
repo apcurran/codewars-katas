befe2ec20bab0a85f74510dbcb328b2c67eabe5a
Alex Curran <alexcurran.va@gmail.com>
2024-10-07 10:45:21 -0500

    Math-based solution for simple-beads-count.js

---
+++ 7-kyu/simple-beads-count.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1 -- math formula
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {number} red beads count
+ */
+function countRedBeads(n) {
+    if (n < 2) return 0;
+
+    return (n - 1) * 2;
+}
+
+console.log(countRedBeads(0)); // 0
+console.log(countRedBeads(1)); // 0
+console.log(countRedBeads(3)); // 4
+console.log(countRedBeads(5)); // 8
