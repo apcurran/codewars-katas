e3e743052705dfe587d8081296707feee6a915c3
Alex Curran <alexcurran.va@gmail.com>
2024-10-11 16:13:35 -0500

    Regex match solution for clever-split.js

---
+++ 6-kyu/clever-split.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1 -- regex match
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s 
+ * @returns {string[]|null}
+ */
+function cleverSplit(s) {
+    return s.match(/\[.*?\]|\S+/g);
+}
+
+console.log(cleverSplit("Buy a !car [!red green !white] [cheap or !new]"));
+// ['Buy', 'a', '!car', '[!red green !white]', '[cheap or !new]']
