f297d959be7476c015be7d45fd567c299332d2fe
Alex Curran <alexcurran.va@gmail.com>
2024-10-21 11:11:49 -0500

    Hashset solution for simple-fun-259-different-digits-number-search.js

---
+++ 6-kyu/simple-fun-259-different-digits-number-search.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * n = arr length
+ * m = num length
+ * time: O(n * m)
+ * space: O(m)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function differentDigitsNumberSearch(arr) {
+    let result = -1;
+
+    arrLoop: for (let num of arr) {
+        const uniqueDigits = new Set();
+
+        for (let digitStr of String(num)) {
+            if (uniqueDigits.has(digitStr)) {
+                // duplicate digit found, go to next num in arr
+                continue arrLoop;
+            }
+
+            uniqueDigits.add(digitStr);
+        }
+
+        // all num digits are unique, so update result
+        result = num;
+        // exit loop
+        break;
+    }
+
+    return result;
+}
+
+console.log(differentDigitsNumberSearch([22, 111, 101, 124, 33, 30])); // 124
+console.log(differentDigitsNumberSearch([1111, 404])); // -1
