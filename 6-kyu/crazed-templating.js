67c1fc24d3b6066c20db3f753fde2b9ccc0eccb2
Alex Curran <alexcurran.va@gmail.com>
2024-10-21 12:00:22 -0500

    Iterative words function to adjust for non-same chars at indexed location for crazed-templating.js

---
+++ 6-kyu/crazed-templating.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * n = words length
+ * m = word length
+ * time: O(n * m)
+ * space: O(m)
+ * 
+ * @param {string[]} words 
+ * @returns {string}
+ */
+function letterPattern(words) {
+    let pattern = words[0].split("");
+
+    // iterate each word in words list (start at the second word in list)
+    for (let i = 1; i < words.length; i++) {
+        // iterate each char in current word
+        for (let j = 0; j < words[i].length; j++) {
+            // if this current word's letter is not the same
+            // as the pattern template set by the first word,
+            // change current char to "*"
+            if (words[i][j] !== pattern[j]) {
+                pattern[j] = "*";
+            }
+        }
+    }
+
+    return pattern.join("");
+}
+
+console.log(letterPattern(["war", "rad", "dad"])); // "*a*"
+console.log(letterPattern(["general", "admiral", "piglets", "secrets"])); // "*******"
+console.log(letterPattern(["family"])); // "family"
