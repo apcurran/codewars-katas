87efbe3d0e5e6f04ec369367a82b9cea57fc248a
Alex Curran <alexcurran.va@gmail.com>
2024-10-25 10:40:42 -0500

    Iterative solution for min-value-of-each-n-elements-simple-version.js

---
+++ 6-kyu/min-value-of-each-n-elements-simple-version.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * m = arr length
+ * n = group arr length
+ * time: O(m * n)
+ * time: O(m + n)
+ * 
+ * @param {number[]} arr 
+ * @param {number} n 
+ * @returns {number[]}
+ */
+function minValue(arr, n) {
+    let results = [];
+
+    for (let i = 0; i < arr.length - n + 1; i++) {
+        const group = arr.slice(i, i + n);
+        const minValue = Math.min(...group);
+        results.push(minValue);
+    }
+
+    return results;
+}
+
+console.log(minValue([1, -2, 3, -4, 5, -6, 7, 8], 1)); // [1,-2,3,-4,5,-6,7,8]
+console.log(minValue([1, -2, 3, -4, 5, -6, 7, 8], 2)); // [-2,-2,-4,-4,-6,-6,7]
+console.log(minValue([1, -2, 3, -4, 5, -6, 7, 8], 3)); // [-2,-4,-4,-6,-6,-6]
+console.log(minValue([1, -2, 3, -4, 5, -6, 7, 8], 4)); // [-4,-4,-6,-6,-6]
