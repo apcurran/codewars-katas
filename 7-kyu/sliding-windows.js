528794abf2d286b4cdc68e93636e47f0d06273a8
Alex Curran <alexcurran.va@gmail.com>
2024-10-29 10:23:09 -0500

    Working solution for sliding-windows.js

---
+++ 7-kyu/sliding-windows.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n^2)
+ * 
+ * @param {number} length 
+ * @param {number} offset 
+ * @param {number[]} list 
+ * @returns {number[][]}
+ */
+function myWindow(length, offset, list) {
+    let results = [];
+
+    if (length === 0) {
+        results.push([]);
+    }
+
+    for (let i = 0; i < list.length; i += offset) {
+        const chunk = list.slice(i, i + length);
+
+        if (chunk.length !== length) {
+            // exit early if not enough at the end to accomodate the length
+            break;
+        }
+
+        results.push(chunk);
+    }
+
+    return results;
+}
+
+console.log(myWindow(2, 1, [0, 1, 2, 3, 4])); // [ [0,1], [1,2], [2,3], [3,4] ]
+console.log(myWindow(2, 3, [0, 1, 2, 3, 4])); // [ [0,1], [3,4] ]
+console.log(myWindow(0, 1, [0,1,2])); // [ [], [], [], [] ]
