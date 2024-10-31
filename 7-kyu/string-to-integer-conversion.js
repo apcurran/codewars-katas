fd3f8969cc3a07fb660544196d7e75b219736103
Alex Curran <alexcurran.va@gmail.com>
2024-10-31 11:18:44 -0500

    Working solution for string-to-integer-conversion.js

---
+++ 7-kyu/string-to-integer-conversion.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @returns {number|NaN}
+ */
+function myParseInt(str) {
+    const onlyUnsignedInt = /^\s*\d+\s*$/.test(str);
+
+    if (onlyUnsignedInt) {
+        return Number(str);
+    } else {
+        return NaN;
+    }
+}
+
+console.log(myParseInt("08")); // 8
+console.log(myParseInt("5 friends")); // NaN
+console.log(myParseInt("16.5")); // NaN
