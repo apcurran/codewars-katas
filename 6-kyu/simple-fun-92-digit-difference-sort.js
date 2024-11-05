420f4e175de0d89f15f9c3f8a05178f1cff2688b
Alex Curran <alexcurran.va@gmail.com>
2024-11-05 12:01:48 -0600

    Custom sort comparator func for built-in array method solution for simple-fun-92-digit-difference-sort.js

---
+++ 6-kyu/simple-fun-92-digit-difference-sort.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/**
+ * solution 1 -- array.sort() custom comparator func
+ * n = arr length
+ * m = num digits length
+ * time: O(n * log n * m)
+ * space: O(n + m)
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function digitDifferenceSort(arr) {
+    return arr.sort(function sortDigitDiff(numA, numB) {
+        const [numADigitMin, numADigitMax] = getDigitMinAndMax(numA);
+        const numADigitsDiff = numADigitMax - numADigitMin;
+        const [numBDigitMin, numBDigitMax] = getDigitMinAndMax(numB);
+        const numBDigitsDiff = numBDigitMax - numBDigitMin;
+
+        if (numADigitsDiff === numBDigitsDiff) {
+            return -1;
+        } else {
+            return numADigitsDiff - numBDigitsDiff;
+        }
+    });
+}
+
+/**
+ * @param {*} num
+ * @returns {[number, number]} [min, max]
+ */
+function getDigitMinAndMax(num) {
+    const strNum = String(num);
+    let min = Infinity;    
+    let max = -Infinity;
+
+    for (let strDigit of strNum) {
+        const digit = Number(strDigit);
+        min = Math.min(min, digit);
+        max = Math.max(max, digit);
+    }
+
+    return [min, max];
+}
+
+console.log(digitDifferenceSort([152, 23, 7, 887, 243])); // [7, 887, 23, 243, 152]
