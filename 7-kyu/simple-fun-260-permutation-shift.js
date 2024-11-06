1ca79cf2be11fe25684a66490ea0e5c071cd788d
Alex Curran <alexcurran.va@gmail.com>
2024-11-06 11:51:59 -0600

    Linear time and constant space solution for simple-fun-260-permutation-shift.js

---
+++ 7-kyu/simple-fun-260-permutation-shift.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} permutation 
+ * @returns {number} max and min diff
+ */
+function permutationShift(permutation) {
+    let maxPermutationShift = -Infinity;
+    let minPermutationShift = Infinity;
+
+    for (let i = 0; i < permutation.length; i++) {
+        const value = permutation[i];
+        const permutationShiftDifference = value - i;
+
+        if (permutationShiftDifference > maxPermutationShift) {
+            maxPermutationShift = permutationShiftDifference;
+        } else if (permutationShiftDifference < minPermutationShift) {
+            minPermutationShift = permutationShiftDifference;
+        }
+    }
+
+    return maxPermutationShift - minPermutationShift;
+}
+
+console.log(permutationShift([1, 0, 2, 3])); // 2
