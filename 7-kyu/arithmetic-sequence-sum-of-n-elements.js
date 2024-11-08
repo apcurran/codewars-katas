7f4bde3878e07b6402efe5b68c38d70a8b5b06e9
Alex Curran <alexcurran.va@gmail.com>
2024-11-08 17:24:42 -0600

    Iterative solution for arithmetic-sequence-sum-of-n-elements.js

---
+++ 7-kyu/arithmetic-sequence-sum-of-n-elements.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @param {number} r 
+ * @param {number} n 
+ * @returns {number}
+ */
+function ArithmeticSequenceSum(a, r, n) {
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        const currentSum = a + r * i;
+        sum += currentSum;
+    }
+
+    return sum;
+}
+
+console.log(ArithmeticSequenceSum(2, 3, 5)); // 40
