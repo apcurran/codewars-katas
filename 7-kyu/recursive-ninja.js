337a95773d8fa096bf1819b7ec378642c1ba4e6e
Alex Curran <alexcurran.va@gmail.com>
2024-11-11 10:27:08 -0600

    Recusive solution without the use of .join or .repeat methods

---
+++ 7-kyu/recursive-ninja.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * no join or repeat methods
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @param {string} chirps 
+ * @returns {string}
+ */
+function chirp(n, chirps = "") {
+    if (n === 1) {
+        // stop recursion
+        return "chirp";
+    }
+
+    const chirpsSoFar = chirp(n - 1);
+
+    return chirpsSoFar + "-chirp";
+}
+
+console.log(chirp(4)); // "chirp-chirp-chirp-chirp"
+console.log(chirp(2)); // "chirp-chirp"
