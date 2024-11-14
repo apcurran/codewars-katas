62919a63a05b29a141cb53ae4aa9bcef4eb135b5
Alex Curran <alexcurran.va@gmail.com>
2024-11-14 10:50:19 -0600

    O(sqrt(n)) solution for is-a-number-prime.js

---
+++ 6-kyu/is-a-number-prime.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(sqrt(n))
+ * space: O(1)
+ * 
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isPrime(num) {
+    if (num <= 1) return false;
+
+    const limit = Math.sqrt(num);
+
+    for (let i = 2; i <= limit; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(0)); // false
+console.log(isPrime(1)); // false
+console.log(isPrime(2)); // true
+console.log(isPrime(73)); // true
+console.log(isPrime(75)); // false
+console.log(isPrime(-1)); // false
