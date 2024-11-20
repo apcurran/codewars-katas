fb0cf8139297488ca47d94b3306189d0382a0066
Alex Curran <alexcurran.va@gmail.com>
2024-11-20 12:02:40 -0600

    Iterative solution for find-missing-numbers.js

---
+++ 7-kyu/find-missing-numbers.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n) with results arr
+ * 
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function findMissingNumbers(arr) {
+    const start = arr[0];
+    const end = arr[arr.length - 1];
+    let results = [];
+
+    for (let i = start + 1; i < end; i++) {
+        if (!arr.includes(i)) {
+            results.push(i);
+        }
+    }
+
+    return results;
+}
+
+console.log(findMissingNumbers([-3,-2,1,5])); // [-1,0,2,3,4]
