06e2942dbc4011623fbf97eafcdef20eeb3a82a8
Alex Curran <alexcurran.va@gmail.com>
2024-11-30 11:15:20 -0600

    Working solution for grasshopper-debug.js

---
+++ 8-kyu/grasshopper-debug.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} temp 
+ * @returns {string}
+ */
+function weatherInfo(temp) {
+    const c = convertToCelsius(temp);
+
+    if (c < 0) {
+        return `${c} is freezing temperature`;
+    } else {
+        return `${c} is above freezing temperature`;
+    }
+}
+
+/**
+ * @param {number} fahrenheitTemp 
+ * @returns {number}
+ */
+function convertToCelsius(fahrenheitTemp) {
+    const celsius = (fahrenheitTemp - 32) * (5 / 9);
+
+    return celsius;
+}
+
+console.log(weatherInfo(50)); // "10 is above freezing temperature"
+console.log(weatherInfo(23)); //  "-5 is freezing temperature"
