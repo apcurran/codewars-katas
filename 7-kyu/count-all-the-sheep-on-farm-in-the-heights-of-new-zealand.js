4fc30415dca54481999f67f1b2d08884817d1e91
Alex Curran <alexcurran.va@gmail.com>
2024-12-09 11:33:28 -0600

    Iterative solution for count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js

---
+++ 7-kyu/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n + m)
+ * space: O(1)
+ * 
+ * @param {number[]} sheepFriday 
+ * @param {number[]} sheepSaturday 
+ * @param {number} sheepTotal 
+ * @returns {number}
+ */
+function lostSheep(sheepFriday, sheepSaturday, sheepTotal) {
+    const sheepFridaySum = sheepFriday.reduce((sum, current) => sum + current, 0);
+    const sheepSaturdaySum = sheepSaturday.reduce((sum, current) => sum + current, 0);
+
+    return sheepTotal - sheepFridaySum - sheepSaturdaySum;
+}
+
+console.log(lostSheep([1, 2], [3, 4], 15)); // 5
