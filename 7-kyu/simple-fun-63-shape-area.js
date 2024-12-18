7635f9f1b9a81f11b7dab7c48a104764f1fcdec7
Alex Curran <alexcurran.va@gmail.com>
2024-12-18 10:14:55 -0600

    Math formula solution for simple-fun-63-shape-area.js

---
+++ 7-kyu/simple-fun-63-shape-area.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1 -- math formula
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {number}
+ */
+function shapeArea(n) {
+    return (n ** 2) + ((n - 1) ** 2);
+}
+
+console.log(shapeArea(1)); // 1
+console.log(shapeArea(2)); // 5
+console.log(shapeArea(3)); // 13
