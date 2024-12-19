5060a59e320b585c87ccbac95fd15133e73fd28a
Alex Curran <alexcurran.va@gmail.com>
2024-12-19 11:09:32 -0600

    Optimize solution to round without string coercion

--- 7-kyu/so-easy-charge-time-calculation.js
+++ 7-kyu/so-easy-charge-time-calculation.js
@@ -1,9 +1,27 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n) -- stringify num
+//  * space: O(n) -- stringify num
+//  * 
+//  * @param {number} battery 
+//  * @param {number} charger 
+//  * @returns {number} rounded to 2 decimal places
+//  */
+// function calculateTime(battery, charger) {
+//     const zeroToEightyFivePercent = (battery * .85) / charger;
+//     const eightyFiveToNinetyFivePercent = (battery * .1) / (charger * .5);
+//     const ninetyFiveToFull = (battery * .05) / (charger * .2);
+//     const totalTime = zeroToEightyFivePercent + eightyFiveToNinetyFivePercent + ninetyFiveToFull;
+
+//     return Number(totalTime.toFixed(2));
+// }
+
 /**
- * solution 1
- * time: O(n) -- stringify num
- * space: O(n) -- stringify num
+ * solution 2 -- no stringification of num
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} battery 
  * @param {number} charger 
@@ -15,7 +33,7 @@ function calculateTime(battery, charger) {
     const ninetyFiveToFull = (battery * .05) / (charger * .2);
     const totalTime = zeroToEightyFivePercent + eightyFiveToNinetyFivePercent + ninetyFiveToFull;
 
-    return Number(totalTime.toFixed(2));
+    return Math.round(totalTime * 100) / 100;
 }
 
 console.log(calculateTime(1000, 500)); // 2.6
