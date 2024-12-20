4a880cb2129e8eb1f62dc3b0efb3b660c887f112
Alex Curran <alexcurran.va@gmail.com>
2024-12-20 10:21:52 -0600

    Working solution for simple-fun-10-range-bit-counting.js

---
+++ 7-kyu/simple-fun-10-range-bit-counting.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = a to b list
+ * m = binaryStr of each num
+ * time: O(n * m)
+ * space: O(m)
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function rangeBitCount(a, b) {
+    let totalOneBitsCount = 0;
+
+    for (let i = a; i <= b; i++) {
+        const binaryNum = i.toString(2);
+        let binaryNumOneBitsCount = 0;
+
+        for (let digitStr of binaryNum) {
+            if (digitStr === "1") binaryNumOneBitsCount++;
+        }
+
+        totalOneBitsCount += binaryNumOneBitsCount;
+    }
+
+    return totalOneBitsCount;
+}
+
+console.log(rangeBitCount(2, 7)); // 11
+console.log(rangeBitCount(0, 1)); // 1
+console.log(rangeBitCount(4, 4)); // 1
