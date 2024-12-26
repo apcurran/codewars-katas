dab8d95fec51ca05d979e4a55e9cb46d32377269
Alex Curran <alexcurran.va@gmail.com>
2024-12-26 10:36:09 -0600

    Linear time, constant space iterative solution for ean-validation.js

---
+++ 7-kyu/ean-validation.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} eanCode 
+ * @returns {boolean}
+ */
+function validateEAN(eanCode) {
+    let total = 0;
+
+    for (let i = 0; i < eanCode.length; i++) {
+        const digit = Number(eanCode[i]);
+
+        if (i % 2 === 0) {
+            total += digit;
+        } else {
+            total += digit * 3;
+        }
+    }
+
+    return total % 10 === 0;
+}
+
+console.log(validateEAN("400330101839")); // false (checksum: 8)
