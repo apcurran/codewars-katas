98f7ebdebea3b7d6911e8c91d84a277d223f696d
Alex Curran <alexcurran.va@gmail.com>
2025-01-03 15:31:51 -0600

    Anagram solution for simple-fun-32-create-anagram.js

---
+++ 7-kyu/simple-fun-32-create-anagram.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(s * t)
+ * space: O(t)
+ * 
+ * @param {string} s 
+ * @param {string} t 
+ * @returns {number}
+ */
+function createAnagram(s, t) {
+    let uniqueTChars = t;
+
+    for (let char of s) {
+        const updatedStr = uniqueTChars.replace(char, "");
+        uniqueTChars = updatedStr;
+    }
+
+    return uniqueTChars.length;
+}
+
+console.log(createAnagram("AABAA", "BBAAA")); // 1
+console.log(createAnagram("OVGHK", "RPGUC")); // 4
