90550d257af3ccd4da6832405b4484a3f0a0bdd7
Alex Curran <alexcurran.va@gmail.com>
2025-01-10 10:23:15 -0600

    Exponentiation operator solution for potenciation.js

---
+++ 8-kyu/potenciation.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * solution 1 -- exponentiation operator
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns {number}
+ */
+function power(x, y) {
+    return x ** y;
+}
