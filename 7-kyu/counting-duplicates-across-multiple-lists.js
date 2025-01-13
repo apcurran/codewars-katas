9463354bc76f0c3c0602bdc01bf23e9b0a46a51c
Alex Curran <alexcurran.va@gmail.com>
2025-01-13 10:31:13 -0600

    Hashset solution for counting-duplicates-across-multiple-lists.js

---
+++ 7-kyu/counting-duplicates-across-multiple-lists.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * n = name length
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[]} name 
+ * @param {number[]} age 
+ * @param {number[]} height 
+ * @returns {number}
+ */
+function countDuplicates(name, age, height) {
+    let uniquePeople = new Set();
+
+    for (let i = 0; i < name.length; i++) {
+        const person = `${name[i]}${age[i]}${height[i]}`;
+        uniquePeople.add(person);            
+    }
+
+    return name.length - uniquePeople.size;
+}
+
+const myNames = ["John","Beth","Beth","Beth","Bill"];
+const age = [37,23,23,23,46];
+const height = [183,170,170,170,175];
+console.log(countDuplicates(myNames, age, height)); // 2
