5a6f95265489f43fe24c7db3a603a98a7e9ca9c0
Alex Curran <alexcurran.va@gmail.com>
2025-01-16 11:17:44 -0600

    Array .every() prototype func callback solution for every-archer-has-its-arrows.js

---
+++ 7-kyu/every-archer-has-its-arrows.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} archers 
+ * @returns {boolean}
+ */
+function archersReady(archers) {
+    if (archers.length < 1) return false;
+
+    return archers.every((archer) => archer >= 5);
+}
+
+console.log(archersReady([1, 2, 3, 4])); // false
+console.log(archersReady([5, 6, 7, 8])); // true
