b399b2deb74d7899d5e997822b8c6dc34f63211f
Alex Curran <alexcurran.va@gmail.com>
2025-01-21 11:07:51 -0600

    Two pointers solution for valid submission

--- 7-kyu/simple-fun-12-count-sum-of-two-representations.js
+++ 7-kyu/simple-fun-12-count-sum-of-two-representations.js
@@ -1,8 +1,40 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (correct, but TLE)
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number} goalSum 
+//  * @param {number} left 
+//  * @param {number} right 
+//  * @returns {number}
+//  */
+// function countSumOfTwoRepresentations(goalSum, left, right) {
+//     let validSumsCount = 0;
+
+//     for (let a = left; a <= right; a++) {
+//         for (let b = left; b <= right; b++) {
+//             if (a > b) continue;
+
+//             const currentSum = a + b;
+
+//             if (currentSum > goalSum) {
+//                 break;
+//             }
+
+//             if (currentSum === goalSum) {
+//                 validSumsCount++;
+//             }
+//         }
+//     }
+
+//     return validSumsCount;
+// }
+
 /**
- * solution 1 -- brute force (correct, but TLE)
- * time: O(n^2)
+ * solution 2 -- two pointers technique
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number} goalSum 
@@ -13,19 +45,16 @@
 function countSumOfTwoRepresentations(goalSum, left, right) {
     let validSumsCount = 0;
 
-    for (let a = left; a <= right; a++) {
-        for (let b = left; b <= right; b++) {
-            if (a > b) continue;
+    while (left <= right) {
+        const sum = left + right;
 
-            const currentSum = a + b;
-
-            if (currentSum > goalSum) {
-                break;
-            }
-
-            if (currentSum === goalSum) {
-                validSumsCount++;
-            }
+        if (sum === goalSum) {
+            validSumsCount++;
+            left++;
+        } else if (sum > goalSum) {
+            right--;
+        } else if (sum < goalSum) {
+            left++;
         }
     }
 
