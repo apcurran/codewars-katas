e4d9410c8a73f48491c46c68c9033c2aae88d1f8
Alex Curran <alexcurran.va@gmail.com>
2025-01-21 10:56:44 -0600

    Brute force TLE solution for simple-fun-12-count-sum-of-two-representations.js

---
+++ 7-kyu/simple-fun-12-count-sum-of-two-representations.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1 -- brute force (correct, but TLE)
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} goalSum 
+ * @param {number} left 
+ * @param {number} right 
+ * @returns {number}
+ */
+function countSumOfTwoRepresentations(goalSum, left, right) {
+    let validSumsCount = 0;
+
+    for (let a = left; a <= right; a++) {
+        for (let b = left; b <= right; b++) {
+            if (a > b) continue;
+
+            const currentSum = a + b;
+
+            if (currentSum === goalSum) {
+                validSumsCount++;
+            }
+        }
+    }
+
+    return validSumsCount;
+}
+
+console.log(countSumOfTwoRepresentations(6, 2, 4)); // 2 -> (2 + 4 = 6) (3 + 3 = 6)
