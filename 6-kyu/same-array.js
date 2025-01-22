f2d06289b1fd53f3ed3bfa4fda9f445e1e501037
Alex Curran <alexcurran.va@gmail.com>
2025-01-22 14:19:17 -0600

    Sorting solution for same-array.js

---
+++ 6-kyu/same-array.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * a = aArr length
+ * b = bArr length
+ * time: O((a * log n) + (b * log b))
+ * space: O(a + b)
+ * 
+ * @param {number[][]} aArr 
+ * @param {number[][]} bArr 
+ * @returns 
+ */
+function same(aArr, bArr) {
+    // sort inner subarrays, then sort outer arrays holding subarrays
+    const aOrdered = aArr.map((subArr) => subArr.sort()).sort();
+    const bOrdered = bArr.map((subArr) => subArr.sort()).sort();
+
+    return String(aOrdered) === String(bOrdered);
+}
+
+console.log(same([[2,5], [3,6]], [[5,2], [3,6]])); //  same -> true
