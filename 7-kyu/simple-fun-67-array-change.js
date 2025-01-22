91e5c9a281d098bdc60f9d98add44dc7a5161faf
Alex Curran <alexcurran.va@gmail.com>
2025-01-22 11:42:47 -0600

    Working iterative solution for simple-fun-67-array-change.js

---
+++ 7-kyu/simple-fun-67-array-change.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(1)
+ * 
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function arrayChange(arr) {
+    let movesCounter = 0;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        while (arr[i + 1] <= arr[i]) {
+            // increment next elem
+            arr[i + 1]++;
+            movesCounter++;
+        }
+    }
+
+    return movesCounter;
+}
+
+console.log(arrayChange([-1000, 0, -2, 0])); // 5
