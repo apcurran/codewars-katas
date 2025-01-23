1c6a99417aa8e901adbe7e2cee3c950f5d4e0906
Alex Curran <alexcurran.va@gmail.com>
2025-01-23 11:40:20 -0600

    Regex solution for create-sequence-containing-all-allowed-characters.js

---
+++ 7-kyu/create-sequence-containing-all-allowed-characters.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = total regex chars identified
+ * time: O(n) -- loop is constant time, but regex.test() is dependent upon regex chars identified
+ * space: O(1) -- not including result space
+ * 
+ * @param {RegExp} regex 
+ * @returns {string}
+ */
+function createSequence(regex) {
+    if (!(regex instanceof RegExp)) {
+        throw new Error("The given argument is not in a regex form.");
+    }
+
+    let result = "";
+
+    for (let i = 0; i < 128; i++) {
+        const asciiChar = String.fromCharCode(i);
+
+        if (regex.test(asciiChar)) {
+            result += asciiChar;
+        }
+    }
+
+    return result;
+}
+
+console.log(createSequence(/[0-9A-F]/)); // "0123456789ABCDEF"
