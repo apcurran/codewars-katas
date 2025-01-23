733901daad656d35916928164af48bf5fcc3c32b
Alex Curran <alexcurran.va@gmail.com>
2025-01-23 13:53:37 -0600

    Working solution for string-subpattern-recognition-1.js

---
+++ 6-kyu/string-subpattern-recognition-1.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = str length
+ * m = subPattern length
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {boolean}
+ */
+function hasSubpattern(str) {
+    for (let i = 1; i <= str.length / 2; i++) {
+        if (str.length % i !== 0) continue;
+
+        // otherwise, check
+        const subPattern = str.slice(0, i);
+        const repeatedPattern = subPattern.repeat(str.length / i);
+
+        if (repeatedPattern === str) return true;
+    }
+
+    return false;
+}
+
+console.log(hasSubpattern("a")); // false //no repeated pattern
+console.log(hasSubpattern("aaaa")); // true //created repeating "a"
+console.log(hasSubpattern("abcd")); // false //no repeated pattern
+console.log(hasSubpattern("abababab")); // true //created repeating "ab"
+console.log(hasSubpattern("ababababa")); // false //cannot be entirely reproduced repeating a pattern
