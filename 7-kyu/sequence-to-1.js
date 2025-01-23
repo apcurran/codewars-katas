a0e63a46756c70332447c6321c622c427341a75c
Alex Curran <alexcurran.va@gmail.com>
2025-01-23 09:50:40 -0600

    Linear time solution for sequence-to-1.js

---
+++ 7-kyu/sequence-to-1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- not including results array
+ * 
+ * @param {number} n 
+ * @returns {number[]}
+ */
+function seqToOne(n) {
+    let results = [];
+
+    if (n <= 1) {
+        while (n <= 1) {
+            results.push(n);
+            n++;
+        }
+    } else {
+        while (n >= 1) {
+            results.push(n);
+            n--;
+        }
+    }
+
+    return results;
+}
+
+console.log(seqToOne(5)); // [5, 4, 3, 2, 1]
+console.log(seqToOne(-1)); // [-1, 0, 1]
