4784686e089c15a98b71da323c2418efe5be5a1c
Alex Curran <alexcurran.va@gmail.com>
2025-01-28 12:37:04 -0600

    Optimize math approach for time/space complexities

--- 7-kyu/sine-cosine-and-others.js
+++ 7-kyu/sine-cosine-and-others.js
@@ -1,25 +1,47 @@
 "use strict";
 
+// /**
+//  * solution 1 -- math
+//  * n = numbers
+//  * time: O(max(n)) -- stringification dependent upon largest num
+//  * space: O(max(n))
+//  * 
+//  * @param {number} sine 
+//  * @returns {number[]}
+//  */
+// function sctc(sine) {
+//     const cosine = Math.sqrt(1 - sine ** 2);
+//     const tangent = cosine === 0 ? 0 : sine / cosine;
+//     const cotangent = tangent === 0 ? null : (1 / tangent);
+
+//     const roundedSine = Number(sine.toFixed(2));
+//     const roundedCosine = Number(cosine.toFixed(2));
+//     const roundedTangent = Number(tangent.toFixed(2));
+//     const roundedCotangent = cotangent ? Number(cotangent.toFixed(2)) : null;
+
+//     return [roundedSine, roundedCosine, roundedTangent, roundedCotangent].filter((value) => value !== null);
+// }
+
 /**
- * solution 1 -- math
+ * solution 2 -- optimized math
  * n = numbers
- * time: O(max(n)) -- stringification dependent upon largest num
- * space: O(max(n))
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} sine 
  * @returns {number[]}
  */
 function sctc(sine) {
     const cosine = Math.sqrt(1 - sine ** 2);
-    const tangent = cosine === 0 ? 0 : sine / cosine;
-    const cotangent = tangent === 0 ? null : (1 / tangent);
+    const tangent = sine / cosine;
+    const cotangent = 1 / tangent;
+    const validValues = [sine, cosine, tangent, cotangent].filter(function removeInvalidValues(value) {
+        return Number.isFinite(value);
+    });
 
-    const roundedSine = Number(sine.toFixed(2));
-    const roundedCosine = Number(cosine.toFixed(2));
-    const roundedTangent = Number(tangent.toFixed(2));
-    const roundedCotangent = cotangent ? Number(cotangent.toFixed(2)) : null;
-
-    return [roundedSine, roundedCosine, roundedTangent, roundedCotangent].filter((value) => value !== null);
+    return validValues.map(function roundToTwoDecimalPlaces(num) {
+        return Math.round(num * 100) / 100;
+    });
 }
 
 console.log(sctc(1)); // [1, 0.0, 0.0]
