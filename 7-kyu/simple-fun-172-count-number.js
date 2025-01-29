16d5a4d2b0ac94f411b8ab735e16fe754b00447b
Alex Curran <alexcurran.va@gmail.com>
2025-01-29 10:57:05 -0600

    Optimize solution 2 for linear time and constant space

--- 7-kyu/simple-fun-172-count-number.js
+++ 7-kyu/simple-fun-172-count-number.js
@@ -1,8 +1,33 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (TLE)
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number} n 
+//  * @param {number} targetX 
+//  * @returns {number}
+//  */
+// function countNumber(n, targetX) {
+//     let targetNumCount = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = 1; j <= n; j++) {
+//             const cellValue = i * j;
+
+//             if (cellValue === targetX) {
+//                 targetNumCount++;
+//             }
+//         }
+//     }
+
+//     return targetNumCount;
+// }
+
 /**
- * solution 1 -- brute force (TLE)
- * time: O(n^2)
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number} n 
@@ -13,12 +38,10 @@ function countNumber(n, targetX) {
     let targetNumCount = 0;
 
     for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= n; j++) {
-            const cellValue = i * j;
+        const max = i * n;
 
-            if (cellValue === targetX) {
-                targetNumCount++;
-            }
+        if (targetX % i === 0 && targetX <= max) {
+            targetNumCount++;
         }
     }
 
