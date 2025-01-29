1c2e43e1edcf3ba3c9e6d9eb7192828349247ca1
Alex Curran <alexcurran.va@gmail.com>
2025-01-29 13:45:06 -0600

    Brute force, but TLE for satisfying-numbers.js

---
+++ 7-kyu/satisfying-numbers.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- brute force
+ * time: O(n) -- bounded by n (cannot be greater than 40)
+ * space: O(1)
+ * @param {number} n 
+ * @returns {number}
+ */
+function smallest(n) {
+    let smallestDivisibleNum = n;
+
+    while (true) {
+        for (let i = 2; i <= n; i++) {
+            if (smallestDivisibleNum % i !== 0) {
+                smallestDivisibleNum++;
+                // reset loop
+                i = 1;
+            }
+        }
+
+        // all nums divisible without a remainder
+        return smallestDivisibleNum;
+    }
+}
+
+console.log(smallest(5)); // 60
