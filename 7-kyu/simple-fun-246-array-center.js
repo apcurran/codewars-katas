4beaac6a7d7f7923795bd83f4fafd9ef00a48e1f
Alex Curran <alexcurran.va@gmail.com>
2025-02-05 10:46:25 -0600

    Reduce & filter combo to solve simple-fun-246-array-center.js

---
+++ 7-kyu/simple-fun-246-array-center.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1 -- reduce & filter methods
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} a 
+ * @returns {number[]}
+ */
+function arrayCenter(a) {
+    const aMin = Math.min(...a);
+    const aAvg = (a.reduce((sum, curr) => sum + curr, 0)) / a.length;
+
+    return a.filter((num) =>  Math.abs(num - aAvg) < aMin);
+}
+
+console.log(arrayCenter([8, 3, 4, 5, 2, 8])); // [4, 5]
