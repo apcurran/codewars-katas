fd015421d44c848b4ecab5cee145bf27d0912ac0
Alex Curran <alexcurran.va@gmail.com>
2025-02-06 14:39:08 -0600

    Map, then sort with custom comparator func for find-the-nth-longest-string-in-an-array.js

---
+++ 6-kyu/find-the-nth-longest-string-in-an-array.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1 -- custom sort comparator func
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {string[]} arr 
+ * @param {number} n 1-based indexing
+ * @returns {string}
+ */
+function longest(arr, n) {
+    const sortedArr = arr
+                        .map(function storeStrAndIndex(str, index) {
+                            return {
+                                str,
+                                index
+                            };
+                        })
+                        .sort(function sortLengthThenOriginalOrder(objA, objB) {
+                            // same length? sort by original order
+                            if (objA.str.length === objB.str.length) {
+                                return objA.index - objB.index;
+                            } else {
+                                // otherwise, sort by length
+                                return objB.str.length - objA.str.length;
+                            }
+                        });
+
+    return sortedArr[n - 1].str;
+}
+
+console.log(longest(["Hello", "World", "Codewars", "Katas"], 3)); // "World"
