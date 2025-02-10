411671cf4f3122d9555df392e632ec234185146a
Alex Curran <alexcurran.va@gmail.com>
2025-02-10 11:59:30 -0600

    GCD helper func and main func solution for simple-fun-327-the-final-attack-value.js

---
+++ 7-kyu/simple-fun-327-the-final-attack-value.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = monsterList size
+ * a = playerAttackValue
+ * b = monsterDefenseValue
+ * time: O(n * log(min(a, b)))
+ * space: O(1) -- no extra data structures or recursive calls used
+ * 
+ * @param {number} playerAttackValue 
+ * @param {number[]} monsterList 
+ * @returns {number}
+ */
+function finalAttackValue(playerAttackValue, monsterList) {
+    for (let monsterDefenseValue of monsterList) {
+        if (monsterDefenseValue <= playerAttackValue) {
+            playerAttackValue += monsterDefenseValue;
+        } else {
+            playerAttackValue += getGCD(playerAttackValue, monsterDefenseValue);
+        }
+    }
+
+    return playerAttackValue;
+}
+
+console.log(finalAttackValue(50, [50,105,200])); // 110
+
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function getGCD(a, b) {
+    while (b !== 0) {
+        const temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
