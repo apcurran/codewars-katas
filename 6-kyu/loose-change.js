e524176bc75f7a4e61909a5b2eac2b5056872998
Alex Curran <alexcurran.va@gmail.com>
2025-02-14 15:11:48 -0600

    Working solution for loose-change.js

---
+++ 6-kyu/loose-change.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} cents 
+ * @returns {{
+ *  "Nickels": number,
+ *  "Pennies": number,
+ *  "Dimes": number,
+ *  "Quarters": number
+ * }}
+ */
+function looseChange(cents) {
+    let change = {
+        "Nickels": 0,
+        "Pennies": 0,
+        "Dimes": 0,
+        "Quarters": 0
+    };
+
+    if (cents <= 0) {
+        return change;
+    }
+
+    let flooredCents = Math.floor(cents);
+
+    while (flooredCents / 25 >= 1) {
+        // divide
+        const quartersCount = Math.floor(flooredCents / 25);
+        change.Quarters += quartersCount;
+        // get remainder
+        flooredCents = flooredCents % 25;
+    }
+
+    while (flooredCents / 10 >= 1) {
+        const dimesCount = Math.floor(flooredCents / 10);
+        change.Dimes += dimesCount;
+        flooredCents = flooredCents % 10;
+    }
+
+    while (flooredCents / 5 >= 1) {
+        const nickelsCount = Math.floor(flooredCents / 5);
+        change.Nickels += nickelsCount;
+        flooredCents = flooredCents % 5;
+    }
+
+    while (flooredCents / 1 >= 1) {
+        const penniesCount = Math.floor(flooredCents / 1);
+        change.Pennies += penniesCount;
+        flooredCents = flooredCents % 1;
+    }
+
+    return change;
+}
+
+console.log(looseChange(56)); // {'Nickels': 1, 'Pennies': 1, 'Dimes': 0, 'Quarters': 2}
