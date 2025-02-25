f6a857ebc591ddecbc2bdefad38ea86346c86043
Alex Curran <alexcurran.va@gmail.com>
2025-02-25 11:11:15 -0600

    Cleaner implementation of solution

--- 7-kyu/zero-balanced-array.js
+++ 7-kyu/zero-balanced-array.js
@@ -1,7 +1,34 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2) array.includes() call within loop
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums 
+//  * @returns {boolean}
+//  */
+// function ìsZeroBalanced(nums) {
+//     if (nums.length === 0) return false; // no empty array
+
+//     // sum to 0
+//     const sum = nums.reduce((sum, current) => sum + current, 0);
+
+//     if (sum !== 0) return false;
+//     // each positive num has another neg num
+//     for (let num of nums) {
+//         if (num > 0) {
+//             if (!nums.includes(-num)) {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 1
+ * solution 2 -- cleaner implementation
  * time: O(n^2) array.includes() call within loop
  * space: O(1)
  * 
@@ -9,22 +36,9 @@
  * @returns {boolean}
  */
 function ìsZeroBalanced(nums) {
-    if (nums.length === 0) return false; // no empty array
-
-    // sum to 0
-    const sum = nums.reduce((sum, current) => sum + current, 0);
-
-    if (sum !== 0) return false;
-    // each positive num has another neg num
-    for (let num of nums) {
-        if (num > 0) {
-            if (!nums.includes(-num)) {
-                return false;
-            }
-        }
-    }
-
-    return true;
+    return nums.length > 0 &&
+           nums.reduce((sum, current) => sum + current, 0) === 0 &&
+           nums.every((num) => nums.includes(-num));
 }
 
 console.log(ìsZeroBalanced([3])); // false
