6ea8eb88fb1b8b98712190a2b90075de918307dc
Alex Curran <alexcurran.va@gmail.com>
2025-02-25 11:06:34 -0600

    Working solution for zero-balanced-array.js

---
+++ 7-kyu/zero-balanced-array.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2) array.includes() call within loop
+ * space: O(1)
+ * 
+ * @param {number[]} nums 
+ * @returns {boolean}
+ */
+function ìsZeroBalanced(nums) {
+    if (nums.length === 0) return false; // no empty array
+
+    // sum to 0
+    const sum = nums.reduce((sum, current) => sum + current, 0);
+
+    if (sum !== 0) return false;
+    // each positive num has another neg num
+    for (let num of nums) {
+        if (num > 0) {
+            if (!nums.includes(-num)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+console.log(ìsZeroBalanced([3])); // false
+console.log(ìsZeroBalanced([0,0,0,0,0,0])); // true
