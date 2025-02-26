5c98860e24962d647d413841262e8fa173549ff1
Alex Curran <alexcurran.va@gmail.com>
2025-02-26 11:18:39 -0600

    Linear time / constant space solution for basic-js-calculating-averages.js

---
+++ basic-js-calculating-averages.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Calculator = {
+    /**
+     * solution 1
+     * time: O(n)
+     * space: O(1)
+     * 
+     * @param  {...number} args 
+     * @returns {number}
+     */
+    average(...args) {
+        if (args.length === 0) return 0;
+
+        const sum = args.reduce((sum, currentNum) => sum + currentNum, 0);
+
+        return sum / args.length;
+    }
+};
+
+console.log(Calculator.average()); // 0
+console.log(Calculator.average(3, 4, 5)); // 4
