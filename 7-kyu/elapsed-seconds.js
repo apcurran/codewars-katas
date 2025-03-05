c78a9fd05cd9c1f29d2e4eb6423e56b13c93d566
Alex Curran <alexcurran.va@gmail.com>
2025-03-05 12:03:20 -0600

    Working solution for elapsed-seconds.js

---
+++ 7-kyu/elapsed-seconds.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ * @returns {number} elapsed time in seconds
+ */
+function elapsedSeconds(startDate, endDate) {
+    return (endDate.getTime() - startDate.getTime()) / 1000;
+}
+
+const start = new Date(2013, 1, 1, 0, 0, 1);
+const end = new Date(2013, 1, 1, 0, 0, 20);
+console.log(elapsedSeconds(start, end)); // 19
