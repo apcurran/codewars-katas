6a9543018400e3c6e8372fdc0d33246d38d7eb0c
Alex Curran <alexcurran.va@gmail.com>
2025-03-17 11:42:04 -0500

    Custom sort comparacto func with Intl.Collator obj for locale comparison

---
+++ 7-kyu/alphabetize-a-list-by-the-nth-character.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- custom sort comparator func
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {string} list 
+ * @param {number} n 
+ * @returns {string}
+ */
+function sortIt(list, n) {
+    const collator = new Intl.Collator("en");
+    let words = list.split(", ");
+
+    return words.sort(function sortAlpha(wordA, wordB) {
+        const wordACharCodeLower = wordA[n - 1].toLowerCase().charCodeAt(0);
+        const wordBCharCodeLower = wordB[n - 1].toLowerCase().charCodeAt(0);
+
+        if (wordACharCodeLower === wordBCharCodeLower) {
+            return collator.compare(wordA, wordB);
+        } else {
+            return wordACharCodeLower - wordBCharCodeLower;
+        }
+    }).join(", ");
+}
+
+console.log(sortIt("bid, zag", 2)); // 'zag, bid'
