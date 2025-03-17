3986359fb7594a8b1677408e76f219db044b625a
Alex Curran <alexcurran.va@gmail.com>
2025-03-17 11:51:44 -0500

    Re-format for clarity on chained funcs

--- 7-kyu/alphabetize-a-list-by-the-nth-character.js
+++ 7-kyu/alphabetize-a-list-by-the-nth-character.js
@@ -13,16 +13,18 @@ function sortIt(list, n) {
     const collator = new Intl.Collator("en");
     let words = list.split(", ");
 
-    return words.sort(function sortAlpha(wordA, wordB) {
-        const wordACharCodeLower = wordA[n - 1].toLowerCase().charCodeAt(0);
-        const wordBCharCodeLower = wordB[n - 1].toLowerCase().charCodeAt(0);
+    return words
+        .sort(function sortAlpha(wordA, wordB) {
+            const wordACharCodeLower = wordA[n - 1].toLowerCase().charCodeAt(0);
+            const wordBCharCodeLower = wordB[n - 1].toLowerCase().charCodeAt(0);
 
-        if (wordACharCodeLower === wordBCharCodeLower) {
-            return collator.compare(wordA, wordB);
-        } else {
-            return wordACharCodeLower - wordBCharCodeLower;
-        }
-    }).join(", ");
+            if (wordACharCodeLower === wordBCharCodeLower) {
+                return collator.compare(wordA, wordB);
+            } else {
+                return wordACharCodeLower - wordBCharCodeLower;
+            }
+        })
+        .join(", ");
 }
 
 console.log(sortIt("bid, zag", 2)); // 'zag, bid'
