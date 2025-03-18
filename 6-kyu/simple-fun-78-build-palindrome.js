c1a060c6e60073621249e8b0319f3a2535001464
Alex Curran <alexcurran.va@gmail.com>
2025-03-18 15:13:56 -0500

    Working solution for simple-fun-78-build-palindrome.js

---
+++ 6-kyu/simple-fun-78-build-palindrome.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
+function buildPalindrome(str) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChunk = str.slice(i);
+
+        if (isPalindrome(currentChunk)) {
+            const reversedPriorChunk = str
+                                        .split("")
+                                        .slice(0, i)
+                                        .reverse()
+                                        .join("");
+
+            result = `${str}${reversedPriorChunk}`;
+
+            break;
+        }
+    }
+
+    return result;
+}
+
+/**
+ * @param {string} str 
+ * @returns {boolean}
+ */
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(buildPalindrome("abcdc")); // "abcdcba"
