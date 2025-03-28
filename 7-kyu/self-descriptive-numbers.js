7224c23402ff0e2be989c87f12b885f835ff0826
Alex Curran <alexcurran.va@gmail.com>
2025-03-28 14:22:22 -0500

    Linear time/space hashmap solution for self-descriptive-numbers.js

---
+++ 7-kyu/self-descriptive-numbers.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- hashmap
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function selfDescriptive(num) {
+    const numStr = String(num);
+    let digitsCount = new Map();
+
+    for (let digitStr of numStr) {
+        const previousCount = digitsCount.get(digitStr) || 0;
+        digitsCount.set(digitStr, previousCount + 1);
+    }
+
+    for (let i = 0; i < numStr.length; i++) {
+        const indexDigitCount = digitsCount.get(String(i)) || 0;
+        const currentDigit = Number(numStr[i]);
+
+        if (indexDigitCount !== currentDigit) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(selfDescriptive(21200)); // true
+console.log(selfDescriptive(3211000)); // true
+console.log(selfDescriptive(21230)); // false
