f0769fc487c44dddb623b9925481e2326b500f08
Alex Curran <alexcurran.va@gmail.com>
2025-03-31 10:18:38 -0500

    Object mapping solution for unix-command-line-ls-l-extract-the-file-type.js

---
+++ 7-kyu/unix-command-line-ls-l-extract-the-file-type.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- object mapping
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {string} fileAttribute 
+ * @returns {string}
+ */
+function linuxType(fileAttribute) {
+    const fileTypes = {
+        "-": "file",
+        "d": "directory",
+        "l": "symlink",
+        "c": "character_file",
+        "b": "block_file",
+        "p": "pipe",
+        "s": "socket",
+        "D": "door",
+    };
+
+    return fileTypes[fileAttribute[0]];
+}
+
+console.log(linuxType("-rwxrwxrwx")); //"file"
+console.log(linuxType("Drwxr-xr-x")); //"door"
+console.log(linuxType("lrwxrw-rw-")); //"symlink"
+console.log(linuxType("srwxrwxrwx")); //"socket"
