ae2910d1b9dee20a45d0ac9c840219d328c42736
Alex Curran <alexcurran.va@gmail.com>
2025-03-31 14:07:05 -0500

    Working solution for rotate-array.js

---
+++ 6-kyu/rotate-array.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {Array} arr 
+ * @param {number} n 
+ * @returns {Array}
+ */
+function rotate(arr, n) {
+    let results = arr.slice();
+    const nMod = n % results.length;
+
+    for (let i = 0; i < Math.abs(nMod); i++) {
+        if (n > 0) {
+            const removedElem = results.pop();
+            results.unshift(removedElem);
+        } else {
+            const removedElem = results.shift();
+            results.push(removedElem);
+        }
+    }
+
+    return results;
+}
+
+const data = [1, 2, 3, 4, 5];
+console.log(rotate(data, 2)); // [4, 5, 1, 2, 3]
+console.log(rotate(data, 0)); // [1, 2, 3, 4, 5]
+console.log(rotate(data, -3)); // [4, 5, 1, 2, 3]
