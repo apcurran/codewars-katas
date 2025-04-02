1a3c4ea9b08b1c202de4537a92224e456bbcad29
Alex Curran <alexcurran.va@gmail.com>
2025-04-02 10:48:57 -0500

    Hashmap-based solution for simple-fun-203-strange-coach.js

---
+++ 7-kyu/simple-fun-203-strange-coach.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/**
+ * soltuion 1 -- hashmap
+ * time: O(n * log n) -- sorting results
+ * space: O(n)
+ * 
+ * @param {string[]} players 
+ * @returns {string}
+ */
+function strangeCoach(players) {
+    let firstLetters = new Map();
+
+    for (let player of players) {
+        const firstLetter = player[0];
+        const previousLetterCount = firstLetters.get(firstLetter) || 0;
+        firstLetters.set(firstLetter, previousLetterCount + 1);
+    }
+
+    let results = [];
+
+    for (let [char, charCount] of firstLetters) {
+        if (charCount >= 5) {
+            results.push(char);
+        }
+    }
+
+    if (results.length === 0) {
+        return "forfeit";
+    }
+
+    return results
+        .sort()
+        .join("");
+}
+
+console.log(strangeCoach(["michael", "jordan", "lebron", "james", "kobe", "bryant"])); // "forfeit"
+console.log(strangeCoach(
+    [
+        "babic",
+        "keksic",
+        "boric",
+        "bukic",
+        "sarmic",
+        "balic",
+        "kruzic",
+        "hrenovkic",
+        "beslic",
+        "boksic",
+        "krafnic",
+        "pecivic",
+        "klavirkovic",
+        "kukumaric",
+        "sunkic",
+        "kolacic",
+        "kovacic",
+        "prijestolonasljednikovic",
+    ])); // "bk"
