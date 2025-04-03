91d3e14fdf45264a2a059f14a660f8b1f68b1860
Alex Curran <alexcurran.va@gmail.com>
2025-04-03 10:45:17 -0500

    Iterative approach for coding-3-min-a-b-c.js

---
+++ 7-kyu/coding-3-min-a-b-c.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n^2)
+ * space: O(1)
+ * 
+ * @param {number[]} numbers 
+ * @param {number} c 
+ * @returns {[number, number]|null}
+ */
+function findAB(numbers, c) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const a = numbers[i];
+            const b = numbers[j];
+
+            if (a * b === c) {
+                return [a, b];
+            }
+        }
+    }
+
+    return null;
+}
+
+console.log(findAB([1,2,3], 3)); // [1,3]
+console.log(findAB([1,2,3], 6)); // [2,3]
+console.log(findAB([1,2,3], 7)); // null)
