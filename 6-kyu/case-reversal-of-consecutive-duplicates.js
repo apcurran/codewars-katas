bd4b3a59c23ae9b32fff988e82885076d084d8e4
Alex Curran <alexcurran.va@gmail.com>
2025-04-03 12:01:00 -0500

    Working iterative approach, unoptimized for case-reversal-of-consecutive-duplicates.js

---
+++ 6-kyu/case-reversal-of-consecutive-duplicates.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @returns {string}
+ */
+function reverseCase(str) {
+    let results = [];
+    let consectiveChar = null;
+    let consectiveCharsCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+        const nextChar = str[i + 1];
+
+        if (currentChar === nextChar) {
+            consectiveCharsCount++;
+
+            if (currentChar === currentChar.toLowerCase()) {
+                consectiveChar = currentChar.toUpperCase();
+            } else {
+                consectiveChar = currentChar.toLowerCase();
+            }
+        } else if (consectiveCharsCount > 0) {
+            results.push(consectiveChar.repeat(consectiveCharsCount + 1));
+
+            // reset
+            consectiveChar = null;
+            consectiveCharsCount = 0;
+        } else {
+            results.push(currentChar);
+        }
+    }
+
+    if (consectiveChar) {
+        results.push(consectiveChar.repeat(consectiveCharsCount + 1));
+    }
+
+    return results.join("");
+}
+
+console.log(reverseCase("puzzles")); // "puZZles"
+console.log(reverseCase("shhh")); // "sHHH"
