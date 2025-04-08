4cf34517de164ac40107df86016f8ab9755e8dd2
Alex Curran <alexcurran.va@gmail.com>
2025-04-08 10:19:54 -0500

    Regex solution for regexp-basics-parsing-prices.js

---
+++ 7-kyu/regexp-basics-parsing-prices.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @returns {number|null}
+ */
+String.prototype.toCents = function () {
+    const isValid = /^\$\d+\.\d{2}$/.test(this);
+
+    if (!isValid) return null;
+
+    return Number(this.replace(/[$.]/g, ""));
+};
+
+console.log("$1".toCents()); // null
+console.log("$1.23".toCents()); // 123
+console.log("$99.99".toCents()); // 9999
