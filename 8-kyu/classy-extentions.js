2a1bb8a6722716542a1508547d3aeabfe6706d33
Alex Curran <alexcurran.va@gmail.com>
2025-04-14 14:02:32 -0500

    Class extend with super keyword for classy-extentions.js

---
+++ 8-kyu/classy-extentions.js
@@ -0,0 +1,14 @@
+"use strict";
+
+class Cat extends Animal {
+    /**
+     * @param {string} name 
+     */
+    constructor(name) {
+        super(name);
+    }
+
+    speak() {
+        return `${this.name} meows.`;
+    }
+}
