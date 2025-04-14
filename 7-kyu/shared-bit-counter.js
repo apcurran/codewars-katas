b1d7b06fb5975918254fb216e9bc90506469c72b
Alex Curran <alexcurran.va@gmail.com>
2025-04-14 15:44:41 -0500

    Linear time/space solution for shared-bit-counter.js

---
+++ 7-kyu/shared-bit-counter.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = sharedOneBits lengths
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {boolean}
+ */
+function sharedBits(a, b) {
+    const sharedOneBits = (a & b).toString(2);
+    let oneBitsCounter = 0;
+
+    for (let i = sharedOneBits.length - 1; i >= 0; i--) {
+        if (sharedOneBits[i] === "1") {
+            oneBitsCounter++;
+        }
+
+        if (oneBitsCounter >= 2) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(sharedBits(7, 15)); // true
+console.log(sharedBits(16, 8)); // false
