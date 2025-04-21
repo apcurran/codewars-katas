e31c642ec2fbebb976fc8b3fd70947cf64b1da65
Alex Curran <alexcurran.va@gmail.com>
2025-04-21 11:36:08 -0500

    More efficient math-based solution with O(1) time

--- 7-kyu/consecutive-digits-to-form-sum.js
+++ 7-kyu/consecutive-digits-to-form-sum.js
@@ -28,23 +28,38 @@
 //     return false;
 // }
 
+// /**
+//  * solution 2 -- math (check for power of 2)
+//  * time: O(log n)
+//  * space: O(1)
+//  * 
+//  * @param {number} num 
+//  * @returns {boolean}
+//  */
+// function consecutiveDucks(num) {
+//     if (num < 2) return false;
+
+//     while (num % 2 === 0) {
+//         // while divisible by 2, divide
+//         num = num / 2;
+//     }
+
+//     return num !== 1;
+// }
+
 /**
- * solution 2 -- math (check for power of 2)
- * time: O(log n)
+ * solution 3 -- math (check for power of 2)
+ * time: O(1)
  * space: O(1)
  * 
  * @param {number} num 
  * @returns {boolean}
  */
 function consecutiveDucks(num) {
-    if (num < 2) return false;
+    const numLogBase2 = Math.log2(num);
+    const isNumPowerOfTwo = Number.isInteger(numLogBase2); // int indicates a power of 2
 
-    while (num % 2 === 0) {
-        // while divisible by 2, divide
-        num = num / 2;
-    }
-
-    return num !== 1;
+    return !isNumPowerOfTwo; // nums that are NOT a power of 2 can have a valid consecutive sum
 }
 
 console.log(consecutiveDucks(9)); // true
