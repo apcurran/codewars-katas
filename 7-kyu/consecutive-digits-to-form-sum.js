d891ff6651eead89d4552b6f8785d42e5e201ec0
Alex Curran <alexcurran.va@gmail.com>
2025-04-21 11:28:06 -0500

    Math solution -- check for power of 2

--- 7-kyu/consecutive-digits-to-form-sum.js
+++ 7-kyu/consecutive-digits-to-form-sum.js
@@ -1,31 +1,50 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number} num 
+//  * @returns {boolean}
+//  */
+// function consecutiveDucks(num) {
+//     const upperLimit = Math.ceil(num / 2);
+
+//     for (let i = 1; i <= upperLimit; i++) {
+//         let sum = i; // already start sum with outerNum (i), then add other values
+
+//         for (let j = i + 1; j <= upperLimit; j++) {
+//             // add other values to sum
+//             sum += j;
+
+//             // check if n reached yet
+//             if (sum === num) {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
+ * solution 2 -- math (check for power of 2)
+ * time: O(log n)
  * space: O(1)
  * 
  * @param {number} num 
  * @returns {boolean}
  */
 function consecutiveDucks(num) {
-    const upperLimit = Math.ceil(num / 2);
+    if (num < 2) return false;
 
-    for (let i = 1; i <= upperLimit; i++) {
-        let sum = i; // already start sum with outerNum (i), then add other values
-
-        for (let j = i + 1; j <= upperLimit; j++) {
-            // add other values to sum
-            sum += j;
-
-            // check if n reached yet
-            if (sum === num) {
-                return true;
-            }
-        }
+    while (num % 2 === 0) {
+        // while divisible by 2, divide
+        num = num / 2;
     }
 
-    return false;
+    return num !== 1;
 }
 
 console.log(consecutiveDucks(9)); // true
