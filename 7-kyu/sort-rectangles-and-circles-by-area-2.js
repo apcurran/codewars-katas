2d3baba8febe5d1524db07ad257534b171ebe627
Alex Curran <alexcurran.va@gmail.com>
2025-04-23 10:51:53 -0500

    More efficient refactor

--- 7-kyu/sort-rectangles-and-circles-by-area-2.js
+++ 7-kyu/sort-rectangles-and-circles-by-area-2.js
@@ -10,27 +10,18 @@
  */
 function sortByArea(array) {
     return array
-        .map(function calcArea(value, index) {
-            let area;
-
-            if (Array.isArray(value)) {
-                area = value[0] * value[1];
-            } else {
-                area = Math.PI * value ** 2;
-            }
-
-            return {
-                area,
-                value,
-                index,
-            };
-        })
-        .sort(function sortAreas(objA, objB) {
-            return objA.area - objB.area;
-        })
-        .map(function convertBackToValue(obj) {
-            return obj.value;
+        .slice()
+        .sort(function sortAreas(valueA, valueB) {
+            return calcArea(valueA) - calcArea(valueB);
         });
+}
+
+function calcArea(value) {
+    if (Array.isArray(value)) {
+        return value[0] * value[1];
+    } else {
+        return Math.PI * value ** 2;
+    }
 }
 
 console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]])); // [ [1.342, 3.212], 1.23, [4.23, 6.43], 3.444 ]
