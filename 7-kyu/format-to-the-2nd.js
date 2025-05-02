0970348d1b7aecc96a36e774810c8a9b779b578a
Alex Curran <alexcurran.va@gmail.com>
2025-05-02 11:29:45 -0500

    Working solution for format-to-the-2nd.j

---
+++ 7-kyu/format-to-the-2nd.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = args length
+ * m = num digits length
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param  {...any} args 
+ */
+function printNums(...args) {
+    let largestNumDigitsCount = 0;
+
+    for (let num of args) {
+        // count digits in num
+        let currentNumDigitsCount = 0;
+
+        while (num > 0) {
+            num = Math.floor(num / 10);
+            currentNumDigitsCount++;
+        }
+
+        // now we have digits count
+        // possible update to max digits num
+        largestNumDigitsCount = Math.max(largestNumDigitsCount, currentNumDigitsCount);
+    }
+
+    // max available now
+    return args
+        .map(function formatNum(num) {
+            return String(num).padStart(largestNumDigitsCount, "0");
+        })
+        .join("\n");
+}
