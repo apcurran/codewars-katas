34941edfd3e161c732623b64c8774feeb327b02b
Alex Curran <alexcurran.va@gmail.com>
2025-05-02 16:57:36 -0500

    Bitwise XOR solution with optimal constant space

--- 6-kyu/find-the-unique-number.js
+++ 6-kyu/find-the-unique-number.js
@@ -1,26 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1 -- hashmap (correct, but TLE)
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums 
+//  * @returns {number}
+//  */
+// function findUnique(nums) {
+//     let numsFrequencies = new Map();
+
+//     for (let num of nums) {
+//         const previousNumCount = numsFrequencies.get(num) || 0;
+//         numsFrequencies.set(num, previousNumCount + 1);
+//     }
+
+//     for (let [num, numCount] of numsFrequencies) {
+//         if (numCount === 1) {
+//             return num;
+//         }
+//     }
+// }
+
 /**
- * solution 1 -- hashmap (correct, but TLE)
+ * solution 2 -- Bitwise XOR
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums 
  * @returns {number}
  */
 function findUnique(nums) {
-    let numsFrequencies = new Map();
+    let unique = 0;
 
     for (let num of nums) {
-        const previousNumCount = numsFrequencies.get(num) || 0;
-        numsFrequencies.set(num, previousNumCount + 1);
+        unique ^= num;
     }
 
-    for (let [num, numCount] of numsFrequencies) {
-        if (numCount === 1) {
-            return num;
-        }
-    }
+    return unique;
 }
 
 console.log(findUnique([1, 8, 4, 4, 6, 1, 8])); // 6
