2420acbdb486f73932c6fa9ba48accf28bb01c66
Alex Curran <alexcurran.va@gmail.com>
2025-05-05 15:27:01 -0500

    Math-based GCD helper func solution for simple-fun-179-fraction.js

---
+++ 7-kyu/simple-fun-179-fraction.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(log min(a, b))
+ * space: O(log min(a, b)) -- recursive callstack for getGCD func
+ * 
+ * @param {number} numerator 
+ * @param {number} denominator 
+ * @returns {number}
+ */
+function fraction(numerator, denominator) {
+    const commonValue = getGCD(numerator, denominator);
+    const reducedNumerator = numerator / commonValue;
+    const reducedDenominator = denominator / commonValue;
+
+    return reducedNumerator + reducedDenominator;
+}
+
+console.log(fraction(2, 4)); // 1 + 2 -> 3
+
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function getGCD(a, b) {
+    if (b === 0) {
+        return a;
+    } else {
+        return getGCD(b, a % b);
+    }
+}
