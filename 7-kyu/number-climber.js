7933b8f40a3abf56cb91213d8db7d13dffe93106
Alex Curran <alexcurran.va@gmail.com>
2025-05-06 12:24:26 -0500

    Math-based solution

---
+++ 7-kyu/number-climber.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n 
+ * @returns {number[]}
+ */
+function climb(n) {
+    let results = [n];
+
+    while (n > 1) {
+        n = Math.floor(n / 2);
+        results.push(n);
+    }
+
+    return results.reverse();
+}
+
+console.log(climb(10)); // [1, 2, 5, 10]
+console.log(climb(13)); // [1, 3, 6, 13]
