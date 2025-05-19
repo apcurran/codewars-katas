cbb664f1d172a133d8b9d3408d7073681ba03d8a
Alex Curran <alexcurran.va@gmail.com>
2025-05-19 11:14:58 -0500

    Two solutions for genetic-algorithm-series-1-generate.js

---
+++ 7-kyu/genetic-algorithm-series-1-generate.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// /**
+//  * solution 1 -- with helper function
+//  * time: O(n)
+//  * space: O(1) -- not including results
+//  * 
+//  * @param {number} length 
+//  * @returns {string}
+//  */
+// function generate(length) {
+//     let result = "";
+
+//     for (let i = 0; i < length; i++) {
+//         const randomBinaryDigit = getRandomBinary();
+//         result += randomBinaryDigit;
+//     }
+
+//     return result;
+// }
+
+// /**
+//  * @returns {"0"|"1"}
+//  */
+// function getRandomBinary() {
+//     return Math.random() < 0.5 ? "0" : "1";
+// }
+
+/**
+ * solution 2
+ * time: O(n)
+ * space: O(1) -- not including results
+ * 
+ * @param {number} length 
+ * @returns {string}
+ */
+function generate(length) {
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+        const binaryDigit = Math.round(Math.random());
+        result += String(binaryDigit);
+    }
+
+    return result;
+}
