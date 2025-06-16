0a2c004f76219cb81b5f26ec8164cd863948ee51
Alex Curran <alexcurran.va@gmail.com>
2025-06-16 17:03:54 -0500

    Bitwise XOR with for loop for count

--- 7-kyu/delta-bits.js
+++ 7-kyu/delta-bits.js
@@ -1,7 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(1) -- max 32-bits
+//  * space: O(1)
+//  * 
+//  * @param {number} a 
+//  * @param {number} b 
+//  * @returns {number}
+//  */
+// function convertBits(a, b) {
+//     const aBinary = a.toString(2).padStart(32, "0");
+//     const bBinary = b.toString(2).padStart(32, "0");
+//     let differentBitsCount = 0;
+
+//     for (let i = aBinary.length - 1; i >= 0; i--) {
+//         if (aBinary[i] !== bBinary[i]) {
+//             differentBitsCount++;
+//         }
+//     }
+
+//     return differentBitsCount;
+// }
+
 /**
- * solution 1
+ * solution 2 -- bitwise XOR
  * time: O(1) -- max 32-bits
  * space: O(1)
  * 
@@ -10,17 +33,16 @@
  * @returns {number}
  */
 function convertBits(a, b) {
-    const aBinary = a.toString(2).padStart(32, "0");
-    const bBinary = b.toString(2).padStart(32, "0");
-    let differentBitsCount = 0;
+    const differentBits = (a ^ b).toString(2);
+    let count = 0;
 
-    for (let i = aBinary.length - 1; i >= 0; i--) {
-        if (aBinary[i] !== bBinary[i]) {
-            differentBitsCount++;
+    for (let i = 0; i < differentBits.length; i++) {
+        if (differentBits[i] === "1") {
+            count++;
         }
     }
 
-    return differentBitsCount;
+    return count;
 }
 
 console.log(convertBits(31, 14)); // 2
