1403cb7d26d765ea8bc8bb7d8fb1160b5cdc0d21
Alex Curran <alexcurran.va@gmail.com>
2025-06-16 16:59:03 -0500

    String-based solution with for loop

---
+++ 7-kyu/delta-bits.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1) -- max 32-bits
+ * space: O(1)
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function convertBits(a, b) {
+    const aBinary = a.toString(2).padStart(32, "0");
+    const bBinary = b.toString(2).padStart(32, "0");
+    let differentBitsCount = 0;
+
+    for (let i = aBinary.length - 1; i >= 0; i--) {
+        if (aBinary[i] !== bBinary[i]) {
+            differentBitsCount++;
+        }
+    }
+
+    return differentBitsCount;
+}
+
+console.log(convertBits(31, 14)); // 2
