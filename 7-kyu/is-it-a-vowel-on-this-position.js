d2c361df533fc659af9f48b683036a7384ef7895
Alex Curran <alexcurran.va@gmail.com>
2025-06-18 17:29:21 -0500

    String.includes() proto method version

--- 7-kyu/is-it-a-vowel-on-this-position.js
+++ 7-kyu/is-it-a-vowel-on-this-position.js
@@ -1,7 +1,26 @@
 "use strict";
 
+// /**
+//  * solution 1 -- regex
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string} str 
+//  * @param {number} position 
+//  * @returns {boolean}
+//  */
+// function checkVowel(str, position) {
+//     if (position > str.length - 1 || position < 0) {
+//         return false;
+//     }
+
+//     const isVowel = /[aeiou]/i;
+
+//     return isVowel.test(str[position]);
+// }
+
 /**
- * solution 1 -- regex
+ * solution 2 -- string.includes() prototype method
  * time: O(n)
  * space: O(1)
  * 
@@ -10,13 +29,7 @@
  * @returns {boolean}
  */
 function checkVowel(str, position) {
-    if (position > str.length - 1 || position < 0) {
-        return false;
-    }
-
-    const isVowel = /[aeiou]/i;
-
-    return isVowel.test(str[position]);
+    return "aeiouAEIOU".includes(str[position]);
 }
 
 console.log(checkVowel("cat", 1)); // true
