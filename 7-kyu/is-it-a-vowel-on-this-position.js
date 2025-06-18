4e48edd95e5907cfd8616addbf6575a15f4b218a
Alex Curran <alexcurran.va@gmail.com>
2025-06-18 17:27:42 -0500

    Regex version

---
+++ 7-kyu/is-it-a-vowel-on-this-position.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str 
+ * @param {number} position 
+ * @returns {boolean}
+ */
+function checkVowel(str, position) {
+    if (position > str.length - 1 || position < 0) {
+        return false;
+    }
+
+    const isVowel = /[aeiou]/i;
+
+    return isVowel.test(str[position]);
+}
+
+console.log(checkVowel("cat", 1)); // true
+console.log(checkVowel("cat", 0)); // false
+console.log(checkVowel("cat", 4)); // false
