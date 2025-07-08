5a92eae894dfc1a478e3299da40ed2fd19cd32ed
Alex <alexcurran.va@gmail.com>
2025-07-08 11:34:17 -0500

    Regex solution that captures all nums, alpha chars, and math operators

---
+++ 7-kyu/simple-equation-reversal.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1 -- regex (match method for getting all nums, alpha chars, and math operators)
+ * time: O(n)
+ * space: O(n)
+ *
+ * @param {string} eq
+ * @returns {string}
+ */
+function solve(eq) {
+    return eq
+        .match(/\d+|[a-zA-Z]|[+\-*/]/g)
+        .reverse()
+        .join("");
+}
+
+console.log(solve("100*b/y")); // "y/b*100"
