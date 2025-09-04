bc706df771f7cdafb0aa13c56762d61e9cc33360
Alex Curran <alexcurran.va@gmail.com>
2025-09-04 11:02:37 -0500

    Recusive solution

--- 6-kyu/search-in-multidimensional-array.js
+++ 6-kyu/search-in-multidimensional-array.js
@@ -1,18 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1 -- flatten array proto method
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {array} arr
+//  * @param {string} value
+//  * @returns {boolean}
+//  */
+// function locate(arr, value) {
+//     const flattenedValues = arr.flat(Infinity);
+
+//     return flattenedValues.includes(value);
+// }
+
 /**
- * solution 1 -- flatten array proto method
+ * solution 2 -- recursion
  * time: O(n)
- * space: O(n)
+ * space: O(d) -- depth of nesting
  *
  * @param {array} arr
  * @param {string} value
  * @returns {boolean}
  */
 function locate(arr, value) {
-    const flattenedValues = arr.flat(Infinity);
+    for (let elem of arr) {
+        if (elem === value) return true;
 
-    return flattenedValues.includes(value);
+        if (Array.isArray(elem) && locate(elem, value)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 console.log(locate(["a", "b", ["c", "d", ["e"]]], "e")); // true
