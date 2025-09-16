26811d0f7cc2cf12813555a749b7e6ea81f14a52
Alex Curran <alexcurran.va@gmail.com>
2025-09-16 09:55:03 -0500

    Sliding window optimized approach

--- 6-kyu/zeros-and-ones.js
+++ 6-kyu/zeros-and-ones.js
@@ -1,47 +1,80 @@
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {number[]} arr
+//  * @returns {number} index of replacement 0
+//  */
+// function replaceZero(arr) {
+//     let largestSum = 0;
+//     let replacementIndex = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let leftOnesCount = 0;
+
+//         for (let left = i - 1; left >= 0; left--) {
+//             if (arr[left] === 1) {
+//                 leftOnesCount++;
+//             } else {
+//                 // reset ones count if broken by a 0
+//                 break;
+//             }
+//         }
+
+//         let rightOnesCount = 0;
+
+//         for (let right = i + 1; right < arr.length; right++) {
+//             if (arr[right] === 1) {
+//                 rightOnesCount++;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         const currentOnesTotal = leftOnesCount + rightOnesCount;
+
+//         if (currentOnesTotal >= largestSum) {
+//             largestSum = currentOnesTotal;
+//             // update index
+//             replacementIndex = i;
+//         }
+//     }
+
+//     return replacementIndex;
+// }
+
 /**
- * solution 1
- * time: O(n^2)
+ * solution 2 -- sliding window
+ * time: O(n)
  * space: O(1)
  *
  * @param {number[]} arr
  * @returns {number} index of replacement 0
  */
 function replaceZero(arr) {
-    let largestSum = 0;
-    let replacementIndex = 0;
+    let left = 0;
+    let zeroIndex = -1;
+    let bestIndex = -1;
+    let maxLength = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        let leftOnesCount = 0;
-
-        for (let left = i - 1; left >= 0; left--) {
-            if (arr[left] === 1) {
-                leftOnesCount++;
-            } else {
-                // reset ones count if broken by a 0
-                break;
-            }
+    for (let right = 0; right < arr.length; right++) {
+        if (arr[right] === 0) {
+            // move left pointer past the prev zero
+            left = zeroIndex + 1;
+            zeroIndex = right;
         }
 
-        let rightOnesCount = 0;
+        const windowLength = right - left + 1;
 
-        for (let right = i + 1; right < arr.length; right++) {
-            if (arr[right] === 1) {
-                rightOnesCount++;
-            } else {
-                break;
-            }
-        }
-
-        const currentOnesTotal = leftOnesCount + rightOnesCount;
-
-        if (currentOnesTotal >= largestSum) {
-            largestSum = currentOnesTotal;
-            // update index
-            replacementIndex = i;
+        // >= updates the latest index in tie cases
+        if (windowLength >= maxLength) {
+            maxLength = windowLength;
+            bestIndex = zeroIndex;
         }
     }
 
-    return replacementIndex;
+    return bestIndex;
 }
 
 console.log(
