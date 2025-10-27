3cf7d8e0a619f9682d3417ad17ef259b0fd61488
Alex Curran <alexcurran.va@gmail.com>
2025-10-27 16:53:18 -0500

    Map solution for groupby-advanced.js

---
+++ 6-kyu/groupby-advanced.js
@@ -0,0 +1,23 @@
+/**
+ * solution 1
+ *
+ * @param {array} people
+ * @param {function} classifier
+ * @param {function} downstream
+ * @param {function} accumulatorSupplier
+ * @returns {Map}
+ */
+function groupBy(people, classifier, downstream, accumulatorSupplier) {
+    let results = new Map();
+
+    for (let person of people) {
+        const key = classifier(person);
+        const current = results.has(key)
+            ? results.get(key)
+            : accumulatorSupplier();
+        const downstreamResult = downstream(current, person);
+        results.set(key, downstreamResult);
+    }
+
+    return results;
+}
