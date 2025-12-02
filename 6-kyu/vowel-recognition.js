b7d8f202316f2915db1a2d0b2f5ed9c2dbea48d6
Alex Curran <alexcurran.va@gmail.com>
2025-12-02 16:24:54 -0600

    Math formula needed for linear time solution

--- 6-kyu/vowel-recognition.js
+++ 6-kyu/vowel-recognition.js
@@ -27,9 +27,36 @@
 //     return vowelsSum;
 // }
 
+// /**
+//  * solution 2 -- brute force (TLE)
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {string} input
+//  * @returns {number} - sum of all vowel counts in substrs
+//  */
+// function vowelRecognition(input) {
+//     const vowels = "aeiouAEIOU";
+//     let vowelsSum = 0;
+
+//     for (let i = 0; i < input.length; i++) {
+//         let substrVowelCount = 0;
+
+//         for (let j = i; j < input.length; j++) {
+//             if (vowels.includes(input[j])) {
+//                 substrVowelCount++;
+//             }
+
+//             vowelsSum += substrVowelCount;
+//         }
+//     }
+
+//     return vowelsSum;
+// }
+
 /**
- * solution 2 -- brute force (TLE)
- * time: O(n^2)
+ * solution 3 -- math formula
+ * time: O(n)
  * space: O(1)
  *
  * @param {string} input
@@ -37,17 +64,13 @@
  */
 function vowelRecognition(input) {
     const vowels = "aeiouAEIOU";
+    const n = input.length;
     let vowelsSum = 0;
 
-    for (let i = 0; i < input.length; i++) {
-        let substrVowelCount = 0;
-
-        for (let j = i; j < input.length; j++) {
-            if (vowels.includes(input[j])) {
-                substrVowelCount++;
-            }
-
-            vowelsSum += substrVowelCount;
+    for (let i = 0; i < n; i++) {
+        if (vowels.includes(input[i])) {
+            const vowelCount = (i + 1) * (n - i);
+            vowelsSum += vowelCount;
         }
     }
 
