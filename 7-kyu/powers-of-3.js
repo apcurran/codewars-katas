59877acdd8ace9e6e7623a66a9ca3407a2aec464
Alex Curran <alexcurran.va@gmail.com>
2026-02-11 13:38:30 -0600

    Logarithm calc solution 2

--- 7-kyu/powers-of-3.js
+++ 7-kyu/powers-of-3.js
@@ -1,19 +1,36 @@
+// /**
+//  * solution 1
+//  * time: O(log(3) n)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @returns {number}
+//  */
+// function largestPower(n) {
+//     let power = 0;
+
+//     while (3 ** power < n) {
+//         power++;
+//     }
+
+//     return power - 1;
+// }
+
 /**
- * solution 1
- * time: O(log(3) n)
+ * solution 2 -- logarithm
+ * time: O(1)
  * space: O(1)
  *
  * @param {number} n
  * @returns {number}
  */
 function largestPower(n) {
-    let power = 0;
+    // change of base formula
+    // need log10 to solve imprecision leading to some false answers with log()
+    const logBase3 = Math.ceil(Math.log10(n) / Math.log10(3));
 
-    while (3 ** power < n) {
-        power++;
-    }
-
-    return power - 1;
+    // must be less than log(base 3)(n)
+    return logBase3 - 1;
 }
 
 console.log(largestPower(1)); // -1
