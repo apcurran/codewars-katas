ce21868a131ae12d74bb4e7bad57560619ca5c5e
Alex Curran <alexcurran.va@gmail.com>
2026-07-21 12:00:00 -0500

    Sort solution -- simpler to understand, but worse big-O time

--- 7-kyu/sheeeeps.js
+++ 7-kyu/sheeeeps.js
@@ -1,47 +1,72 @@
+// /**
+//  * solution 1 -- hash map
+//  * n = possibleSheep length
+//  * m = word length
+//  * time: O(n * m)
+//  * space: O(m) -- not including results arr
+//  *
+//  * @param {string[]} possibleSheep
+//  * @returns {string[]}
+//  */
+// function reloadSheeps(possibleSheep) {
+//     // "sheep" hash map should contain
+//     const SHEEP_CHARS = new Map([
+//         ["s", 1],
+//         ["h", 1],
+//         ["e", 2],
+//         ["p", 1],
+//     ]);
+//     let results = [];
+
+//     outerWord: for (let word of possibleSheep) {
+//         let wordChars = new Map();
+
+//         for (let char of word) {
+//             const previousCount = wordChars.get(char) || 0;
+//             wordChars.set(char, previousCount + 1);
+//         }
+
+//         // compare size first!
+//         if (SHEEP_CHARS.size !== wordChars.size) {
+//             continue outerWord;
+//         }
+
+//         // iterate wordChars and compare to known SHEEP_CHARS values
+//         for (let [knownSheepChar, knownSheepCharCount] of SHEEP_CHARS) {
+//             const possibleSheepCharCount = wordChars.get(knownSheepChar) || 0;
+
+//             if (knownSheepCharCount !== possibleSheepCharCount) {
+//                 continue outerWord;
+//             }
+//         }
+
+//         // matching "sheep" to results
+//         results.push("sheep");
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1 -- hash map
+ * solution 2 -- sort
  * n = possibleSheep length
  * m = word length
- * time: O(n * m)
+ * time: O(n * (m * log m))
  * space: O(m) -- not including results arr
  *
  * @param {string[]} possibleSheep
  * @returns {string[]}
  */
 function reloadSheeps(possibleSheep) {
-    // "sheep" hash map should contain
-    const SHEEP_CHARS = new Map([
-        ["s", 1],
-        ["h", 1],
-        ["e", 2],
-        ["p", 1],
-    ]);
+    const SHEEP_SORTED = "eehps";
     let results = [];
 
-    outerWord: for (let word of possibleSheep) {
-        let wordChars = new Map();
+    for (let word of possibleSheep) {
+        const sortedWord = word.split("").sort().join("");
 
-        for (let char of word) {
-            const previousCount = wordChars.get(char) || 0;
-            wordChars.set(char, previousCount + 1);
+        if (SHEEP_SORTED === sortedWord) {
+            results.push("sheep");
         }
-
-        // compare size first!
-        if (SHEEP_CHARS.size !== wordChars.size) {
-            continue outerWord;
-        }
-
-        // iterate wordChars and compare to known SHEEP_CHARS values
-        for (let [knownSheepChar, knownSheepCharCount] of SHEEP_CHARS) {
-            const possibleSheepCharCount = wordChars.get(knownSheepChar) || 0;
-
-            if (knownSheepCharCount !== possibleSheepCharCount) {
-                continue outerWord;
-            }
-        }
-
-        // matching "sheep" to results
-        results.push("sheep");
     }
 
     return results;
