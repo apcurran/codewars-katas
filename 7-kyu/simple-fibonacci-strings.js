f91ccfd686c3f4e4c40c6b843adec3ce97ea09ba
Alex Curran <alexcurran.va@gmail.com>
2026-07-29 10:51:29 -0500

    Hash map memo solution 2

--- 7-kyu/simple-fibonacci-strings.js
+++ 7-kyu/simple-fibonacci-strings.js
@@ -1,12 +1,32 @@
+// /**
+//  * solution 1 -- recursion
+//  * time: O(2^n)
+//  * space: O(n)
+//  *
+//  * @param {number} n
+//  * @returns {string}
+//  */
+// function solve(n) {
+//     if (n === 0) {
+//         return "0";
+//     }
+
+//     if (n === 1) {
+//         return "01";
+//     }
+
+//     return solve(n - 1) + solve(n - 2);
+// }
+
 /**
- * solution 1 -- recursion
- * time: O(2^n)
+ * solution 2 -- recursion with memoization
+ * time: O(n)
  * space: O(n)
  *
  * @param {number} n
  * @returns {string}
  */
-function solve(n) {
+function solve(n, cache = new Map()) {
     if (n === 0) {
         return "0";
     }
@@ -15,7 +35,14 @@ function solve(n) {
         return "01";
     }
 
-    return solve(n - 1) + solve(n - 2);
+    if (cache.has(n)) {
+        return cache.get(n);
+    }
+    // otherwise, calc solution and store in cache
+    const result = solve(n - 1, cache) + solve(n - 2, cache);
+    cache.set(n, result);
+
+    return result;
 }
 
 console.log(solve(3)); // "01001"
